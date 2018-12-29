30905c5bccd2b6885f12904a9138bf5bf6a18b90
Mark Hindess <soft-github@temporalanomaly.com>
2018-12-29 22:53:16 +0000

    2016 Go day 23

---
+++ 2016/23/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+func TestPart1(t *testing.T) {
+	prog := ReadElfProg2016(ReadLines("test.txt"))
+	assert.Equal(t, 3, Part1(prog))
+	prog = ReadElfProg2016(ReadLines("input.txt"))
+	assert.Equal(t, 12654, Part1(prog))
+}
+
+func TestPart2(t *testing.T) {
+	prog := ReadElfProg2016(ReadLines("test.txt"))
+	assert.Equal(t, 3, Part2(prog))
+	//prog = ReadElfProg2016(ReadLines("input.txt"))
+	//assert.Equal(t, 3, Part2(prog))
+}
