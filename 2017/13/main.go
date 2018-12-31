5eaeb8920e44975bd63eea3d096240a8079103ca
Mark Hindess <soft-github@temporalanomaly.com>
2018-12-31 16:04:13 +0000

    2017 Day 13: Go

---
+++ 2017/13/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+type Scanner struct {
+	depth int
+	r     int
+}
+
+type Firewall struct {
+	scanners []Scanner
+	debug    bool
+}
+
+func NewFirewall(lines []string) *Firewall {
+	fw := &Firewall{[]Scanner{}, false}
+	for _, line := range lines {
+		ints := SimpleReadInts(line)
+		fw.scanners = append(fw.scanners, Scanner{ints[0], ints[1]})
+	}
+	return fw
+}
+
+func (fw *Firewall) Run(delay int) (int, bool) {
+	sev := 0
+	caught := false
+	for _, s := range fw.scanners {
+		if (delay+s.depth)%((s.r-1)*2) == 0 {
+			if fw.debug {
+				fmt.Printf("hit at depth %d\n", s.depth)
+			}
+			sev += s.depth * s.r
+			caught = true
+		}
+	}
+	return sev, caught
+}
+
+func (fw *Firewall) Part1() int {
+	sev, _ := fw.Run(0)
+	return sev
+}
+
+func (fw *Firewall) Part2() int {
+	for delay := 0; ; delay++ {
+		_, caught := fw.Run(delay)
+		if !caught {
+			return delay
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <input.txt>\n", os.Args[0])
+	}
+	fw := NewFirewall(ReadLines(os.Args[1]))
+	fmt.Printf("Part 1: %d\n", fw.Part1())
+	fmt.Printf("Part 2: %d\n", fw.Part2())
+}
---
+++ 2017/13/main_test.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+func TestPart1(t *testing.T) {
+	assert.Equal(t, 24, NewFirewall(ReadLines("test.txt")).Part1())
+	assert.Equal(t, 1928, NewFirewall(ReadLines("input.txt")).Part1())
+}
+
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 10, NewFirewall(ReadLines("test.txt")).Part2())
+	assert.Equal(t, 3830344, NewFirewall(ReadLines("input.txt")).Part2())
+}
