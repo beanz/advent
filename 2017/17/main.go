96ad34ac81c84a625c3767fc2876c29f234261ae
Mark Hindess <soft-github@temporalanomaly.com>
2019-01-01 10:14:06 +0000

    2017 Day 17: Go

---
+++ 2017/17/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+type Game struct {
+	state *Circle
+	steps int
+	debug bool
+}
+
+func (g *Game) String() string {
+	t := g.state
+	for t.Num != 0 {
+		t = t.Cw
+	}
+	s := []string{}
+	for {
+		if t == g.state {
+			s = append(s, fmt.Sprintf("(%d)", t.Num))
+		} else {
+			s = append(s, fmt.Sprintf(" %d ", t.Num))
+		}
+		t = t.Cw
+		if t.Num == 0 {
+			break
+		}
+	}
+	return strings.Join(s, " ")
+}
+
+func NewGame(steps int) *Game {
+	g := &Game{nil, steps, false}
+	g.state = NewCircle(0)
+	return g
+}
+
+func (g *Game) Part1(nums int) int {
+	if g.debug {
+		fmt.Printf("%s\n", g)
+	}
+	for i := 1; i <= nums; i++ {
+		for j := 0; j < g.steps; j++ {
+			g.state = g.state.Cw
+		}
+		g.state = g.state.AddNew(i)
+		if g.debug {
+			fmt.Printf("%s\n", g)
+		}
+	}
+	return g.state.Cw.Num
+}
+
+func (g *Game) Part2() int {
+	g.Part1(50000000)
+	t := g.state
+	for t.Num != 0 {
+		t = t.Cw
+	}
+	return t.Cw.Num
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <input>\n", os.Args[0])
+	}
+	steps, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatalf("Invalid integer input %s: %s", os.Args[1], err)
+	}
+	fmt.Printf("Part 1: %d\n", NewGame(steps).Part1(2017))
+	fmt.Printf("Part 2: %d\n", NewGame(steps).Part2())
+}
---
+++ 2017/17/main_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	assert.Equal(t, 638, NewGame(3).Part1(2017))
+}
+
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 1222153, NewGame(3).Part2())
+}
