25b152c042e91ace8e0be42c7ca8cd4277c14840
Mark Hindess <soft-github@temporalanomaly.com>
2019-01-04 20:31:18 +0000

    2017 Day 22: Go

---
+++ 2017/22/main.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+type NodeState byte
+
+const (
+	CLEAN    NodeState = 0
+	INFECTED NodeState = '#'
+	WEAKENED NodeState = 'W'
+	FLAGGED  NodeState = 'F'
+)
+
+type Game struct {
+	points map[Point]NodeState
+	bb     *BoundingBox
+	cur    Point
+	dir    Direction
+	count  int
+	debug  bool
+}
+
+func (g *Game) String() string {
+	s := ""
+	for y := g.bb.Min.Y; y <= g.bb.Max.Y; y++ {
+		for x := g.bb.Min.X; x <= g.bb.Max.X; x++ {
+			p := Point{x, y}
+			var sq string
+			switch g.points[p] {
+			case INFECTED:
+				sq = "#"
+			case WEAKENED:
+				sq = "W"
+			case FLAGGED:
+				sq = "F"
+			default:
+				sq = "."
+			}
+			if p == g.cur {
+				sq = Red(sq)
+			}
+			s += sq
+		}
+		s += "\n"
+	}
+	return s
+}
+
+func NewGame(lines []string) *Game {
+	g := &Game{map[Point]NodeState{},
+		NewBoundingBox(),
+		Point{0, 0},
+		Compass("N").Direction(),
+		0,
+		false}
+	for y, line := range lines {
+		for x, ch := range line {
+			p := Point{x, y}
+			g.bb.Add(p)
+			if ch == '#' {
+				g.points[p] = INFECTED
+			}
+		}
+	}
+	g.cur = Point{(g.bb.Max.X + g.bb.Min.X) / 2, (g.bb.Max.Y + g.bb.Min.Y) / 2}
+	return g
+}
+
+func (g *Game) Burst1() {
+	infected := g.points[g.cur] == INFECTED
+	if infected {
+		g.dir = g.dir.CW()
+	} else {
+		g.dir = g.dir.CCW()
+	}
+	if infected {
+		delete(g.points, g.cur)
+	} else {
+		g.points[g.cur] = INFECTED
+		g.count++
+	}
+	g.cur = g.cur.In(g.dir)
+	g.bb.Add(g.cur)
+}
+
+func (g *Game) Part1(bursts int) int {
+	for i := 0; i < bursts; i++ {
+		g.Burst1()
+		if g.debug {
+			fmt.Printf("%s\n", g)
+		}
+	}
+	return g.count
+}
+
+func (g *Game) Burst2() {
+	state := g.points[g.cur]
+	switch state {
+	case WEAKENED:
+		// no turn
+		g.points[g.cur] = INFECTED
+		g.count++
+	case INFECTED:
+		g.dir = g.dir.CW()
+		g.points[g.cur] = FLAGGED
+	case FLAGGED:
+		g.dir = g.dir.CW().CW()
+		delete(g.points, g.cur)
+	default: // CLEAN
+		g.dir = g.dir.CCW()
+		g.points[g.cur] = WEAKENED
+	}
+	g.cur = g.cur.In(g.dir)
+	g.bb.Add(g.cur)
+}
+
+func (g *Game) Part2(bursts int) int {
+	for i := 0; i < bursts; i++ {
+		g.Burst2()
+		if g.debug {
+			fmt.Printf("%s\n", g)
+		}
+	}
+	return g.count
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <input.txt>\n", os.Args[0])
+	}
+	lines := ReadLines(os.Args[1])
+	fmt.Printf("Part 1: %d\n", NewGame(lines).Part1(10000))
+	fmt.Printf("Part 2: %d\n", NewGame(lines).Part2(10000000))
+}
---
+++ 2017/22/main_test.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+func TestPart1(t *testing.T) {
+	assert.Equal(t, 5587, NewGame(ReadLines("test.txt")).Part1(10000))
+	assert.Equal(t, 5240, NewGame(ReadLines("input.txt")).Part1(10000))
+}
+
+func TestPart2(t *testing.T) {
+	assert.Equal(t, 26, NewGame(ReadLines("test.txt")).Part2(100))
+	assert.Equal(t, 2511944, NewGame(ReadLines("test.txt")).Part2(10000000))
+	assert.Equal(t, 2512144, NewGame(ReadLines("input.txt")).Part2(10000000))
+}
