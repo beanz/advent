fc5e5a602b1cf1c2024d7202786c53a64e53bce0
Mark Hindess <soft-github@temporalanomaly.com>
2019-12-18 11:58:15 +0000

    2019 Day 18

---
+++ 2019/18/aoc.pl
@@ -0,0 +1,242 @@
+#!/usr/bin/perl
+use warnings FATAL => 'all';
+use strict;
+use v5.10;
+use lib "../lib";
+use AoC::Helpers qw/:all/;
+use Carp::Always qw/carp verbose/;
+use List::PriorityQueue;
+
+$|=1;
+my @i = <>;
+chomp @i;
+
+my $i = parse_input(\@i);
+
+sub opt {
+  my ($D) = @_;
+  while (1) {
+    my $changes = 0;
+    for my $y ($D->{bb}->[MINY] .. $D->{bb}->[MAXY]) {
+      for my $x ($D->{bb}->[MINX] .. $D->{bb}->[MAXX]) {
+        if ($D->{m}->[$y]->[$x] ne ".") {
+          next;
+        }
+        if ($D->{pos}->[X] == $x && $D->{pos}->[Y] == $y) {
+          next;
+        }
+        my $w = 0;
+        $w++ if ($D->{m}->[$y]->[$x-1] eq "#");
+        $w++ if ($D->{m}->[$y]->[$x+1] eq "#");
+        $w++ if ($D->{m}->[$y-1]->[$x] eq "#");
+        $w++ if ($D->{m}->[$y+1]->[$x] eq "#");
+        if ($w > 2) {
+          $D->{m}->[$y]->[$x] = '#';
+          $changes++;
+        }
+      }
+    }
+    last if (!$changes);
+  }
+  return;
+}
+
+sub draw {
+  my ($D) = @_;
+  my $s = "";
+  for my $y ($D->{bb}->[MINY] .. $D->{bb}->[MAXY]) {
+    for my $x ($D->{bb}->[MINX] .. $D->{bb}->[MAXX]) {
+      if ($D->{pos}->[X] == $x && $D->{pos}->[Y] == $y) {
+        $s .= '@';
+      } else {
+        $s .= $D->{m}->[$y]->[$x]
+      }
+    }
+    $s .= "\n";
+  }
+  return $s;
+}
+
+sub posstr {
+  my $pos = shift;
+  return "[".$pos->[X].','.$pos->[Y]."]";
+}
+
+sub at {
+  my ($D, $x, $y) = @_;
+  return $D->{m}->[$y]->[$x] // '#';
+}
+
+sub parse_input {
+  my ($lines) = @_;
+  my $D = { m => [], bb => [], pos => [-1,-1], doors => 0, keys => 0 };
+  $D->{bb}->[MINX] = $D->{bb}->[MINY] = 0;
+  $D->{bb}->[MAXX] = length($lines->[0])-1;
+  $D->{bb}->[MAXY] = @$lines-1;
+
+  for my $y (0..@$lines-1) {
+    my @r = ();
+    for my $x (0..length($lines->[$y])-1) {
+      my $ch = substr $lines->[$y], $x, 1;
+      if ($ch eq "@") {
+        $D->{pos}->[X] = $x;
+        $D->{pos}->[Y] = $y;
+        $ch = '.';
+      } elsif ($ch =~ /[a-z]/) {
+        $D->{keys}++;
+      } elsif ($ch =~ /[A-Z]/) {
+        $D->{doors}++;
+      }
+      push @r, $ch;
+    }
+    push @{$D->{m}}, \@r;
+  }
+  return $D;
+}
+
+use constant
+  {
+   POS => 0,
+   STEPS => 1,
+   NUM_KEYS => 2,
+   KEYS => 3,
+   PATH => 4,
+   SORTEDPATH => 5,
+  };
+
+sub is_key_in_quad {
+  my ($D, $key, $quad) = @_;
+  if (!defined $quad) {
+    return 1;
+  }
+  return exists $D->{qk}->{$quad}->{$key};
+}
+
+sub find_keys {
+  my ($D, $pos) = @_;
+  my %k;
+  my %visited;
+  my @search = ($pos);
+  while (@search) {
+    my $cur = shift @search;
+    my $ch = at($D, @$cur);
+    if ($ch eq '#') {
+      next;
+    }
+    next if ($visited{$cur->[Y]}->{$cur->[X]}++);
+    if ($ch =~ /[a-z]/) {
+      $k{$ch}++;
+    }
+    for my $off (@{fourNeighbourOffsets()}) {
+      push @search, [$cur->[X] + $off->[X], $cur->[Y] + $off->[Y]];
+    }
+  }
+  return \%k;
+}
+
+sub find {
+  my ($D, $pos, $quad) = @_;
+  my $search = List::PriorityQueue->new();
+  $search->insert([$pos, 0, 0, {}, "", ""], 0);
+  #               pos, steps, num keys, keys, path, sortedpath
+  my $tested = 0;
+  my $todo = 1;
+  my $expected_keys =
+    defined $quad ? scalar keys %{$D->{qk}->{$quad}} : $D->{keys};
+  print "Searching for $expected_keys keys from @$pos",
+    (defined $quad ? " in quad $quad\n" : ''), "\n" if DEBUG;
+  my %VISITED;
+  my $min = 999999999;
+  #while (@search) {
+    #my $cur = shift @search;
+  while (my $cur = $search->pop()) {
+    print STDERR
+      "Todo: $todo $tested $cur->[STEPS] $cur->[PATH]                    \r"
+      if (DEBUG && ($tested%1000) == 0);
+    $tested++;
+    for my $off (@{fourNeighbourOffsets()}) {
+      my $nx = $cur->[POS][X] + $off->[X];
+      my $ny = $cur->[POS][Y] + $off->[Y];
+      my $ch = at($D, $nx, $ny);
+      if ($ch eq '#') {
+        next;
+      }
+      # POS, STEPS, NUM_KEYS, KEYS, PATH, SORTEDPATH
+      my $new =
+        [
+         [$nx, $ny],
+         $cur->[STEPS]+1,
+         $cur->[NUM_KEYS],
+         { map { $_ => 1 } keys %{$cur->[KEYS]} },
+         $cur->[PATH],
+         $cur->[SORTEDPATH]
+        ];
+      next if ($new->[STEPS] > $min);
+      if ($ch =~ /[A-Z]/) {
+        if (!$new->[KEYS]->{lc $ch} && is_key_in_quad($D, (lc $ch), $quad)) {
+          #print "$quad stuck at door $ch\n";
+          next;
+        }
+      }
+      if ($ch =~ /[a-z]/ && !exists $new->[KEYS]->{$ch}) {
+        $new->[KEYS]->{$ch}++;
+        $new->[NUM_KEYS]++;
+        $new->[PATH] .= $ch;
+        $new->[SORTEDPATH] = join '', sort split //, $new->[PATH];
+        if ($new->[NUM_KEYS] == $expected_keys) {
+          print STDERR "Found: $new->[PATH] $new->[STEPS]                      \n" if DEBUG;
+          if ($min > $new->[STEPS]) {
+            $min = $new->[STEPS];
+          }
+          next;
+        }
+      }
+      my $vkey = $new->[POS]->[X].'!'.$new->[POS]->[Y].'!'.$new->[SORTEDPATH];
+      if (exists $VISITED{$vkey} && $VISITED{$vkey} <= $new->[STEPS]) {
+        next;
+      }
+      $VISITED{$vkey} = $new->[STEPS];
+      my $remaining_keys = $expected_keys - $new->[NUM_KEYS];
+      #push @search, $new;
+      $search->insert($new, $remaining_keys);
+      $todo++;
+    }
+  }
+  if ($min == 999999999) {
+    die "\nFailed to find any keys out of $expected_keys\n";
+  }
+  print "\n" if DEBUG;
+  return $min;
+}
+
+sub part1 {
+  my ($D) = @_;
+  return find($D, $D->{pos});
+}
+
+sub part2 {
+  my ($D) = @_;
+  for my $off (@{fourNeighbourOffsets()}) {
+    $D->{m}->[$D->{pos}->[Y] + $off->[Y]]->[$D->{pos}->[X] + $off->[X]] = '#';
+  }
+  my $sum = 0;
+  for my $r ([-1,-1, 'A'], [1,-1,'B'], [-1,1,'C'], [1,1,'D']) {
+    my ($ox, $oy, $quad) = @$r;
+    my $x = $ox + $D->{pos}->[X];
+    my $y = $oy + $D->{pos}->[Y];
+    $D->{qk}->{$quad} = find_keys($D, [$x, $y]);
+    print "Quad $quad ($x,$y) has ",
+      (scalar keys %{$D->{qk}->{$quad}}), " keys: ",
+      join '', keys %{$D->{qk}->{$quad}} if DEBUG;
+    $sum += find($D, [$x, $y], $quad);
+  }
+  return $sum;
+}
+
+my $D = $i;
+opt($D);
+print draw($D) if DEBUG;
+my $p1 = part1($D);
+print "Part 1: $p1\n";
+my $p2 = part2($D);
+print "Part 2: $p2\n";
---
+++ 2019/18/main.go
@@ -0,0 +1,311 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"sort"
+	"strings"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+type Rogue struct {
+	m        map[Point]rune
+	bb       *BoundingBox
+	pos      Point
+	doors    int
+	keys     int
+	quadKeys map[rune]map[rune]bool
+	debug    bool
+}
+
+func NewRogue(lines []string) *Rogue {
+	bb := NewBoundingBox()
+	bb.Add(Point{0, 0})
+	bb.Add(Point{len(lines[0]) - 1, len(lines) - 1})
+	m := make(map[Point]rune)
+	pos := Point{-1, -1}
+	doors := 0
+	keys := 0
+	for y, line := range lines {
+		for x, ch := range line {
+			if ch == '@' {
+				pos = Point{x, y}
+				ch = '.'
+			} else if 'a' <= ch && ch <= 'z' {
+				keys++
+			} else if 'A' <= ch && ch <= 'Z' {
+				doors++
+			}
+			m[Point{x, y}] = ch
+		}
+	}
+	return &Rogue{m, bb, pos, doors, keys, make(map[rune]map[rune]bool), false}
+}
+
+func (r *Rogue) String() string {
+	str := ""
+	for y := r.bb.Min.Y; y <= r.bb.Max.Y; y++ {
+		for x := r.bb.Min.X; x <= r.bb.Max.X; x++ {
+			if r.pos.X == x && r.pos.Y == y {
+				str += "@"
+			} else {
+				str += string(r.m[Point{x, y}])
+			}
+		}
+		str += "\n"
+	}
+	return str
+}
+
+func (r *Rogue) Optimaze() {
+	for {
+		changes := 0
+		for y := r.bb.Min.Y; y <= r.bb.Max.Y; y++ {
+			for x := r.bb.Min.X; x <= r.bb.Max.X; x++ {
+				p := Point{x, y}
+				if r.m[p] != '.' {
+					continue
+				}
+				if r.pos.X == x && r.pos.Y == y {
+					continue
+				}
+				nw := 0
+				for _, np := range p.Neighbours() {
+					if r.m[np] == '#' {
+						nw++
+					}
+				}
+				if nw > 2 {
+					r.m[p] = '#'
+					changes++
+				}
+			}
+		}
+		if changes == 0 {
+			break
+		}
+	}
+}
+
+func (r *Rogue) isKeyInQuad(key rune, quad rune) bool {
+	if quad == '*' {
+		return true
+	}
+	_, ok := r.quadKeys[quad][key]
+	return ok
+}
+
+func (r *Rogue) findKeys(pos Point, quad rune) {
+	r.quadKeys[quad] = make(map[rune]bool)
+	visited := make(map[Point]bool)
+	search := []Point{pos}
+	for len(search) > 0 {
+		cur := search[0]
+		search = search[1:]
+		ch := r.m[cur]
+		if ch == '#' {
+			continue
+		}
+		if _, ok := visited[cur]; ok {
+			continue
+		}
+		visited[cur] = true
+		if 'a' <= ch && ch <= 'z' {
+			r.quadKeys[quad][ch] = true
+		}
+		for _, np := range cur.Neighbours() {
+			search = append(search, np)
+		}
+	}
+}
+
+func SortString(w string) string {
+	s := strings.Split(w, "")
+	sort.Strings(s)
+	return strings.Join(s, "")
+}
+
+type SearchRecord struct {
+	pos        Point
+	steps      int
+	remaining  int
+	keys       map[rune]bool
+	path       string
+	sortedPath string
+}
+
+type Search []SearchRecord
+
+type PQ []*SearchRecord
+
+func (pq PQ) Len() int { return len(pq) }
+
+func (pq PQ) Less(i, j int) bool {
+	return pq[i].remaining < pq[j].remaining
+}
+
+func (pq PQ) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+
+func (pq *PQ) Push(x interface{}) {
+	item := x.(*SearchRecord)
+	*pq = append(*pq, item)
+}
+
+func (pq *PQ) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[0 : n-1]
+	return item
+}
+
+func (r *Rogue) find(pos Point, quad rune) int {
+	var expectedKeys int
+	if quad == '*' {
+		expectedKeys = r.keys
+	} else {
+		expectedKeys = len(r.quadKeys[quad])
+	}
+	if r.debug {
+		fmt.Printf("Searching for %d keys in quad %s\n",
+			expectedKeys, string(quad))
+	}
+	visited := make(map[string]int)
+	pq := make(PQ, 1)
+	pq[0] = &SearchRecord{pos, 0,
+		expectedKeys, make(map[rune]bool, expectedKeys),
+		"", ""}
+	min := math.MaxInt32
+	heap.Init(&pq)
+	for pq.Len() > 0 {
+		if r.debug {
+			fmt.Printf("pq len: %d\n", pq.Len())
+		}
+		cur := heap.Pop(&pq).(*SearchRecord)
+		ch := r.m[cur.pos]
+		if r.debug {
+			fmt.Printf("checking %s %s '%s'\n", cur.pos, cur.path, string(ch))
+		}
+		if ch == '#' {
+			continue
+		}
+		if cur.steps > min {
+			if r.debug {
+				fmt.Printf("  too many steps: %d > %d\n", cur.steps, min)
+			}
+			continue
+		}
+		if 'A' <= ch && ch <= 'Z' {
+			lch := ch + 32
+			if _, ok := cur.keys[lch]; !ok &&
+				r.isKeyInQuad(lch, quad) {
+				if r.debug {
+					fmt.Printf("  blocked by door %s\n", string(ch))
+				}
+				continue
+			}
+		} else if 'a' <= ch && ch <= 'z' {
+			if _, ok := cur.keys[ch]; !ok {
+				if r.debug {
+					fmt.Printf("  found key %s (%ds)\n", string(ch), cur.steps)
+				}
+				cur.keys[ch] = true
+				cur.remaining--
+				cur.path += string(ch)
+				cur.sortedPath = SortString(cur.path)
+				if cur.remaining == 0 {
+					if r.debug {
+						fmt.Printf("Found all keys via %s in %d\n",
+							cur.path, cur.steps)
+					}
+					if min > cur.steps {
+						min = cur.steps
+					}
+					continue
+				}
+			}
+		}
+		vkey := fmt.Sprintf("%d!%d!%s",
+			cur.pos.X, cur.pos.Y, cur.sortedPath)
+		if v, ok := visited[vkey]; ok && v <= cur.steps {
+			if r.debug {
+				fmt.Printf("  de ja vu (%s)\n", vkey)
+			}
+			continue
+		}
+		visited[vkey] = cur.steps
+		for _, np := range cur.pos.Neighbours() {
+			if r.m[np] == '#' {
+				continue
+			}
+			newKeys := make(map[rune]bool, len(cur.keys))
+			for k, v := range cur.keys {
+				newKeys[k] = v
+			}
+			new := &SearchRecord{np, cur.steps + 1, cur.remaining, newKeys,
+				cur.path, cur.sortedPath}
+			heap.Push(&pq, new)
+		}
+	}
+	return min
+}
+
+func (r *Rogue) part1() int {
+	return r.find(r.pos, '*')
+}
+
+type QuadRecord struct {
+	startOffset Point
+	name        rune
+}
+
+func (r *Rogue) part2() int {
+	for _, np := range r.pos.Neighbours() {
+		r.m[np] = '#'
+	}
+	if r.debug {
+		fmt.Printf("%s\n", r)
+	}
+	quads := []QuadRecord{
+		QuadRecord{Point{-1, -1}, 'A'},
+		QuadRecord{Point{1, -1}, 'B'},
+		QuadRecord{Point{-1, 1}, 'C'},
+		QuadRecord{Point{1, 1}, 'D'},
+	}
+	sum := 0
+	for _, rec := range quads {
+		start := Point{r.pos.X + rec.startOffset.X,
+			r.pos.Y + rec.startOffset.Y}
+		quad := rec.name
+		r.findKeys(start, quad)
+		if r.debug {
+			fmt.Printf("Quad %s / %s has %d keys\n",
+				string(quad), start, len(r.quadKeys[quad]))
+		}
+		sum += r.find(start, quad)
+	}
+	return sum
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <input.txt>\n", os.Args[0])
+	}
+	lines := ReadLines(os.Args[1])
+	rogue := NewRogue(lines)
+	if rogue.debug {
+		fmt.Printf("%s\n", rogue)
+	}
+	rogue.Optimaze()
+	if rogue.debug {
+		fmt.Printf("%s\n", rogue)
+	}
+	fmt.Printf("Part 1: %d\n", rogue.part1())
+	fmt.Printf("Part 2: %d\n", rogue.part2())
+}
