4f4c7cac92e6f48dae835472d5786cd046b38611
Mark Hindess <soft-github@temporalanomaly.com>
2019-12-25 10:41:11 +0000

    2019 Day 25

---
+++ 2019/25/aoc.pl
@@ -0,0 +1,91 @@
+#!/usr/bin/perl
+use warnings FATAL => 'all';
+use strict;
+use v5.10;
+use lib "../lib";
+use AoC::Helpers qw/:all/;
+use IntCode;
+use Carp::Always qw/carp verbose/;
+use Algorithm::Combinatorics qw(combinations);
+
+my $i = parse_input(read_lines("input.txt"));
+
+sub parse_input {
+  my ($lines) = @_;
+  return [split /,/, $lines->[0]];
+}
+
+my @commands =
+  ("", # hull breach
+   "south", "take fixed point", "north", "west", "west",
+   "west", "take hologram", "east", "north", "west", "east", "east",
+   "west","south","east","east","north",
+   "take candy cane","west", "take antenna",
+   "south", "take whirled peas", "north",
+   "west", "take shell",
+   "east", "east", "north",
+   "north", "take polygon", "south",
+   "west", "take fuel cell","west",
+   "inv",
+  );
+
+my @items =
+  (
+   "hologram","shell","whirled peas","fuel cell",
+   "fixed point","polygon","antenna","candy cane",
+   );
+
+my @combos;
+for my $n (1..@items) {
+  push @combos, combinations(\@items, $n);
+}
+@combos = ["shell", "fixed point", "polygon", "candy cane"];
+
+sub calc {
+  my ($prog) = @_;
+  my $ic = IntCode->new($prog, [$i]);
+  my $auto;
+  while (!$ic->{done}) {
+    my $rc = $ic->run();
+    if ($rc == 0) {
+      my $s = "";
+      while (@{$ic->{o}}) {
+        my $ch = shift @{$ic->{o}};
+        $s .= chr($ch);
+      }
+      print $s;
+      if ($s =~ /Analysis complete/) {
+        exit;
+      }
+    } elsif ($rc == 2) {
+      if ($auto) {
+        my $comb = shift @combos;
+        my $inp = "";
+        for my $i (@items) {
+          $inp .= "drop $i\n";
+        }
+        for my $i (@$comb) {
+          $inp .= "take $i\n";
+        }
+        $inp .= "west\n";
+        push @{$ic->{i}}, map { ord $_ } split//, $inp;
+      } else {
+        my $inp;
+        if (@commands) {
+          $inp = shift @commands;
+          $inp .= "\n";
+        } else {
+          $inp = <>;
+        }
+        push @{$ic->{i}}, map { ord $_ } split//, $inp;
+        if ($inp eq "inv\n") {
+          $auto = 1;
+        }
+      }
+    }
+  }
+  return -1;
+}
+
+my $part1 = calc($i);
+print "Part 1: ", $part1, "\n";
---
+++ 2019/25/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+
+	"github.com/mxschmitt/golang-combinations"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+func part1(prog []int) int {
+	ic := NewIntCode(prog, []int{})
+	inv := []string{}
+	bad := make(map[string]bool, 5)
+	for _, b := range []string{"giant electromagnet", "infinite loop",
+		"photons", "escape pod", "molten lava"} {
+		bad[b] = true
+	}
+	combos := [][]string{}
+	lastDir := ""
+	for {
+		s := ""
+		rc := ic.Run()
+		if rc == 2 || rc == 1 {
+			for {
+				out := ic.Out(1)
+				if len(out) != 1 {
+					break
+				}
+				s += string(rune(out[0]))
+			}
+			//fmt.Print(s)
+			if strings.Contains(s, "Analysis complete") {
+				//fmt.Print(s)
+				ints := SimpleReadInts(s)
+				return ints[0]
+			}
+			if len(combos) > 0 {
+				//fmt.Print(s)
+				combo := combos[0]
+				combos = combos[1:]
+				//fmt.Printf("Trying combo %v\n", combo)
+				for _, item := range inv {
+					ic.InStr("drop " + item + "\n")
+				}
+				for _, item := range combo {
+					ic.InStr("take " + item + "\n")
+				}
+				// ic.InStr("inv\n")
+				ic.InStr(lastDir + "\n")
+				continue
+			}
+			if strings.Contains(s, "eject") {
+				//fmt.Printf("Found exit\n")
+				if len(inv) == 8 {
+					//fmt.Printf("Have all items!\n")
+					combos = combinations.All(inv)
+					continue
+				} else {
+					//fmt.Printf("Need more items (%v)!\n", inv)
+				}
+			}
+			ii := strings.Index(s, "Items")
+			if ii != -1 {
+				ii += 12
+				for {
+					if s[ii] != '-' {
+						break
+					}
+					ii += 2
+					eol := strings.Index(s[ii:], "\n")
+					item := s[ii : ii+eol]
+					ii += eol
+					if _, ok := bad[item]; !ok {
+						ic.InStr("take " + item + "\n")
+						//fmt.Printf("picked up %s\n", item)
+						inv = append(inv, item)
+					} else {
+						//fmt.Printf("Ignoring %s\n", item)
+					}
+				}
+			}
+			dirIndex := strings.LastIndex(s, "Doors here lead")
+			dirStr := s[dirIndex:]
+			dir := []string{}
+			for _, d := range []string{"north", "south", "east", "west"} {
+				if strings.Contains(dirStr, d) {
+					dir = append(dir, d)
+				}
+			}
+			n := rand.Int() % len(dir)
+			lastDir = dir[n]
+			//fmt.Printf("Sending '%s'\n", lastDir)
+			ic.InStr(lastDir + "\n")
+			s = ""
+		}
+	}
+	return -1
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <input.txt>\n", os.Args[0])
+	}
+	prog := SimpleReadInts(ReadLines(os.Args[1])[0])
+	fmt.Printf("Part 1: %d\n", part1(prog))
+}
