44063e92bd41795b2b8c45df1bff4467dc837fc0
Mark Hindess <soft-github@temporalanomaly.com>
2020-12-13 11:15:05 +0000

    2020 Day 11: Testing wasting memory in Go

--- 2020/11/main.go
+++ 2020/11/main.go
@@ -19,30 +19,48 @@ const (
 
 type Seats []Seat
 
+type Neighbours []int
+
 type Map struct {
-	cur   Seats
-	new   Seats
-	w, h  int
-	debug bool
+	cur    Seats
+	new    Seats
+	index  []int
+	cache1 map[int]Neighbours
+	cache2 map[int]Neighbours
+	w, h   int
+	debug  bool
 }
 
 func NewMap(lines []string) *Map {
 	h := len(lines)
 	w := len(lines[0])
-	m := make(Seats, h*w)
-	nm := make(Seats, h*w)
+	cur := make(Seats, h*w)
+	new := make(Seats, h*w)
+	index := make([]int, 0, h*w)
 	for y, line := range lines {
 		for x, s := range line {
+			i := y*w + x
 			if s == 'L' {
-				m[y*w+x] = Empty
+				cur[i] = Empty
 			} else if s == '#' {
-				m[y*w+x] = Occupied
+				cur[i] = Occupied
 			} else {
-				m[y*w+x] = None
+				cur[i] = None
+				continue
 			}
+			index = append(index, i)
 		}
 	}
-	return &Map{m, nm, w, h, DEBUG()}
+	cache1 := make(map[int]Neighbours, len(index))
+	cache2 := make(map[int]Neighbours, len(index))
+	m := &Map{cur, new, index, cache1, cache2, w, h, DEBUG()}
+	for _, i := range m.index {
+		x := i % w
+		y := i / w
+		cache1[i] = m.Neighbours(x, y, false)
+		cache2[i] = m.Neighbours(x, y, true)
+	}
+	return m
 }
 
 func (m *Map) String() string {
@@ -66,6 +84,10 @@ func (m *Map) SetSeat(x, y int, state Seat) {
 	m.new[y*m.w+x] = state
 }
 
+func (m *Map) SetSeatByIndex(i int, state Seat) {
+	m.new[i] = state
+}
+
 func (m *Map) Swap() {
 	m.cur, m.new = m.new, m.cur
 }
@@ -77,8 +99,12 @@ func (m *Map) Seat(x, y int) Seat {
 	return NoneOutside
 }
 
-func (m *Map) Next(cur Seat, x, y int, group int, sight bool) Seat {
-	oc := 0
+func (m *Map) SeatByIndex(i int) Seat {
+	return m.cur[i]
+}
+
+func (m *Map) Neighbours(x, y int, sight bool) Neighbours {
+	n := make(Neighbours, 0, 8)
 	for _, o := range EightNeighbourOffsets {
 		ox, oy := x, y
 		s := None
@@ -89,44 +115,57 @@ func (m *Map) Next(cur Seat, x, y int, group int, sight bool) Seat {
 				break
 			}
 		}
+		if s == Empty || s == Occupied {
+			n = append(n, ox+m.w*oy)
+		}
+	}
+	return n
+}
+
+func (m *Map) OccupiedCount(i int, sight bool) int {
+	oc := 0
+	var cache map[int]Neighbours
+	if sight {
+		cache = m.cache2
+	} else {
+		cache = m.cache1
+	}
+	for _, i := range cache[i] {
+		s := m.SeatByIndex(i)
 		if s == Occupied {
 			oc++
 		}
 	}
-	if cur == Empty && oc == 0 {
-		return Occupied
-	} else if cur == Occupied && oc >= group {
-		return Empty
-	}
-	return cur
+	return oc
 }
 
 func (m *Map) Run(group int, sight bool) int {
 	for {
 		ch := 0
-		oc := 0
-		for x := 0; x < m.w; x++ {
-			for y := 0; y < m.h; y++ {
-				cur := m.Seat(x, y)
-				if cur == None {
-					continue
-				}
-				newSeat := m.Next(cur, x, y, group, sight)
-				if cur != newSeat {
-					ch++
-				}
-				m.SetSeat(x, y, newSeat)
-				if newSeat == Occupied {
-					oc++
-				}
+		c := 0
+		for _, i := range m.index {
+			cur := m.SeatByIndex(i)
+			oc := m.OccupiedCount(i, sight)
+			newSeat := cur
+			if cur == Empty && oc == 0 {
+				newSeat = Occupied
+			} else if cur == Occupied && oc >= group {
+				newSeat = Empty
+			}
+			if cur != newSeat {
+				ch++
+			}
+			m.SetSeatByIndex(i, newSeat)
+			if newSeat == Occupied {
+				c++
 			}
 		}
 		if m.debug {
 			fmt.Printf("%s", m)
-			fmt.Printf("changes=%d oc=%d\n", ch, oc)
+			fmt.Printf("changes=%d oc=%d\n", ch, c)
 		}
 		if ch == 0 {
-			return oc
+			return c
 		}
 		m.Swap()
 	}
