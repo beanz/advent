19408b4f1823bc6b5f9816799c127b6beb94c522
Mark Hindess <soft-github@temporalanomaly.com>
2020-12-19 06:19:00 +0000

    2020 Day 19: Perl and Go

---
+++ 2020/19/aoc.pl
@@ -0,0 +1,118 @@
+#!/usr/bin/perl
+use warnings FATAL => 'all';
+use strict;
+use v5.10;
+use lib "../lib";
+no warnings 'portable';
+use AoC::Helpers qw/:all/;
+use Carp::Always qw/carp verbose/;
+$; = $" = ',';
+
+my $file = shift // "input.txt";
+my $i = read_chunks($file);
+my $i2 = read_chunks($file);
+
+sub make_re {
+  my ($rs, $n) = @_;
+  #print "make_re: $n\n";
+  my $r = $rs->{$n};
+  if (ref $r) {
+    return
+      '(?:'.(join'|',
+       map {
+          (join '',map { make_re($rs, $_) } @$_ )
+       } @$r).')';
+  } else {
+    #print "ch $r\n";
+    return $r;
+  }
+}
+
+sub parse_rules {
+  my ($in) = @_;
+  my %r;
+  my @v;
+  my $re;
+  for my $l (@$in) {
+    if ($l =~ s/^(\d+): //) {
+      my $n = $1;
+      if ($l =~ /"(.)"/) {
+        $r{$n} = $1;
+      } else {
+        $r{$n} = [map { [split / /, $_] } split / \| /, $l];
+      }
+    }
+  }
+  return \%r;
+}
+
+sub calc {
+  my ($in) = @_;
+  my $t = 0;
+  my $r = parse_rules([split/\n/,$in->[0]]);
+  my $re = '^'.make_re($r, 0).'$';
+  #print "made $re\n";
+  my @v = split/\n/,$in->[1];
+  #dd([$r, \@v],[qw/r v/]);
+  for my $v (@v) {
+    #print "checking $v\n";
+    $t++ if ($v =~ /$re/);
+  }
+  return $t;
+}
+
+sub calc2 {
+  my ($in) = @_;
+  my $t = 0;
+  my $r = parse_rules([split/\n/,$in->[0]]);
+  my $re42 = make_re($r, 42);
+  my $re31 = make_re($r, 31);
+  #print "re42 = $re42\n";
+  #print "re31 = $re31\n";
+  $r->{8} = make_re($r, 42).'+';
+  $r->{11} = '(?:'.(join'|', map "$re42\{$_}$re31\{$_}", (1..40)).')';
+  my $re = '^'.make_re($r, 0).'$';
+  #print "made $re\n";
+  my @v = split/\n/,$in->[1];
+  #dd([$r, \@v],[qw/r v/]);
+  for my $v (@v) {
+    #print "checking $v\n";
+    if ($v =~ /$re/) {
+      $t++
+    }
+  }
+  return $t;
+}
+
+testPart1() if (TEST);
+
+print "Part 1: ", calc($i), "\n";
+
+testPart2() if (TEST);
+
+print "Part 2: ", calc2($i2), "\n";
+
+sub testPart1 {
+  my @test_cases =
+    (
+     [ "test0.txt", 1 ],
+     [ "test1.txt", 1 ],
+     [ "test2.txt", 2 ],
+     [ "input.txt", 285 ],
+    );
+  for my $tc (@test_cases) {
+    my $res = calc(read_chunks($tc->[0]));
+    assertEq("Test 1 [$tc->[0]]", $res, $tc->[1]);
+  }
+}
+
+sub testPart2 {
+  my @test_cases =
+    (
+     [ "input.txt", 412 ],
+    );
+  for my $tc (@test_cases) {
+    my $res = calc2(read_chunks($tc->[0]));
+    assertEq("Test 2 [$tc->[0]]", $res, $tc->[1]);
+  }
+}
---
+++ 2020/19/main.go
@@ -0,0 +1,134 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+type Rule struct {
+	ch string
+	or [][]int
+}
+
+func (r Rule) String() string {
+	if r.ch == "" {
+		return fmt.Sprintf("%v", r.or)
+	}
+	return r.ch
+}
+
+type Matcher struct {
+	rules  map[int]Rule
+	values []string
+	debug  bool
+}
+
+func NewMatcher(in []string) *Matcher {
+	rs := strings.Split(in[0], "\n")
+	r := make(map[int]Rule, len(rs))
+	for _, l := range rs {
+		s := strings.Split(l, ": ")
+		n, err := strconv.Atoi(s[0])
+		if err != nil {
+			log.Fatalf("invalid rule %s\n", l)
+		}
+		if s[1][0] == '"' {
+			r[n] = Rule{ch: string(s[1][1])}
+		} else {
+			os := strings.Split(s[1], " | ")
+			or := make([][]int, len(os))
+			for i := 0; i < len(os); i++ {
+				opt := SimpleReadInts(os[i])
+				or[i] = opt
+			}
+			r[n] = Rule{or: or}
+		}
+	}
+	return &Matcher{r, strings.Split(in[1], "\n"), true}
+}
+
+func (m *Matcher) String() string {
+	s := ""
+	for k, v := range m.rules {
+		s += fmt.Sprintf("R[%d]: %s\n", k, v)
+	}
+	for _, v := range m.values {
+		s += fmt.Sprintf("V: %s\n", v)
+	}
+	return s
+}
+
+func (m *Matcher) Regexp(i int) string {
+	r := m.rules[i]
+	if r.ch != "" {
+		// ch rule
+		return string(r.ch)
+	}
+
+	// or rule
+	res := "(?:"
+	os := make([]string, len(r.or))
+	for i, o := range r.or {
+		s := ""
+		for _, rn := range o {
+			s += m.Regexp(rn)
+		}
+		os[i] = s
+	}
+	res += strings.Join(os, "|")
+	res += ")"
+	return res
+}
+
+func (m *Matcher) Part1() int {
+	//fmt.Printf("%s\n", m)
+	re := "^" + m.Regexp(0) + "$"
+	//fmt.Printf("RE[0]: %s\n", re)
+	c := 0
+	cre := regexp.MustCompile(re)
+	for _, v := range m.values {
+		if cre.MatchString(v) {
+			//fmt.Printf("%s matched\n", v)
+			c++
+		}
+	}
+	return c
+}
+
+func (m *Matcher) Part2() int {
+	re31 := m.Regexp(31)
+	re42 := m.Regexp(42)
+	m.rules[8] = Rule{ch: re42 + "+"}
+	ns := make([]string, 40)
+	for i := 0; i < 40; i++ {
+		n := fmt.Sprintf("{%d}", i+1)
+		ns[i] = re42 + n + re31 + n
+	}
+	m.rules[11] = Rule{ch: "(?:" + strings.Join(ns, "|") + ")"}
+	re := "^" + m.Regexp(0) + "$"
+	//fmt.Printf("RE[0]: %s\n", re)
+	c := 0
+	cre := regexp.MustCompile(re)
+	for _, v := range m.values {
+		if cre.MatchString(v) {
+			//fmt.Printf("%s matched\n", v)
+			c++
+		}
+	}
+	return c
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Usage: %s <input.txt>\n", os.Args[0])
+	}
+	chunks := ReadChunks(os.Args[1])
+	fmt.Printf("Part 1: %d\n", NewMatcher(chunks).Part1())
+	fmt.Printf("Part 2: %d\n", NewMatcher(chunks).Part2())
+}
