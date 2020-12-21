09496c65c4cfd0031427b4e6cda3c3534b8042f8
Mark Hindess <soft-github@temporalanomaly.com>
2020-12-21 16:22:53 +0000

    2020 Add template

---
+++ 2020/template/main_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+
+	. "github.com/beanz/advent-of-code-go"
+)
+
+type TestCase struct {
+	file string
+	ans  int
+}
+
+func TestPart1(t *testing.T) {
+	tests := []TestCase{
+		{"test0.txt", 1},
+		{"test1.txt", 1},
+		{"test2.txt", 2},
+		{"input.txt", 285},
+	}
+	for _, tc := range tests {
+		assert.Equal(t, tc.ans, NewMatcher(ReadChunks(tc.file)).Part1(), tc.file)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []TestCase{
+		{"input.txt", 412},
+	}
+	for _, tc := range tests {
+		assert.Equal(t, tc.ans, NewMatcher(ReadChunks(tc.file)).Part2(), tc.file)
+	}
+}
