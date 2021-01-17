718cb2c7afaefbd5089420b207ade5965cdd4053
Mark Hindess <soft-github@temporalanomaly.com>
2021-01-17 11:40:44 +0000

    Day 2015 Day 8 Rust

---
+++ aoc-rust/src/bin/aoc-2015-08.rs
@@ -0,0 +1,52 @@
+pub fn calc(s: &str) -> (i32, i32) {
+    let mut c1: i32 = -2;
+    let mut c2: i32 = 2;
+    let chs: Vec<char> = s.chars().collect();
+    let mut i: usize = 0;
+    while i < s.len() {
+        let ch = chs[i];
+        c1 += 1;
+        if ch == '\\' {
+            let esc = chs[i + 1];
+            match esc {
+                '\\' | '"' => {
+                    i += 1;
+                    c2 += 2;
+                }
+                'x' => {
+                    i += 3;
+                    c2 += 1;
+                }
+                _ => panic!("Invalid escape {}", esc),
+            }
+        } else if ch == '"' {
+            c2 += 1;
+        }
+        i += 1;
+    }
+    (s.len() as i32 - c1, c2)
+}
+
+fn main() {
+    let inp = aoc::read_input_lines().map(|l| l.unwrap());
+    let (p1, p2) = inp
+        .map(|x| calc(&x))
+        .fold((0, 0), |(a1, a2), (b1, b2)| (a1 + b1, a2 + b2));
+    println!("Part 1: {}", p1);
+    println!("Part 2: {}", p2);
+}
+
+#[test]
+fn calc_works() {
+    for &(inp, exp) in [
+        ("\"\"", (2 - 0, 6 - 2)),
+        ("\"abc\"", (5 - 3, 9 - 5)),
+        ("\"aaa\\\"aaa\"", (10 - 7, 16 - 10)),
+        ("\"\\x27\"", (6 - 1, 11 - 6)),
+        ("\"h\\\\\"", (5 - 2, 11 - 5)),
+    ]
+    .iter()
+    {
+        assert_eq!(calc(&inp.to_string()), exp, "{}", inp);
+    }
+}
