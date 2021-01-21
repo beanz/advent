cd53bd2466d8f17f43decc1b94322832ff615e42
Mark Hindess <soft-github@temporalanomaly.com>
2021-01-21 23:05:53 +0000

    Day 2016 Day 6 Rust

---
+++ aoc-rust/src/bin/aoc-2016-06.rs
@@ -0,0 +1,50 @@
+use itertools::Itertools;
+
+fn calc(inp: &[String], mul: isize) -> String {
+    (0..inp[0].len())
+        .map(|i| {
+            let chs = inp.iter().map(|l| l.chars().nth(i).unwrap());
+            chs.clone()
+                .unique()
+                .max_by_key(|x| {
+                    chs.clone().filter(|y| y == x).count() as isize * mul
+                })
+                .unwrap()
+        })
+        .collect::<String>()
+}
+
+fn part1(inp: &[String]) -> String {
+    calc(inp, 1)
+}
+
+fn part2(inp: &[String]) -> String {
+    calc(inp, -1)
+}
+
+fn main() {
+    let inp = aoc::input_lines();
+    println!("Part 1: {}", part1(&inp));
+    println!("Part 2: {}", part2(&inp));
+}
+
+#[allow(dead_code)]
+const EX: [&str; 16] = [
+    "eedadn", "drvtee", "eandsr", "raavrd", "atevrs", "tsrnev", "sdttsa",
+    "rasrtv", "nssdts", "ntnada", "svetve", "tesnvt", "vntsnd", "vrdear",
+    "dvrsen", "enarar",
+];
+
+#[test]
+fn part1_works() {
+    let e: Vec<String> =
+        EX.iter().map(|x| x.to_string()).collect::<Vec<String>>();
+    assert_eq!(part1(&e), "easter", "part 1 example");
+}
+
+#[test]
+fn part2_works() {
+    let e: Vec<String> =
+        EX.iter().map(|x| x.to_string()).collect::<Vec<String>>();
+    assert_eq!(part2(&e), "advent", "part 2 example");
+}
