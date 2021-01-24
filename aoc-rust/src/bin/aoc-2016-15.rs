c4bd39e4b3438417436f9b878766c2ec2d4d1842
Mark Hindess <soft-github@temporalanomaly.com>
2021-01-24 16:28:19 +0000

    2016 Day 15 Rust (w/o Chinese Remainder Theorem)

---
+++ aoc-rust/src/bin/aoc-2016-15.rs
@@ -0,0 +1,82 @@
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct Disc {
+    num: usize,
+    positions: usize,
+    start: usize,
+}
+
+impl Disc {
+    fn new(l: &str) -> Disc {
+        let mut nums = aoc::ints::<usize>(l);
+        Disc {
+            num: nums.next().unwrap(),
+            positions: nums.next().unwrap(),
+            start: nums.nth(1).unwrap(),
+        }
+    }
+    fn is_aligned(&self, t: usize) -> bool {
+        ((t + self.num + self.start) % self.positions) == 0
+    }
+}
+
+#[allow(dead_code)]
+const EX: [&str; 2] = [
+    "Disc #1 has 5 positions; at time=0, it is at position 4.",
+    "Disc #2 has 2 positions; at time=0, it is at position 1.",
+];
+
+#[test]
+fn disc_is_aligned_works() {
+    let d1 = Disc::new(EX[0]);
+    assert_eq!(d1.num, 1, "disc 1 number");
+    assert_eq!(d1.positions, 5, "disc 1 positions");
+    assert_eq!(d1.start, 4, "disc 1 start");
+    assert_eq!(d1.is_aligned(0), true, "disc 1 aligned at t=0");
+    assert_eq!(d1.is_aligned(1), false, "disc 1 not aligned at t=1");
+    assert_eq!(d1.is_aligned(5), true, "disc 1 aligned at t=5");
+
+    let d2 = Disc::new(EX[1]);
+    assert_eq!(d2.num, 2, "disc 2 number");
+    assert_eq!(d2.positions, 2, "disc 2 positions");
+    assert_eq!(d2.start, 1, "disc 2 start");
+
+    assert_eq!(d2.is_aligned(0), false, "disc 2 not aligned at t=0");
+    assert_eq!(d2.is_aligned(1), true, "disc 2 aligned at t=1");
+    assert_eq!(d2.is_aligned(5), true, "disc 2 aligned at t=5");
+}
+
+fn calc(discs: &[Disc]) -> usize {
+    (0_usize..)
+        .find(|t| discs.iter().all(|d| d.is_aligned(*t)))
+        .unwrap()
+}
+
+fn part1(discs: &[Disc]) -> usize {
+    calc(discs)
+}
+
+fn part2(discs: &[Disc]) -> usize {
+    let mut mdiscs = discs.to_owned();
+    mdiscs.push(Disc {
+        num: 7,
+        positions: 11,
+        start: 0,
+    });
+    calc(&mdiscs)
+}
+
+#[test]
+fn calc_parts() {
+    let discs = EX.iter().map(|l| Disc::new(l)).collect::<Vec<Disc>>();
+    assert_eq!(part1(&discs), 5, "example part 1");
+    assert_eq!(part2(&discs), 15, "example part 2");
+}
+
+fn main() {
+    let discs = aoc::input_lines()
+        .iter()
+        .map(|l| Disc::new(l))
+        .collect::<Vec<Disc>>();
+    println!("Part 1: {}", part1(&discs));
+    println!("Part 2: {}", part2(&discs));
+}
