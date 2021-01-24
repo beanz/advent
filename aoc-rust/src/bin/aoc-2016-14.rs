e7e362aa73dba3c52ee3e1702ef64a6c562a17b7
Mark Hindess <soft-github@temporalanomaly.com>
2021-01-24 14:23:38 +0000

    2016 Day 14 Rust (complete but slow)

--- aoc-rust/src/bin/aoc-2016-14.rs
+++ aoc-rust/src/bin/aoc-2016-14.rs
@@ -1,9 +1,13 @@
 use crypto::digest::Digest;
 use crypto::md5::Md5;
+use itertools::Itertools;
 
-struct Md5er {
-    ns: aoc::NumStr,
-    md5: Md5,
+fn md5sum(b: &[u8]) -> Box<[u8; 16]> {
+    let mut md5 = Md5::new();
+    md5.input(b);
+    let mut cs = [0; 16];
+    md5.result(&mut cs);
+    Box::new(cs)
 }
 
 struct Md5erResult {
@@ -11,8 +15,8 @@ struct Md5erResult {
 }
 
 impl Md5erResult {
-    fn new(cs: [u8; 16]) -> Md5erResult {
-        Md5erResult { cs: Box::new(cs) }
+    fn new(cs: Box<[u8; 16]>) -> Md5erResult {
+        Md5erResult { cs }
     }
     fn string(&self) -> String {
         self.cs
@@ -22,11 +26,14 @@ impl Md5erResult {
     }
 }
 
+struct Md5er {
+    ns: aoc::NumStr,
+}
+
 impl Md5er {
-    fn new(s: &String) -> Md5er {
+    fn new(s: &str) -> Md5er {
         Md5er {
             ns: aoc::NumStr::new(s.to_string()),
-            md5: Md5::new(),
         }
     }
 }
@@ -35,10 +42,7 @@ impl Iterator for Md5er {
     type Item = Md5erResult;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.md5.input(self.ns.bytes());
-        let mut cs = [0; 16];
-        self.md5.result(&mut cs);
-        self.md5.reset();
+        let cs = md5sum(self.ns.bytes());
         self.ns.inc();
         Some(Md5erResult::new(cs))
     }
@@ -51,30 +55,99 @@ fn md5er_works() {
         md5er.next().unwrap().string(),
         "577571be4de9dcce85a041ba0410f29f"
     );
-    let mut c = 0;
-    for (i, md5) in md5er.enumerate() {
-        println!(i, md5.string());
-        c += 1;
-        if c > 10 {
-            break;
+}
+
+struct StretchedMd5er {
+    ns: aoc::NumStr,
+}
+
+impl StretchedMd5er {
+    fn new(s: &str) -> StretchedMd5er {
+        StretchedMd5er {
+            ns: aoc::NumStr::new(s.to_string()),
         }
     }
 }
 
-fn main() {
-    let md5er = Md5er::new(&"abc".to_string());
+impl Iterator for StretchedMd5er {
+    type Item = Md5erResult;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut cs = md5sum(self.ns.bytes());
+        for _n in 1..2017 {
+            cs = md5sum(
+                &cs.iter()
+                    .map(|x| format!("{:02x}", x))
+                    .collect::<String>()
+                    .as_bytes(),
+            )
+        }
+        self.ns.inc();
+        Some(Md5erResult::new(cs))
+    }
+}
+
+#[test]
+fn stretched_md5er_works() {
+    let mut md5er = StretchedMd5er::new(&"abc".to_string());
+    assert_eq!(
+        md5er.next().unwrap().string(),
+        "a107ff634856bb300138cac6568c0f24"
+    );
+}
+
+fn find_key<I>(md5er: I) -> usize
+where
+    I: Iterator<Item = Md5erResult>,
+{
     let mut c = 0;
-    for (i, md5) in md5er.enumerate().filter(|(_, md5)| {
-        md5.string()
-            .bytes()
-            .collect::<Vec<u8>>()
-            .windows(3)
-            .any(|c| c[0] == c[1] && c[1] == c[2])
-    }) {
-        println!("{:02}: {}", i, md5.string());
-        c += 1;
-        if c > 10 {
-            break;
+    let mut it = md5er.enumerate().multipeek();
+    loop {
+        let (i, md5) = it.next().unwrap();
+        let s = md5.string().bytes().collect::<Vec<u8>>();
+        let triple = s.windows(3).find(|c| c[0] == c[1] && c[1] == c[2]);
+        if triple == None {
+            continue;
+        }
+        let ch = triple.unwrap()[0];
+        //println!("{}: {} {}", i, ch as char, md5.string());
+        it.reset_peek();
+        for _n in 1..1000 {
+            let (_, nmd5) = it.peek().unwrap();
+            let has_5 =
+                nmd5.string().bytes().collect::<Vec<u8>>().windows(5).any(
+                    |c| {
+                        c[0] == ch
+                            && c[1] == ch
+                            && c[2] == ch
+                            && c[3] == ch
+                            && c[4] == ch
+                    },
+                );
+            if has_5 {
+                //println!("!! {} {}: {}", i, ch as char, nmd5.string());
+                c += 1;
+                if c == 64 {
+                    return i;
+                }
+                break;
+            }
         }
     }
+}
+
+fn part1(salt: &str) -> usize {
+    let md5er = Md5er::new(salt);
+    find_key(md5er)
+}
+
+fn part2(salt: &str) -> usize {
+    let md5er = StretchedMd5er::new(salt);
+    find_key(md5er)
+}
+
+fn main() {
+    let salt = aoc::read_input_line();
+    println!("Part 1: {}", part1(&salt));
+    println!("Part 2: {}", part2(&salt));
 }
