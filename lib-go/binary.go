da4072f09d657aeefa6beb94235e1917ece79743
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-03 07:35:39 +0000

    Go helpers for binary strings

---
+++ lib-go/binary.go
@@ -0,0 +1,18 @@
+package aoc
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func IntToBin(n int) string {
+	return strconv.FormatInt(int64(n), 2)
+}
+
+func BinToInt(s string) int {
+	i, err := strconv.ParseInt(s, 2, 32)
+	if err != nil {
+		panic(fmt.Sprintf("invalid binary int %d: %s", s, err))
+	}
+	return int(i)
+}
