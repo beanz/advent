054108b6e8902f81e99267df993df18a88c542ba
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-04 12:51:55 +0000

    chore: ignore future days

--- bin/missing.sh
+++ bin/missing.sh
@@ -2,6 +2,9 @@
 
 F="aoc.pl cpp/aoc.cpp aoc.zig aoc.cr aoc.nim main.go"
 for d in ????/?? ; do
+  if [ ! -e $d/input.txt ]; then
+    continue
+  fi
   for f in $F; do
     test -f $d/$f || echo $d/$f missing
   done
