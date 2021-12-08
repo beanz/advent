10295cffb78350bb5fec226a0768f598fa9f3705
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-08 16:11:31 +0000

    2021 Day 8 Smarter Perl

--- 2021/08/aoc.pl
+++ 2021/08/aoc.pl
@@ -6,7 +6,6 @@ use lib "../../lib-perl";
 no warnings 'portable';
 use AoC::Helpers qw/:all/;
 #use Carp::Always qw/carp verbose/;
-use Algorithm::Combinatorics qw/permutations/;
 $; = $" = ',';
 
 my $file = shift // "input.txt";
@@ -14,25 +13,29 @@ my $reader = \&read_stuff;
 my $i = $reader->($file);
 my $i2 = $reader->($file);
 
+sub l2i {
+  2**((ord $_[0]) - (ord 'a'))
+}
 
-sub canon {
-  join "", sort split//, $_[0]
+sub s2i {
+  my $r = 0;
+  $r |= l2i($_) for (split//, $_[0]);
+  $r
+}
+
+sub i2s {
+  my $r = "";
+  for my $i (0..6) {
+    $r .= chr((ord'a')+$i) if (($_[0]&(2**$i))!=0);
+  }
+  return $r;
 }
 
 my @digits = qw/abcefg cf acdeg acdfg bcdf abdfg abdefg acf abcdefg abcdfg/;
 my %digitm;
 for my $j (0..@digits-1) {
-  $digitm{canon($digits[$j])} = $j;
+  $digitm{s2i($digits[$j])} = $j;
 }
-#dd([\%digitm],[qw/perms/]); exit;
-
-my @n = split //, "abcdefg";
-my $iter = permutations([split //, "abcdefg"]);
-my @perms;
-while (my $p = $iter->next) {
-  push @perms, { zip(@n, @$p) };
-}
-#dd([\@perms],[qw/perms/]); exit;
 
 sub read_stuff {
   my ($file) = @_;
@@ -41,23 +44,22 @@ sub read_stuff {
   for my $i (0..@$lines-1) {
     my $l = $lines->[$i];
     my @s = split / \| /, $l;
-    my $r =
-      {
-       sp => [split/ /, $s[0]], d => [split/ /,$s[1]],
-       line => $l, lineno => $i,
-      };
+    my @p = split/ /, $s[0];
+    my @o = split/ /,$s[1];
+    my $r = { line => $l, lineno => $i, };
     push @i, $r;
-    my %m;
-    for my $w (@{$r->{sp}}) {
-      my $s = join"", sort split //, $w;
-      push @{$r->{sps}}, $s;
-      $m{$s}++;
+    my @pi = map { s2i($_) } @p;
+    $r->{oi} = [map { s2i($_) } @o];
+    my %oc;
+    for my $i (0..@o-1) {
+      $oc{length($o[$i])}++;
     }
-    for my $w (@{$r->{d}}) {
-      my $s = join"", sort split //, $w;
-      push @{$r->{ds}}, $s;
+    $r->{oc} = \%oc;
+    my %pm = ();
+    for my $i (0..@p-1) {
+      push @{$pm{length($p[$i])}}, $pi[$i];
     }
-    $r->{m} = \%m;
+    $r->{pm} = \%pm;
   }
   return \@i;
 }
@@ -65,40 +67,64 @@ sub read_stuff {
 sub calc {
   my ($in) = @_;
   my $c = 0;
-  my %d = map { (length $digits[$_]) => $_ } (1, 4, 7, 8);
-  for my $r (@$in) {
-    for my $w (@{$r->{d}}) {
-      $c++ if (exists $d{length $w});
-    }
+  return sum(map { my $r = $_; sum(map { $r->{oc}->{$_}//0 } 2,3,4,7) } @$in);
+}
+
+sub pretty_perm {
+  my ($p) = @_;
+  return join(", ", sort map { i2s($_).">".i2s($p->{$_}) } keys %$p);
+}
+
+sub perm_for {
+  my ($lm) = @_;
+  my %m;
+  $m{cf} = $lm->{2}->[0]; # 1
+  $m{acf} = $lm->{3}->[0]; # 7
+  $m{bcdf} = $lm->{4}->[0]; # 4
+  $m{abcdefg} = $lm->{7}->[0]; # 8
+  $m{a} = $m{acf} ^ $m{cf};
+  $m{abfg} = ($lm->{6}->[0]^$lm->{6}->[1]^$lm->{6}->[2]);
+  $m{cde} = $m{abcdefg}^$m{abfg};
+  $m{c} = $m{cde}&$m{cf};
+  $m{f} = $m{c}^$m{cf};
+  $m{bg} = $m{abfg}^($m{f}|$m{a});
+  $m{b} = $m{bcdf}&$m{bg};
+  $m{g} = $m{b}^$m{bg};
+  $m{de} = $m{cde}^$m{c};
+  $m{d} = $m{bcdf}&$m{de};
+  $m{e} = $m{d}^$m{de};
+
+  my %p;
+  for my $k (sort keys %m) {
+    printf("%7s: %07b\n", $k, $m{$k}) if DEBUG;
+    $p{$m{$k}} = s2i($k)  if (length $k == 1);
   }
-  return $c;
+  return \%p;
+}
+
+sub apply_perm {
+  my ($p, $v) = @_;
+  my $r = 0;
+  for my $i (0..6) {
+    my $bit = 2**$i;
+    $r |= $p->{$bit} if (($v&$bit)!=0);
+  }
+  return $r;
 }
 
 sub calc2 {
   my ($in) = @_;
-  my %d = map { (length $digits[$_]) => $_ } (1, 4, 7, 8);
   my $c = 0;
- LINE:
   for my $r (@$in) {
-  PERM:
-    for my $p (@perms) {
-      my $ps = (join "", keys %$p);
-      for my $w (@{$r->{sps}}) {
-        #print "  $w / $ps => ";
-        my $nw = join "", sort map { $p->{$_} } split //, $w;
-        #print "$nw\n";
-        next PERM unless (exists $digitm{$nw});
-      }
-      #print "F: $ps\n";
-      my $n;
-      for my $d (@{$r->{ds}}) {
-        my $nd = join "", sort map { $p->{$_} } split //, $d;
-        $n .= $digitm{$nd} // die "Invalid $d for $ps\n";
-      }
-      $c += $n;
-      next LINE;
+    my $p = perm_for($r->{pm});
+    print "$r->{line}: ", pretty_perm($p), "\n" if DEBUG;
+    my $n = 0;
+    for my $d (@{$r->{oi}}) {
+      my $nd = apply_perm($p, $d);
+      print $nd, " ", i2s($nd), "\n" if DEBUG;
+      $n = $n * 10 + $digitm{$nd} // die "Invalid $d\n";
     }
-    die "Failed to find perm for $r->{lineno}: $r->{line}\n";
+    $c += $n;
   }
   return $c;
 }
