db5c38e5171266eec7084c174730ef22d3b36f49
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-08 20:45:27 +0000

    2021 Day 7 Implemented median of medians for no real benefit

--- 2021/07/main.go
+++ 2021/07/main.go
@@ -24,8 +24,53 @@ func FuelSum1(p int, inp []int) int {
 	return c
 }
 
+func MedianOfMedians(inp []int, n int) int {
+	if len(inp) <= 10 {
+		return SimpleMedianN(inp, n)
+	}
+
+	numSlices := (len(inp) + 4) / 5
+	meds := make([]int, numSlices)
+	for i := 0; i < numSlices; i++ {
+		start := i * 5
+		end := start + 5
+		if end > len(inp) {
+			end = len(inp)
+		}
+		sort.Ints(inp[start:end])
+		meds[i] = inp[(start+end)/2]
+	}
+	pivot := MedianOfMedians(meds, len(meds)/2)
+	var lhs, equal, rhs []int
+	for i := range inp {
+		if inp[i] < pivot {
+			lhs = append(lhs, inp[i])
+		} else if inp[i] > pivot {
+			rhs = append(rhs, inp[i])
+		} else {
+			equal = append(equal, inp[i])
+		}
+	}
+	if n < len(lhs) {
+		return MedianOfMedians(lhs, n)
+	} else if n < len(lhs)+len(equal) {
+		return pivot
+	}
+	return MedianOfMedians(rhs, n-len(lhs)-len(equal))
+}
+
+func SimpleMedianN(inp []int, n int) int {
+	sort.Ints(inp)
+	return inp[n]
+}
+
+func SimpleMedian(inp []int) int {
+	sort.Ints(inp)
+	return inp[len(inp)/2]
+}
+
 func MinFuel1(inp []int) int {
-	return FuelSum1(inp[len(inp)/2], inp)
+	return FuelSum1(MedianOfMedians(inp, len(inp)/2), inp)
 }
 
 func FuelSum2(p int, inp []int) int {
@@ -51,7 +96,6 @@ func MinFuel2(inp []int) int {
 }
 
 func Calc(inp []int) (int, int) {
-	sort.Ints(inp)
 	return MinFuel1(inp), MinFuel2(inp)
 }
 
--- 2021/07/main_test.go
+++ 2021/07/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,6 +25,32 @@ func TestCalc(t *testing.T) {
 		p1, p2 := Calc(ReadIntsFromFile(tc.file))
 		assert.Equal(t, tc.p1, p1, tc.file+" part 1")
 		assert.Equal(t, tc.p2, p2, tc.file+" part 2")
+	}
+}
+
+func TestMedianOfMedians(t *testing.T) {
+	tests := [][]int{
+		[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12},
+		[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13},
+		[]int{474, 475, 488, 479, 480, 476, 484, 485, 485, 473, 479, 480, 488, 483, 490, 496, 507, 498, 509, 491, 500, 492, 492, 497, 513, 493, 497, 503, 500, 501, 505, 492},
+	}
+	for _, inp := range tests {
+		for i := 0; i < len(inp); i++ {
+			t.Run(fmt.Sprintf("%v [%d]", inp, i), func(t *testing.T) {
+				mom := MedianOfMedians(inp, i)
+				simple := SimpleMedianN(inp, i)
+				assert.Equal(t, simple, mom)
+			})
+		}
+	}
+}
+
+func TestMedianOfMediansBig(t *testing.T) {
+	for _, file := range []string{"test1.txt", "test2.txt", "input.txt"} {
+		inp := ReadIntsFromFile(file)
+		mom := MedianOfMedians(inp, len(inp)/2)
+		simple := SimpleMedian(inp)
+		assert.Equal(t, simple, mom, file)
 	}
 }
 
