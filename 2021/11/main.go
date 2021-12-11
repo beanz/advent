c3f79763c8c42af5823ea2e56f3fe6d7d810e462
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-11 09:07:36 +0000

    2021 Day 11 Go

---
+++ 2021/11/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strings"
+
+	. "github.com/beanz/advent/lib-go"
+)
+
+//go:embed input.txt
+var input []byte
+
+type Octopodes struct {
+	m *ByteMap
+}
+
+func NewOctopodes(in []byte) *Octopodes {
+	return &Octopodes{NewByteMap(in)}
+}
+
+func (o Octopodes) String() string {
+	return o.m.String()
+}
+
+func (o *Octopodes) Flash(p int) {
+	o.m.Set(p, '~')
+	n8 := o.m.Neighbours8(p)
+	for _, nb := range n8 {
+		v := o.m.Get(nb)
+		if v == '~' {
+			continue
+		}
+		o.m.Set(nb, v+1)
+		if v >= '9' {
+			o.Flash(nb)
+		}
+	}
+}
+
+func (o *Octopodes) Calc(days int) (int, int) {
+	c := 0
+	day := 1
+	p1 := -1
+	for {
+		o.m.Visit(func(p int, v byte) {
+			o.m.Add(p, 1)
+		})
+		o.m.Visit(func(p int, v byte) {
+			if v > '9' && v != '~' {
+				o.Flash(p)
+			}
+		})
+		p2 := 0
+		o.m.Visit(func(p int, v byte) {
+			if v == '~' {
+				o.m.Set(p, '0')
+				c++
+				p2++
+			}
+		})
+		if day == days {
+			p1 = c
+		}
+		if p2 == o.m.Size() {
+			return p1, day
+		}
+		//fmt.Printf("Day %d\n%s\n", day, o)
+		day++
+	}
+	return p1, -1
+}
+
+func main() {
+	bytes := InputBytes(input)
+	var inp []byte
+	inp = make([]byte, len(input))
+	copy(inp, bytes)
+	g := NewOctopodes(inp)
+	p1, p2 := g.Calc(100)
+	if !benchmark {
+		fmt.Printf("Part 1: %d\n", p1)
+		fmt.Printf("Part 2: %d\n", p2)
+	}
+}
+
+type ByteMap struct {
+	d    []byte
+	w, h int
+}
+
+func NewByteMap(in []byte) (i *ByteMap) {
+	var w int
+	for i, ch := range in {
+		if ch == '\n' {
+			w = i + 1 // +1 since we are keeping the newlines
+			break
+		}
+	}
+	h := len(in) / w
+	return &ByteMap{in, w, h}
+}
+
+func (m *ByteMap) String() string {
+	var sb strings.Builder
+	for y := 0; y < m.h; y++ {
+		sb.WriteString(string(m.d[y*m.w : (1+y)*m.w]))
+	}
+	sb.WriteString(fmt.Sprintf("%d x %d\n", m.w, m.h))
+	return sb.String()
+}
+
+func (m *ByteMap) Size() int {
+	return (m.w - 1) * m.h
+}
+
+func (m *ByteMap) IndexToXY(i int) (int, int) {
+	x := i % m.w
+	return x, (i - x) / m.w
+}
+
+func (m *ByteMap) Contains(i int) bool {
+	return i >= 0 && (i%m.w) < m.w-1 && i/m.w < m.h
+}
+
+func (m *ByteMap) Get(i int) byte {
+	return m.d[i]
+}
+
+func (m *ByteMap) Set(i int, v byte) {
+	m.d[i] = v
+}
+
+func (m *ByteMap) Add(i int, v byte) {
+	m.d[i] += v
+}
+
+func (m *ByteMap) Neighbours(i int) []int {
+	x, y := m.IndexToXY(i)
+	res := make([]int, 0, 4)
+	if x > 0 {
+		res = append(res, i-1)
+	}
+	if x < m.w-2 { // because we've still got newlines!
+		res = append(res, i+1)
+	}
+	if y > 0 {
+		res = append(res, i-m.w)
+	}
+	if y < m.h-1 {
+		res = append(res, i+m.w)
+	}
+	return res
+}
+
+func (m *ByteMap) Neighbours8(i int) []int {
+	res := make([]int, 0, 8)
+	for oy := -1; oy <= 1; oy++ {
+		for ox := -1; ox <= 1; ox++ {
+			ni := i + ox + oy*m.w
+			if m.Contains(ni) {
+				res = append(res, ni)
+			}
+		}
+	}
+	return res
+}
+
+func (m *ByteMap) Visit(fn func(i int, v byte)) {
+	for y := 0; y < m.h; y++ {
+		for x := 0; x < m.w-1; x++ { // -1 due to newlines
+			i := x + y*m.w
+			fn(i, m.d[i])
+		}
+	}
+}
+
+var benchmark = false
---
+++ 2021/11/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	_ "embed"
+	assert "github.com/stretchr/testify/require"
+	"testing"
+)
+
+//go:embed test0.txt
+var test0 []byte
+
+//go:embed test1.txt
+var test1 []byte
+
+type TestCase struct {
+	file   string
+	data   []byte
+	days   int
+	p1, p2 int
+}
+
+func TestCalc(t *testing.T) {
+	tests := []TestCase{
+		{"test0.txt", test0, 1, 9, 6},
+		{"test1.txt", test1, 100, 1656, 195},
+		{"input.txt", input, 100, 1652, 220},
+	}
+	for _, tc := range tests {
+		p1, p2 := NewOctopodes(tc.data).Calc(tc.days)
+		assert.Equal(t, tc.p1, p1, tc.file)
+		assert.Equal(t, tc.p2, p2, tc.file)
+	}
+}
+
+func BenchmarkMain(b *testing.B) {
+	benchmark = true
+	for i := 0; i < b.N; i++ {
+		main()
+	}
+}
