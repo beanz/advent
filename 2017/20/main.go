e943ee62ca92cf400e2ae46e4b76ee739c69ce04
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-12 18:41:35 +0000

    2017 Day 20 Compute x only until collision

--- 2017/20/main.go
+++ 2017/20/main.go
@@ -24,6 +24,30 @@ func (p *Particle) String() string {
 	return fmt.Sprintf("%s v%s a%s dead=%t", p.p, p.v, p.a, p.dead)
 }
 
+func Position(p, v, a, t int) int {
+	for i := 1; i <= t; i++ {
+		v += a
+		p += v
+	}
+	return p
+}
+
+func (p *Particle) Collision(o *Particle, t int) bool {
+	y := Position(p.p.Y, p.v.Y, p.a.Y, t)
+	oy := Position(o.p.Y, o.v.Y, o.a.Y, t)
+	if y != oy {
+		return false
+	}
+	z := Position(p.p.Z, p.v.Z, p.a.Z, t)
+	oz := Position(o.p.Z, o.v.Z, o.a.Z, t)
+	if z != oz {
+		return false
+	}
+	x := Position(p.p.X, p.v.X, p.a.X, t)
+	ox := Position(o.p.X, o.v.X, o.a.X, t)
+	return x == ox
+}
+
 type Game struct {
 	particles []*Particle
 	debug     bool
@@ -49,10 +73,11 @@ func NewGame(lines []string) *Game {
 }
 
 func (g *Game) Part1() int {
-	minAccel := float64(math.MaxInt64)
+	minAccel := math.MaxInt64
 	minA := []int{}
+	origin := Point3D{0, 0, 0}
 	for i, p := range g.particles {
-		a := p.a.Size()
+		a := p.a.ManhattanDistance(origin)
 		if a < minAccel {
 			minAccel = a
 			minA = []int{i}
@@ -69,22 +94,25 @@ func (g *Game) Part1() int {
 
 func (g *Game) Part2() int {
 	count := len(g.particles)
-	for i := 0; i < 10000; i++ {
-		seen := map[Point3D]*Particle{}
+	px := make([]int, len(g.particles))
+	vx := make([]int, len(g.particles))
+	for j, p := range g.particles {
+		px[j] = p.p.X
+		vx[j] = p.v.X
+	}
+	for t := 1; t < 10000; t++ {
+		seen := make(map[int]*Particle, count)
 		for j, p := range g.particles {
 			if g.debug {
-				fmt.Printf("%d %d: %s\n", i, j, p)
+				fmt.Printf("%d %d: %s\n", t, j, p)
 			}
 			if p.dead {
 				continue
 			}
-			p.v.X += p.a.X
-			p.v.Y += p.a.Y
-			p.v.Z += p.a.Z
-			p.p.X += p.v.X
-			p.p.Y += p.v.Y
-			p.p.Z += p.v.Z
-			if cp, ok := seen[p.p]; ok {
+			vx[j] += p.a.X
+			px[j] += vx[j]
+
+			if cp, ok := seen[px[j]]; ok && p.Collision(cp, t) {
 				if !cp.dead {
 					cp.dead = true
 					count--
@@ -92,7 +120,7 @@ func (g *Game) Part2() int {
 				p.dead = true
 				count--
 			}
-			seen[p.p] = p
+			seen[px[j]] = p
 		}
 	}
 	return count
--- 2017/20/main_test.go
+++ 2017/20/main_test.go
@@ -7,6 +7,26 @@ import (
 	. "github.com/beanz/advent/lib-go"
 )
 
+func TestCollisionDetction(t *testing.T) {
+	p1 := &Particle{Point3D{-6, 0, 0}, Point3D{3, 0, 0}, Point3D{0, 0, 0},
+		false}
+	p2 := &Particle{Point3D{-4, 0, 0}, Point3D{2, 0, 0}, Point3D{0, 0, 0},
+		false}
+	p3 := &Particle{Point3D{3, 0, 0}, Point3D{-1, 0, 0}, Point3D{0, 0, 0},
+		false}
+	assert.False(t, p1.Collision(p2, 0), "p1&p2 t=0")
+	assert.False(t, p1.Collision(p2, 1), "p1&p2 t=1")
+	assert.True(t, p1.Collision(p2, 2), "p1&p2 t=2")
+	assert.False(t, p1.Collision(p3, 0), "p1&p3 t=0")
+	assert.False(t, p1.Collision(p3, 1), "p1&p3 t=1")
+	assert.False(t, p1.Collision(p3, 2), "p1&p3 t=2")
+	bp1 := &Particle{Point3D{1209, 168, -645}, Point3D{-85, 39, 108},
+		Point3D{-7, -9, -9}, false}
+	bp2 := &Particle{Point3D{339, -537, -720}, Point3D{-97, 5, 66},
+		Point3D{11, 10, 0}, false}
+	assert.True(t, bp1.Collision(bp2, 10), "bp1 & bp2")
+}
+
 func TestPart1(t *testing.T) {
 	assert.Equal(t, 0, NewGame(ReadLines("test.txt")).Part1())
 	assert.Equal(t, 91, NewGame(ReadLines("input.txt")).Part1())
@@ -14,6 +34,7 @@ func TestPart1(t *testing.T) {
 
 func TestPart2(t *testing.T) {
 	assert.Equal(t, 1, NewGame(ReadLines("test-2.txt")).Part2())
+	assert.Equal(t, 0, NewGame(ReadLines("test-2a.txt")).Part2())
 	assert.Equal(t, 567, NewGame(ReadLines("input.txt")).Part2())
 }
 
