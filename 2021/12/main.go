2d84ac4ecec2e4533107203db812104b120e0375
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-12 07:46:13 +0000

    2021 Day 12 go faster

--- 2021/12/main.go
+++ 2021/12/main.go
@@ -9,37 +9,68 @@ import (
 //go:embed input.txt
 var input []byte
 
-type Cave struct {
-	g map[string][]string
+type Cave int
+
+const (
+	START Cave = 0x7374617274
+	END        = 0x656e64
+)
+
+func (c Cave) String() string {
+	s := ""
+	i := int(c)
+	for i > 0 {
+		s = string(byte(i&0xff)) + s
+		i >>= 8
+	}
+	return s
 }
 
-func NewCave(in []byte) *Cave {
+func (c *Cave) HashKey() int {
+	return int(*c)
+}
+
+type CaveSystem struct {
+	g map[Cave][]Cave
+}
+
+func NewCaveSystem(in []byte) *CaveSystem {
 	// 16 seems small but only 12 in my input
-	neighbors := make(map[string][]string, 16)
-	var start, dash int
-	for i, ch := range in {
+	neighbors := make(map[Cave][]Cave, 16)
+	var start, end int
+	var foundDash bool
+	for _, ch := range in {
 		switch ch {
 		case '-':
-			dash = i
+			foundDash = true
 		case '\n':
-			st, end := string(in[start:dash]), string(in[dash+1:i])
-			if end != "start" {
-				neighbors[st] = append(neighbors[st], end)
+			cs := Cave(start)
+			ce := Cave(end)
+			if ce != START {
+				neighbors[cs] = append(neighbors[cs], ce)
 			}
-			if st != "start" {
-				neighbors[end] = append(neighbors[end], st)
+			if cs != START {
+				neighbors[ce] = append(neighbors[ce], cs)
 			}
-			start = i + 1
+			start = 0
+			end = 0
+			foundDash = false
+		default:
+			if foundDash {
+				end = end<<8 + int(ch)
+			} else {
+				start = start<<8 + int(ch)
+			}
 		}
 	}
-	return &Cave{neighbors}
+	return &CaveSystem{neighbors}
 }
 
-func (c *Cave) Solve(start, end string, seen map[string]bool, p2 bool, twice bool) int {
-	if start == end {
+func (c *CaveSystem) Solve(start Cave, seen map[Cave]bool, p2 bool, twice bool) int {
+	if start == END {
 		return 1
 	}
-	if start[0] >= 'a' && start[0] <= 'z' {
+	if (start & 0x20) == 0x20 { // lower case
 		seen[start] = true
 	}
 	paths := 0
@@ -51,25 +82,25 @@ func (c *Cave) Solve(start, end string, seen map[string]bool, p2 bool, twice boo
 			}
 			ntwice = true
 		}
-		nseen := make(map[string]bool, len(seen)+4)
+		nseen := make(map[Cave]bool, len(seen)+4)
 		for k := range seen {
 			nseen[k] = true
 		}
-		paths += c.Solve(nb, end, nseen, p2, ntwice)
+		paths += c.Solve(nb, nseen, p2, ntwice)
 	}
 	return paths
 }
 
-func (c *Cave) Part1() int {
-	return c.Solve("start", "end", make(map[string]bool), false, false)
+func (c *CaveSystem) Part1() int {
+	return c.Solve(START, make(map[Cave]bool), false, false)
 }
 
-func (c *Cave) Part2() int {
-	return c.Solve("start", "end", make(map[string]bool), true, false)
+func (c *CaveSystem) Part2() int {
+	return c.Solve(START, make(map[Cave]bool), true, false)
 }
 
 func main() {
-	g := NewCave(InputBytes(input))
+	g := NewCaveSystem(InputBytes(input))
 	p1 := g.Part1()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
