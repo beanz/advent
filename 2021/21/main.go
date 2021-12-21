6bb38c9a9567f389eadaaccd4affff7753c3445b
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-21 07:24:01 +0000

    2021 Day 21 Go

---
+++ 2021/21/main.go
@@ -0,0 +1,133 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	. "github.com/beanz/advent/lib-go"
+)
+
+//go:embed input.txt
+var input []byte
+
+type Game struct {
+	p1, p2 int
+}
+
+type D100 struct {
+	d int
+}
+
+func NewD100() *D100 {
+	return &D100{0}
+}
+
+func (d *D100) Roll() int {
+	r := (d.d % 100) + 1
+	d.d++
+	return r
+}
+
+func (d *D100) Count() int {
+	return d.d
+}
+
+func NewGame(in []byte) *Game {
+	ints := FastInts(in, 4)
+	return &Game{ints[1], ints[3]}
+}
+
+func (g *Game) Part1() int {
+	s := []int{0, 0}
+	p := []int{g.p1, g.p2}
+	dice := NewD100()
+	for {
+		for i := 0; i < 2; i++ {
+			r := dice.Roll() + dice.Roll() + dice.Roll()
+			p[i] += r
+			for p[i] > 10 {
+				p[i] -= 10
+			}
+			s[i] += p[i]
+			//fmt.Printf("Player %d moves to space %d for a total score of %d\n",
+			//	i+1, p[i], s[i])
+			if s[i] >= 1000 {
+				return s[1-i] * dice.Count()
+			}
+		}
+	}
+}
+
+var rollWays = map[int]int{
+	3: 1,
+	4: 3,
+	5: 6,
+	6: 7,
+	7: 6,
+	8: 3,
+	9: 1,
+}
+
+type VisitKey struct {
+	p1, s1, p2, s2 int64
+}
+
+type Counts struct {
+	w1, w2 int64
+}
+
+type WinCounter struct {
+	cache map[VisitKey]Counts
+}
+
+func NewWinCounter() *WinCounter {
+	return &WinCounter{make(map[VisitKey]Counts, 1024)}
+}
+
+func (wc *WinCounter) CountWins(pos1, score1, pos2, score2 int64) (int64, int64) {
+	if score1 >= 21 {
+		return 1, 0
+	}
+	if score2 >= 21 {
+		return 0, 1
+	}
+	k := VisitKey{pos1, score1, pos2, score2}
+	if v, ok := wc.cache[k]; ok {
+		return v.w1, v.w2
+	}
+	var w1, w2 int64
+	for roll, ways := range rollWays {
+		np1 := pos1 + int64(roll)
+		for np1 > 10 {
+			np1 -= 10
+		}
+		ns1 := score1 + np1
+		// reverse arguments pairs and reverse answers
+		sw2, sw1 := wc.CountWins(pos2, score2, np1, ns1)
+		w1 += sw1 * int64(ways)
+		w2 += sw2 * int64(ways)
+	}
+	wc.cache[k] = Counts{w1, w2}
+	return w1, w2
+}
+
+func (g *Game) Part2() int64 {
+	w1, w2 := NewWinCounter().CountWins(int64(g.p1), 0, int64(g.p2), 0)
+	if w1 > w2 {
+		return w1
+	}
+	return w2
+}
+
+func main() {
+	g := NewGame(InputBytes(input))
+	p1 := g.Part1()
+	if !benchmark {
+		fmt.Printf("Part 1: %d\n", p1)
+	}
+	p2 := g.Part2()
+	if !benchmark {
+		fmt.Printf("Part 2: %d\n", p2)
+	}
+}
+
+var benchmark = false
---
+++ 2021/21/main_test.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	_ "embed"
+	assert "github.com/stretchr/testify/require"
+	"testing"
+)
+
+//go:embed test1.txt
+var test1 []byte
+
+//go:embed input.txt
+var safeinput []byte
+
+func ExampleMain() {
+	main()
+	//Output:
+	// Part 1: 428736
+	// Part 2: 57328067654557
+}
+
+func TestPart1(t *testing.T) {
+	tests := []struct {
+		file string
+		data []byte
+		ans  int
+	}{
+		{"test1.txt", test1, 739785},
+		{"input.txt", input, 428736},
+	}
+	for _, tc := range tests {
+		assert.Equal(t, tc.ans, NewGame(tc.data).Part1(), tc.file)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	tests := []struct {
+		file string
+		data []byte
+		ans  int64
+	}{
+		{"test1.txt", test1, 444356092776315},
+		{"input.txt", input, 57328067654557},
+	}
+	for _, tc := range tests {
+		assert.Equal(t, tc.ans, NewGame(tc.data).Part2(), tc.file)
+	}
+}
+
+func BenchmarkMain(b *testing.B) {
+	benchmark = true
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		copy(input, safeinput)
+		b.StartTimer()
+		main()
+	}
+}
