44936feb7a264c1ccca4ae054f843d34083cc586
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-24 20:35:08 +0000

    2018 Day 14 Go faster

--- 2018/14/main.go
+++ 2018/14/main.go
@@ -12,8 +12,10 @@ import (
 var input []byte
 
 type RecipeState struct {
-	recipes []byte
-	e0, e1  int
+	recipes   []byte
+	e0, e1    int
+	si        int
+	end []byte
 }
 
 func (r *RecipeState) len() int {
@@ -21,55 +23,77 @@ func (r *RecipeState) len() int {
 }
 
 func (r *RecipeState) next() {
-	v0 := r.recipes[r.e0] - '0'
-	v1 := r.recipes[r.e1] - '0'
-	s := fmt.Sprintf("%d", v0+v1)
-	for _, e := range s {
-		r.recipes = append(r.recipes, byte(e))
+	v0 := r.recipes[r.e0]
+	v1 := r.recipes[r.e1]
+	n := v0 + v1
+	if n >= 10 {
+		r.recipes = append(r.recipes, n/10)
 	}
+	r.recipes = append(r.recipes, n%10)
 	r.e0 = (r.e0 + int(v0) + 1) % len(r.recipes)
 	r.e1 = (r.e1 + int(v1) + 1) % len(r.recipes)
 }
 
-func (r *RecipeState) contains(s string) int {
-	return strings.Index(string(r.recipes), s)
+func (r *RecipeState) hasEnd() int {
+SEARCH:
+	for ; r.si < len(r.recipes)-len(r.end); r.si++ {
+		for j := 0; j < len(r.end); j++ {
+			if r.recipes[r.si+j] != r.end[j] {
+				continue SEARCH
+			}
+		}
+		return r.si
+	}
+	return -1
 }
 
 type Game struct {
-	endString string
+	endString []byte
 	end       int
 	init      []byte
 	debug     bool
 }
 
 func NewGame(lines []string) *Game {
-	g := &Game{lines[1], SimpleReadInts(lines[1])[0], []byte{}, false}
-	for _, e := range lines[0] {
-		g.init = append(g.init, byte(e))
-	}
+	g := &Game{[]byte(lines[1]), SimpleReadInts(lines[1])[0],
+		[]byte(lines[0]), false}
 	return g
 }
 
 func (g *Game) Part1() string {
-	s := RecipeState{g.init, 0, 1}
+	state := make([]byte, len(g.init), g.end+10)
+	for i := 0; i < len(g.init); i++ {
+		state[i] = g.init[i] - '0'
+	}
+	s := RecipeState{state, 0, 1, 0, []byte{}}
 	for {
 		s.next()
 		if s.len() >= g.end+10 {
 			break
 		}
 	}
-	return fmt.Sprintf("%s", s.recipes[g.end:g.end+10])
+	var sb strings.Builder
+	for _, v := range s.recipes[g.end : g.end+10] {
+		sb.WriteByte('0' + v)
+	}
+	return sb.String()
 }
 
 func (g *Game) Part2() int {
-	s := RecipeState{g.init, 0, 1}
+	state := make([]byte, len(g.init), 20480000)
+	for i := 0; i < len(g.init); i++ {
+		state[i] = g.init[i] - '0'
+	}
+	end := make([]byte, len(g.endString))
+	for i := 0; i < len(g.endString); i++ {
+		end[i] = g.endString[i]-'0'
+	}
+	s := RecipeState{state, 0, 1, 0, end}
 	i := -1
-	for {
+	for ; s.len() < 30000000; {
 		s.next()
-		if s.len()%100000 == 0 {
-			if i = s.contains(g.endString); i != -1 {
-				break
-			}
+		if i = s.hasEnd(); i != -1 {
+			break
 		}
 	}
 	return i
@@ -78,12 +102,9 @@ func (g *Game) Part2() int {
 func main() {
 	g := NewGame(InputLines(input))
 	p1 := g.Part1()
-	if !benchmark {
-		fmt.Printf("Part1: %s\n", p1)
-	}
-	g = NewGame(InputLines(input))
 	p2 := g.Part2()
 	if !benchmark {
+		fmt.Printf("Part1: %s\n", p1)
 		fmt.Printf("Part2: %d\n", p2)
 	}
 }
