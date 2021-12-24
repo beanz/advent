a42dce4750a05e795ab934a61105412310387b39
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-24 13:19:45 +0000

    2021 Day 24 Perl hacks

---
+++ 2021/24/aoc-1.pl
@@ -0,0 +1,457 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use v5.10;
+$; = $" = ',';
+my $model = shift // 99999999999999;
+my @inp = split //, $model;
+my $r = prog();
+print "\n$r\n";
+
+sub prog {
+  my $x = 0;
+  my $y = 0;
+  my $z = 0;
+  return inp0($x, $y, $z, "");
+}
+
+sub inp0 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[0]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $x += 13;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 8;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp1($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp1 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[1]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $x += 12;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 16;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp2($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp2 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[2]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $x += 10;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 4;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp3($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp3 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[3]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $z = int($z / 26);
+  $x += -11;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 1;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp4($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp4 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[4]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $x += 14;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 13;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp5($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp5 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+print STDERR "$ws.........\r";
+
+  for my $wi (reverse 1 .. $inp[5]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $x += 13;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 5;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp6($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp6 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[6]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $x += 12;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 0;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp7($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp7 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[7]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $z = int($z / 26);
+  $x += -5;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 10;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp8($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp8 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[8]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $x += 10;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 7;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp9($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp9 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[9]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $z = int($z / 26);
+  $x += 0;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 2;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp10($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp10 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[10]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $z = int($z / 26);
+  $x += -11;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 13;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp11($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp11 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[11]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $z = int($z / 26);
+  $x += -13;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 15;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp12($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp12 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[12]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $z = int($z / 26);
+  $x += -13;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 14;
+  $y *= $x;
+  $z += $y;
+
+    my $r = inp13($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+
+sub inp13 {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+
+  for my $wi (reverse 1 .. $inp[13]) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+  $x = $z;
+  $x %= 26;
+  $z = int($z / 26);
+  $x += -11;
+  $x = $x == $w ? 0 : 1;
+  $y = 25;
+  $y *= $x;
+  $y += 1;
+  $z *= $y;
+  $y = $w;
+  $y += 9;
+  $y *= $x;
+  $z += $y;
+
+    return $ws.$wi if ($z == 0);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return
+}
---
+++ 2021/24/gen.pl
@@ -0,0 +1,153 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use constant ( DEBUG => $ENV{DEBUG} );
+my %inst =
+  (
+   inp => sub {
+     my ($a, $b) = @_;
+     print "  \$$a = shift \@\$inp;\n";
+   },
+   add => sub {
+     my ($a, $b) = @_;
+     if ($b =~ /\d/) {
+       print "  \$$a += $b;\n";
+     } else {
+       print "  \$$a += \$$b;\n";
+     }
+   },
+   mul => sub {
+     my ($a, $b) = @_;
+     if ($b =~ /\d/) {
+       print "  \$$a *= $b;\n";
+     } else {
+       print "  \$$a *= \$$b;\n";
+     }
+   },
+   div => sub {
+     my ($a, $b) = @_;
+     if ($b =~ /\d/) {
+       print "  \$$a = int(\$$a / $b);\n";
+     } else {
+       print "  \$$a = int(\$$a / \$$b);\n";
+     }
+   },
+   mod => sub {
+     my ($a, $b) = @_;
+     if ($b =~ /\d/) {
+       print "  \$$a %= $b;\n";
+     } else {
+       print "  \$$a %= \$$b;\n";
+     }
+   },
+   eql => sub {
+     my ($a, $b) = @_;
+     if ($b =~ /\d/) {
+       print "  \$$a = \$$a == $b ? 1 : 0;\n";
+     } else {
+       print "  \$$a = \$$a == \$$b ? 1 : 0;\n";
+     }
+   },
+   neq => sub {
+     my ($a, $b) = @_;
+     if ($b =~ /\d/) {
+       print "  \$$a = \$$a == $b ? 0 : 1;\n";
+     } else {
+       print "  \$$a = \$$a == \$$b ? 0 : 1;\n";
+     }
+   },
+   set => sub {
+     my ($a, $b) = @_;
+     if ($b =~ /\d/) {
+       print "  \$$a = $b;\n";
+     } else {
+       print "  \$$a = \$$b;\n";
+     }
+   },
+  );
+
+print <<'EOF';
+#!/usr/bin/perl
+use strict;
+use warnings;
+use v5.10;
+$; = $" = ',';
+my $model = shift // 99999999999999;
+my @inp = split //, $model;
+my $r = prog();
+print "\n$r\n";
+
+sub prog {
+  my $x = 0;
+  my $y = 0;
+  my $z = 0;
+  return inp0($x, $y, $z, "");
+}
+EOF
+
+my $reverse = ~~@ARGV ? '' : 'reverse ';
+
+my $prog;
+{
+  undef $/;
+  $prog = <STDIN>;
+}
+
+$prog =~ s!div ([wxyz]) 1\n!!sg;
+$prog =~ s!mul ([wxyz]) 0\nadd \1 (\w+)!set $1 $2!sg;
+$prog =~ s!eql ([wxyz]) (\w+)\neql \1 0!neq $1 $2!sg;
+
+#print "$prog"; exit;
+my @subs = split /inp w\n/, $prog;
+print STDERR scalar @subs, "\n";
+my $pre = shift @subs;
+print STDERR "Pre:\n$pre\nEnd\n\n";
+
+for my $i (0..@subs-1) {
+  my $sub = $subs[$i];
+  print '
+sub inp'.$i.' {
+  my ($xi, $yi, $zi, $ws) = @_;
+  state %cache;
+  return $cache{$xi,$yi,$zi} if (exists $cache{$xi,$yi,$zi});
+';
+  if ($i == 5) {
+    print 'print STDERR "$ws.........\r";'."\n";
+  }
+  print '
+  for my $wi ('.$reverse.'1 .. $inp['.$i.']) {
+    my $w = $wi;
+    my $x = $xi;
+    my $y = $yi;
+    my $z = $zi;
+    # prog
+';
+  for (split /\n/, $sub) {
+    chomp;
+    my ($inst, @a) = split / /;
+    print STDERR "# $inst @a\n";
+    my ($a, $b) = @a;
+    $inst{$inst}->($a, $b);
+  }
+  if ($i+1 == @subs) {
+    print '
+    return $ws.$wi if ($z == 0);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return
+}
+';
+  } else {
+    print '
+    my $r = inp'.($i+1).'($x, $y, $z, $ws.$wi);
+    return $r if (defined $r);
+  }
+  $cache{$xi,$yi,$zi} = undef;
+  return;
+}
+';
+  }
+}
+exit;
+
