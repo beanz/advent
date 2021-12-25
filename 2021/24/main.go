0d8bb3e855da68985619b63d952663391fe19751
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-25 17:33:28 +0000

    2021 Day 24 Go constraint solving

--- 2021/24/main.go
+++ 2021/24/main.go
@@ -9,11 +9,15 @@ import (
 //go:embed input.txt
 var input []byte
 
+type Constraint struct {
+	i, j int
+}
+
 type ALU struct {
-	addX  []int
-	addY  []int
-	divZ  []int
-	cache map[int]int
+	addX []int
+	addY []int
+	divZ []int
+	cons []Constraint
 }
 
 func nInt(in []byte) int {
@@ -34,7 +38,7 @@ func NewALU(in []byte) *ALU {
 	j := 0
 	y := 0
 	alu := &ALU{make([]int, 0, 14), make([]int, 0, 14), make([]int, 0, 14),
-		make(map[int]int, 2097152),
+		make([]Constraint, 0, 14),
 	}
 	for i := 0; i < len(in); i++ {
 		if in[i] == '\n' {
@@ -70,49 +74,48 @@ func NewALU(in []byte) *ALU {
 	if len(alu.divZ) != 14 {
 		panic("wrong number of div Z found")
 	}
+	stack := make([]int, 0, 14)
+	for i := 0; i < 14; i++ {
+		if alu.divZ[i] != 1 {
+			j, stack = stack[len(stack)-1], stack[:len(stack)-1]
+			alu.cons = append(alu.cons, Constraint{i, j})
+		} else {
+			stack = append(stack, i)
+		}
+	}
 	return alu
 }
 
-func (a *ALU) Calc(zi int, level int, order []int, ans int) int {
-	k := (zi << 4) + level
-	if v, ok := a.cache[k]; ok {
-		return v
+func (a *ALU) Solve(start, inc int) int {
+	ans := make([]int, 14)
+	for i := 0; i < 14; i++ {
+		ans[i] = start
 	}
-	for _, w := range order {
-		z := zi / a.divZ[level]
-		if (zi % 26) + a.addX[level] != w {
-			z *= 26
-			z += (w + a.addY[level])
-		}
-		if level != 13 {
-			r := a.Calc(z, level+1, order, ans*10+w)
-			a.cache[k] = r
-			if r != 0 {
-				return r
+	for _, c := range a.cons {
+		for {
+			ans[c.i] = ans[c.j] + a.addY[c.j] + a.addX[c.i]
+			if 0 < ans[c.i] && ans[c.i] <= 9 {
+				break
 			}
-		} else if z == 0 {
-			a.cache[k] = ans*10 + w
-			return ans*10 + w
+			ans[c.j] += inc
 		}
 	}
-	a.cache[k] = 0
-	return 0
+	n := 0
+	for i := 0; i < 14; i++ {
+		n = n*10 + int(ans[i])
+	}
+	return n
 }
 
-func (a *ALU) Best(order []int) int {
-	return a.Calc(0, 0, order, 0)
-}
-
-func (a *ALU) Solve() (int, int) {
-	p1 := a.Best([]int{9, 8, 7, 6, 5, 4, 3, 2, 1})
-	a.cache =make(map[int]int, 16777216)
-	p2 := a.Best([]int{1, 2, 3, 4, 5, 6, 7, 8, 9})
+func (a *ALU) Parts() (int, int) {
+	p1 := a.Solve(9, -1)
+	p2 := a.Solve(1, 1)
 	return p1, p2
 }
 
 func main() {
 	g := NewALU(InputBytes(input))
-	p1, p2 := g.Solve()
+	p1, p2 := g.Parts()
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2: %d\n", p2)
