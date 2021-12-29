cec07fa5794627cb5c7d8ade54c423adfb3d9a98
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-29 15:03:26 +0000

    2016 Day 14 Go faster

--- 2016/14/main.go
+++ 2016/14/main.go
@@ -15,7 +15,7 @@ type Game struct {
 	salt      []byte
 	index     int
 	numstr    *NumStr
-	ring      []string
+	ring      [][16]byte
 	stretched bool
 }
 
@@ -23,7 +23,7 @@ func NewGame(salt []byte, stretched bool) *Game {
 	if salt[len(salt)-1] == '\n' {
 		salt = salt[:len(salt)-1]
 	}
-	ring := make([]string, 1001)
+	ring := make([][16]byte, 1001)
 	g := &Game{salt, 0, NewNumStrFromBytes(salt), ring, stretched}
 	for i := 0; i < 1001; i++ {
 		ring[i] = g.NextMD5()
@@ -35,15 +35,58 @@ func (g *Game) String() string {
 	return fmt.Sprintf("Salt: %s Index: %d\n", string(g.salt), g.index)
 }
 
-func (g *Game) NextMD5() string {
-	sum := fmt.Sprintf("%x", md5.Sum(g.numstr.Bytes()))
+func (g *Game) NextMD5() [16]byte {
+	sum := md5.Sum(g.numstr.Bytes())
 	if g.stretched {
+		b := make([]byte, 32)
 		for i := 0; i < 2016; i++ {
-			sum = fmt.Sprintf("%x", md5.Sum([]byte(sum)))
+			for j := 0; j < 16; j++ {
+				b[j*2] = IntToHex((sum[j] & 0xf0) >> 4)
+				b[1+j*2] = IntToHex(sum[j] & 0xf)
+			}
+			sum = md5.Sum(b)
 		}
 	}
 	g.numstr.Inc()
 	return sum
+}
+
+func TripleDigit(s []byte) (byte, bool) {
+	a := (s[0] & 0xf0) >> 4
+	b := s[0] & 0x0f
+	for i := 1; i < len(s); i++ {
+		c := (s[i] & 0xf0) >> 4
+		d := s[i] & 0x0f
+		if b == c && (a == b || b == d) {
+			return b, true
+		}
+		a, b = c, d
+	}
+	return 0, false
+}
+
+func IntToHex(d byte) byte {
+	if d > 9 {
+		return 'a' + d - 10
+	}
+	return '0' + d
+}
+
+func HasFive(s []byte, d byte) bool {
+	d0 := d << 4
+	dd := d | d0
+	for i := 0; i < len(s)-2; i++ {
+		if s[i+1] != dd {
+			continue
+		}
+		if s[i] == dd && s[i+2]&0xf0 == d0 {
+			return true
+		}
+		if s[i]&0xf == d && s[i+2] == dd {
+			return true
+		}
+	}
+	return false
 }
 
 func Triple(s string) string {
@@ -64,20 +107,19 @@ func Triple(s string) string {
 }
 
 func (g *Game) NextKey() int {
-	var sum string
+	var sum [16]byte
 	for {
 		ti := g.index
 		sum = g.ring[ti%1001]
 		g.ring[ti%1001] = g.NextMD5()
 		g.index++
-		ch := Triple(sum)
-		if ch == "" {
+		ch, found := TripleDigit(sum[:16])
+		if !found {
 			continue
 		}
-		match := ch + ch + ch + ch + ch
 		for i := 1; i < 1001; i++ {
 			sum = g.ring[(ti+i)%1001]
-			if strings.Contains(sum, match) {
+			if HasFive(sum[:16], ch) {
 				//fmt.Printf("Got a hit at index %d\n", ti)
 				return ti
 			}
--- 2016/14/main_test.go
+++ 2016/14/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -16,14 +18,40 @@ func TestTriple(t *testing.T) {
 	assert.Equal(t, "8", Triple("0034e0923cc38887a57bd7b1d4f953df"))
 }
 
+func TestTripleDigit(t *testing.T) {
+	game := NewGame([]byte("abc"), false)
+	d, found := TripleDigit(game.ring[18][:16])
+	assert.True(t, found)
+	assert.Equal(t, byte(8), d)
+	d, found = TripleDigit(game.ring[39][:16])
+	assert.True(t, found)
+	assert.Equal(t, byte(14), d)
+}
+
+func TestHasFive(t *testing.T) {
+	game := NewGame([]byte("abc"), false)
+	assert.True(t, HasFive(game.ring[816][:16], 0xe))
+}
+
 func TestMD5(t *testing.T) {
 	game := NewGame([]byte("abc"), false)
-	assert.Equal(t, "0034e0923cc38887a57bd7b1d4f953df", game.ring[18])
+	assert.Equal(t, "0034e0923cc38887a57bd7b1d4f953df", fmt.Sprintf("%x", game.ring[18]))
+}
+
+func TestIntToHex(t *testing.T) {
+	sum := md5.Sum([]byte("abc0"))
+	b := make([]byte, 32)
+	for j := 0; j < 16; j++ {
+		b[j*2] = IntToHex((sum[j] & 0xf0) >> 4)
+		b[1+j*2] = IntToHex(sum[j] & 0xf)
+	}
+	assert.Equal(t, fmt.Sprintf("%x", sum), fmt.Sprintf("%s", b))
 }
 
 func TestStretchedMD5(t *testing.T) {
 	game := NewGame([]byte("abc"), true)
-	assert.Equal(t, "a107ff634856bb300138cac6568c0f24", game.ring[0])
+	assert.Equal(t, "a107ff634856bb300138cac6568c0f24",
+		fmt.Sprintf("%x", game.ring[0]))
 }
 
 func TestNext(t *testing.T) {
