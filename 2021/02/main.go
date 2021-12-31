58748671f7f0b09f3d6b262dbc55812e7816a859
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-31 17:50:00 +0000

    2021 Day 2 Go faster

--- 2021/02/main.go
+++ 2021/02/main.go
@@ -10,62 +10,35 @@ import (
 //go:embed "input.txt"
 var input []byte
 
-type Move int
-
-const (
-	Forward Move = iota
-	Down
-	Up
-)
-
-type Cmd struct {
-	move  Move
-	units int
-}
-
-type Game struct {
-	cmds []Cmd
-}
-
-func NewGame(in []string) *Game {
-	g := &Game{make([]Cmd, len(in))}
-	for i, l := range in {
-		units := Ints(l)[0]
-		switch l[0] {
-		case 'f':
-			g.cmds[i] = Cmd{Forward, units}
-		case 'd':
-			g.cmds[i] = Cmd{Down, units}
-		case 'u':
-			g.cmds[i] = Cmd{Up, units}
-		}
-	}
-	return g
-}
-
-func (g *Game) Move() (int, int) {
+func Move(in []byte) (int, int) {
 	p1, p2 := Point{X: 0, Y: 0}, Point3D{X: 0, Y: 0, Z: 0} // Z=aim
-	for _, cmd := range g.cmds {
-		switch cmd.move {
-		case Forward:
-			p1.X += cmd.units
-			p2.X += cmd.units
-			p2.Y += p2.Z * cmd.units
-		case Down:
-			p1.Y += cmd.units
-			p2.Z += cmd.units
-		case Up:
-			p1.Y -= cmd.units
-			p2.Z -= cmd.units
+	for i := 0; i < len(in); {
+		switch in[i] {
+		case 'f':
+			u := int(in[i+8] - '0')
+			p1.X += u
+			p2.X += u
+			p2.Y += p2.Z * u
+			i += 10
+		case 'd':
+			u := int(in[i+5] - '0')
+			p1.Y += u
+			p2.Z += u
+			i += 7
+		case 'u':
+			u := int(in[i+3] - '0')
+			p1.Y -= u
+			p2.Z -= u
+			i += 5
+		default:
+			panic("parse error")
 		}
 	}
 	return p1.X * p1.Y, p2.X * p2.Y
 }
 
 func main() {
-	cmds := InputLines(input)
-	g := NewGame(cmds)
-	p1, p2 := g.Move()
+	p1, p2 := Move(InputBytes(input))
 	if !benchmark {
 		fmt.Printf("Part 1: %d\n", p1)
 		fmt.Printf("Part 2: %d\n", p2)
--- 2021/02/main_test.go
+++ 2021/02/main_test.go
@@ -1,23 +1,26 @@
 package main
 
 import (
+	_ "embed"
 	"github.com/stretchr/testify/assert"
 	"testing"
-
-	. "github.com/beanz/advent/lib-go"
 )
+
+//go:embed test1.txt
+var test1 []byte
 
 func TestMove(t *testing.T) {
 	tests := []struct {
 		file string
+		data []byte
 		p1   int
 		p2   int
 	}{
-		{"test1.txt", 150, 900},
-		{"input.txt", 1714950, 1281977850},
+		{"test1.txt", test1, 150, 900},
+		{"input.txt", input, 1714950, 1281977850},
 	}
 	for _, tc := range tests {
-		p1, p2 := NewGame(ReadFileLines(tc.file)).Move()
+		p1, p2 := Move(tc.data)
 		assert.Equal(t, tc.p1, p1, tc.file+" part 1")
 		assert.Equal(t, tc.p2, p2, tc.file+" part 2")
 	}
