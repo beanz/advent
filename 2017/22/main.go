7d00a28ac8db37265b84793282329f1b8a2b47b1
Mark Hindess <soft-github@temporalanomaly.com>
2021-12-31 13:34:06 +0000

    2017 22 Go faster

--- 2017/22/main.go
+++ 2017/22/main.go
@@ -20,28 +20,27 @@ const (
 )
 
 type Game struct {
-	points []NodeState
-	cur    Point
-	dir    Direction
-	count  int
-	debug  bool
+	points     []NodeState
+	cur        int
+	dirX, dirY int
+	count      int
 }
 
 func (g *Game) String() string {
 	s := ""
-	ci := (g.cur.X + 255) + 512*(g.cur.Y+255)
-	bb := NewBoundingBox()
+	bb := NewFBoundingBox()
 	for y := -255; y <= 255; y++ {
 		for x := -255; x <= 255; x++ {
-			p := Point{x, y}
-			if g.Get(p) != CLEAN {
-				bb.Add(p)
+			p := g.Index(x, y)
+			if g.points[p] != CLEAN {
+				bb.AddXY(int32(x), int32(y))
 			}
 		}
 	}
-	for y := bb.Min.Y; y <= bb.Max.Y; y++ {
-		for x := bb.Min.X; x <= bb.Max.X; x++ {
-			i := (x + 255) + 512*(y+255)
+	xmin, xmax, ymin, ymax := bb.Limits()
+	for y := ymin; y <= ymax; y++ {
+		for x := xmin; x <= xmax; x++ {
+			i := int((x + 255) + 512*(y+255))
 			var sq string
 			switch g.points[i] {
 			case INFECTED:
@@ -53,7 +52,7 @@ func (g *Game) String() string {
 			default:
 				sq = "."
 			}
-			if i == ci {
+			if i == g.cur {
 				sq = Red(sq)
 			}
 			s += sq
@@ -64,85 +63,68 @@ func (g *Game) String() string {
 }
 
 func NewGame(lines []string) *Game {
-	g := &Game{make([]NodeState, 512*512),
-		Point{0, 0},
-		Compass("N").Direction(),
-		0,
-		false}
+	g := &Game{make([]NodeState, 512*512), 0, 0, -1, 0}
+	g.cur = g.Index(len(lines[0])/2, len(lines)/2)
 	for y, line := range lines {
 		for x, ch := range line {
-			p := Point{x, y}
 			if ch == '#' {
-				g.Set(p, INFECTED)
+				g.points[g.Index(x, y)] = INFECTED
 			}
 		}
 	}
-	g.cur = Point{len(lines[0]) / 2, len(lines) / 2}
 	return g
 }
 
-func (g *Game) Get(p Point) NodeState {
-	i := (p.X + 255) + 512*(p.Y+255)
-	return g.points[i]
-}
-
-func (g *Game) Set(p Point, s NodeState) {
-	i := (p.X + 255) + 512*(p.Y+255)
-	g.points[i] = s
+func (g *Game) Index(x, y int) int {
+	return (x + 255) + 512*(y+255)
 }
 
 func (g *Game) Burst1() {
-	infected := g.Get(g.cur) == INFECTED
+	infected := g.points[g.cur] == INFECTED
 	if infected {
-		g.dir = g.dir.CW()
+		g.dirX, g.dirY = -g.dirY, g.dirX
 	} else {
-		g.dir = g.dir.CCW()
+		g.dirX, g.dirY = g.dirY, -g.dirX
 	}
 	if infected {
-		g.Set(g.cur, CLEAN)
+		g.points[g.cur] = CLEAN
 	} else {
-		g.Set(g.cur, INFECTED)
+		g.points[g.cur] = INFECTED
 		g.count++
 	}
-	g.cur = g.cur.In(g.dir)
+	g.cur += g.dirX + 512*g.dirY
 }
 
 func (g *Game) Part1(bursts int) int {
 	for i := 0; i < bursts; i++ {
 		g.Burst1()
-		if g.debug {
-			fmt.Printf("%s\n", g)
-		}
 	}
 	return g.count
 }
 
 func (g *Game) Burst2() {
-	state := g.Get(g.cur)
+	state := g.points[g.cur]
 	switch state {
 	case WEAKENED:
 		// no turn
-		g.Set(g.cur, INFECTED)
+		g.points[g.cur] = INFECTED
 		g.count++
 	case INFECTED:
-		g.dir = g.dir.CW()
-		g.Set(g.cur, FLAGGED)
+		g.dirX, g.dirY = -g.dirY, g.dirX
+		g.points[g.cur] = FLAGGED
 	case FLAGGED:
-		g.dir = g.dir.CW().CW()
-		g.Set(g.cur, CLEAN)
+		g.dirX, g.dirY = -g.dirX, -g.dirY
+		g.points[g.cur] = CLEAN
 	default: // CLEAN
-		g.dir = g.dir.CCW()
-		g.Set(g.cur, WEAKENED)
+		g.dirX, g.dirY = g.dirY, -g.dirX
+		g.points[g.cur] = WEAKENED
 	}
-	g.cur = g.cur.In(g.dir)
+	g.cur += g.dirX + 512*g.dirY
 }
 
 func (g *Game) Part2(bursts int) int {
 	for i := 0; i < bursts; i++ {
 		g.Burst2()
-		if g.debug {
-			fmt.Printf("%s\n", g)
-		}
 	}
 	return g.count
 }
