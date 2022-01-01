0bc0ade181849df940b9b199c4253fd77a51f2bc
Mark Hindess <soft-github@temporalanomaly.com>
2022-01-01 00:08:42 +0000

    2021 Day 14 Minor refactor

--- 2021/14/main.go
+++ 2021/14/main.go
@@ -9,13 +9,13 @@ import (
 //go:embed input.txt
 var input []byte
 
-type Pair struct {
-	a, b byte
+func pairInt(a, b byte) uint {
+	return (uint(a-'A') << 5) + uint(b-'A')
 }
 
 type Polymer struct {
 	init string
-	m    map[Pair]byte
+	m    map[uint]byte
 }
 
 func NewPolymer(in []byte) *Polymer {
@@ -23,22 +23,23 @@ func NewPolymer(in []byte) *Polymer {
 	var p *Polymer
 	for ; i < len(in); i++ {
 		if in[i] == '\n' {
-			p = &Polymer{string(in[0:i]), make(map[Pair]byte, 128)}
+			p = &Polymer{string(in[0:i]), make(map[uint]byte, 128)}
 			break
 		}
 	}
 	i += 2
 	for ; i < len(in); i += 8 {
-		p.m[Pair{in[i], in[i+1]}] = in[i+6]
+		p.m[pairInt(in[i], in[i+1])] = in[i+6]
 	}
 	return p
 }
 
-func (p *Polymer) MostMinusLeast(pc map[Pair]int) int {
+func (p *Polymer) MostMinusLeast(pc map[uint]int) int {
 	chCount := NewSliceByteCounter(26)
 	chCount.Inc(p.init[len(p.init)-1])
 	for pair, c := range pc {
-		chCount.Add(pair.a, c)
+		a := byte('A'+(pair>>5))
+		chCount.Add(a, c)
 	}
 	min := chCount.Count(chCount.Bottom(1)[0])
 	max := chCount.Count(chCount.Top(1)[0])
@@ -46,17 +47,18 @@ func (p *Polymer) MostMinusLeast(pc map[Pair]int) int {
 }
 
 func (p *Polymer) Parts() (int, int) {
-	pairCount := make(map[Pair]int, 100)
+	pairCount := make(map[uint]int, 100)
 	for i := 0; i < len(p.init)-1; i++ {
-		pairCount[Pair{p.init[i], p.init[i+1]}]++
+		pairCount[pairInt(p.init[i], p.init[i+1])]++
 	}
 	p1 := 0
 	for day := 1; day <= 40; day++ {
-		npc := make(map[Pair]int, 100)
+		npc := make(map[uint]int, 100)
 		for pair, c := range pairCount {
+			a, b := byte('A'+(pair>>5)), byte('A'+(pair%32))
 			n := p.m[pair]
-			npc[Pair{pair.a, n}] += c
-			npc[Pair{n, pair.b}] += c
+			npc[pairInt(a, n)] += c
+			npc[pairInt(n, b)] += c
 		}
 		pairCount = npc
 		if day == 10 {
