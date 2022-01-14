33eaf27f26777e7bdd05f885483a3465fdd929c8
Mark Hindess <soft-github@temporalanomaly.com>
2022-01-14 11:23:17 +0000

    2021 Day 9 Zig 0.9.0

--- 2021/09/aoc.zig
+++ 2021/09/aoc.zig
@@ -1,12 +1,5 @@
-const inputfile = @embedFile("input.txt");
-const test1file = @embedFile("test1.txt");
 const std = @import("std");
-const assertEq = std.testing.expectEqual;
-const print = std.io.getStdOut().writer().print;
-const mem = std.mem;
-const alloc = std.heap.page_allocator;
-
-const P1P2 = struct { p1: usize, p2: usize };
+const aoc = @import("aoc-lib.zig");
 
 fn indexOf(h: []const u8, n: u8, o: usize) ?usize {
     var i = o;
@@ -16,10 +9,6 @@ fn indexOf(h: []const u8, n: u8, o: usize) ?usize {
         }
     }
     return null;
-}
-
-pub fn usizeLessThan(c: void, a: usize, b: usize) bool {
-    return a < b;
 }
 
 fn flood(l: []u8, p: usize, w: usize, h: usize) anyerror!usize {
@@ -45,64 +34,58 @@ fn flood(l: []u8, p: usize, w: usize, h: usize) anyerror!usize {
     return size;
 }
 
-fn pp(l: []u8, w: usize, h: usize) void {
+fn pp(l: []u8, w: usize, h: usize) !void {
     var y: usize = 0;
     while (y < h) : (y += 1) {
-        print("{s}", .{l[y * w .. (1 + y) * w]}) catch unreachable;
+        try aoc.print("{s}", .{l[y * w .. (1 + y) * w]});
     }
 }
 
-fn lava(in: []const u8) anyerror!P1P2 {
-    var l: []u8 = mem.dupe(alloc, u8, in) catch unreachable;
+fn lava(alloc: std.mem.Allocator, in: []const u8) ![2]usize {
+    var l: []u8 = alloc.dupe(u8, in) catch unreachable;
+    defer alloc.free(l);
     var w = indexOf(l, '\n', 0).? + 1;
     var h = in.len / w;
-    var p1: usize = 0;
+    var r = [2]usize{ 0, 0 };
     var ch: u8 = '0';
     var sizes = std.ArrayList(usize).init(alloc);
     defer sizes.deinit();
     //print("\n", .{}) catch unreachable;
-    var min: usize = 0;
     while (ch < '9') : (ch += 1) {
         var p: usize = 0;
         while (p < l.len - 1) : (p += 1) {
             if (l[p] != ch) {
                 continue;
             }
-            p1 += 1 + ch - '0';
+            r[0] += 1 + ch - '0';
             var size: usize = try flood(l, p, w, h);
             //print("{},{} s={}\n", .{ p % w, p / w, size }) catch unreachable;
             try sizes.append(size);
         }
     }
-    std.sort.sort(usize, sizes.items, {}, usizeLessThan);
-    var p2 = (sizes.items[sizes.items.len - 3] *
+    std.sort.sort(usize, sizes.items, {}, aoc.usizeLessThan);
+    r[1] = (sizes.items[sizes.items.len - 3] *
         sizes.items[sizes.items.len - 2] *
         sizes.items[sizes.items.len - 1]);
-    return P1P2{ .p1 = p1, .p2 = p2 };
+    return r;
 }
 
 test "examples" {
-    var test1 = lava(test1file) catch unreachable;
-    try assertEq(@as(usize, 15), test1.p1);
-    var real = lava(inputfile) catch unreachable;
-    try assertEq(@as(usize, 456), real.p1);
-    try assertEq(@as(usize, 1134), test1.p2);
-    try assertEq(@as(usize, 1047744), real.p2);
+    var test1 = try lava(aoc.talloc, aoc.test1file);
+    try aoc.assertEq(@as(usize, 15), test1[0]);
+    var real = try lava(aoc.talloc, aoc.inputfile);
+    try aoc.assertEq(@as(usize, 456), real[0]);
+    try aoc.assertEq(@as(usize, 1134), test1[1]);
+    try aoc.assertEq(@as(usize, 1047744), real[1]);
+}
+
+fn day09(inp: []const u8, bench: bool) anyerror!void {
+    var p = try lava(aoc.halloc, inp);
+    if (!bench) {
+        try aoc.print("Part 1: {}\nPart 2: {}\n", .{ p[0], p[1] });
+    }
 }
 
 pub fn main() anyerror!void {
-    var res = try lava(input());
-    try print("Part1: {}\n", .{res.p1});
-    try print("Part2: {}\n", .{res.p2});
-}
-
-pub fn input() []const u8 {
-    var args = std.process.args();
-    _ = args.skip();
-    var res: []const u8 = inputfile;
-    if (args.next(alloc)) |arg1| {
-        alloc.free(arg1 catch unreachable);
-        res = test1file;
-    }
-    return res;
+    try aoc.benchme(aoc.input(), day09);
 }
