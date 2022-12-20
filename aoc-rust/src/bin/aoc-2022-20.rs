d813504e684d276b6879738ce254d8996d22e249
Mark Hindess <soft-github@temporalanomaly.com>
2022-12-20 10:51:44 +0000

    2022 Day 20 Rust

---
+++ aoc-rust/src/bin/aoc-2022-20.rs
@@ -0,0 +1,90 @@
+fn mix(nums: &[isize], rounds: usize, key: isize) -> isize {
+    let len = nums.len();
+    let mut idx: [usize; 5000] = [0; 5000];
+    for i in 0..len {
+        idx[i] = i;
+    }
+    //pretty(&nums, &idx[0..len], key);
+    for _ in 0..rounds {
+        for i in 0..len {
+            let num = nums[i] * key;
+            //eprintln!("moving {}", num);
+            let mut j = 0;
+            while idx[j] != i {
+                j += 1
+            }
+            let ni = (j as isize + num).rem_euclid((len - 1) as isize) as usize;
+            if ni > j {
+                for k in j..ni {
+                    (idx[k], idx[k + 1]) = (idx[k + 1], idx[k]);
+                }
+            } else if ni < j {
+                let mut k = j - 1;
+                loop {
+                    (idx[k], idx[k + 1]) = (idx[k + 1], idx[k]);
+                    if k == ni {
+                        break;
+                    }
+                    k -= 1;
+                }
+            }
+            //pretty(&nums, &idx[0..len], key);
+        }
+    }
+    let mut zero = 0;
+    while nums[zero] != 0 {
+        zero += 1
+    }
+    let mut n_zero = 0;
+    while idx[n_zero] != zero {
+        n_zero += 1;
+    }
+    nums[idx[(n_zero + 1000) % len]] * key
+        + nums[idx[(n_zero + 2000) % len]] * key
+        + nums[idx[(n_zero + 3000) % len]] * key
+}
+
+#[allow(dead_code)]
+fn pretty(nums: &[isize], idx: &[usize], key: isize) {
+    for i in idx {
+        eprint!("{}, ", nums[*i] * key);
+    }
+    eprintln!();
+}
+
+fn parts(inp: &[u8]) -> (isize, isize) {
+    let mut nums: [isize; 5000] = [0; 5000];
+    let mut i = 0;
+    let mut l = 0;
+    while i < inp.len() {
+        let (j, n) = aoc::read::int::<isize>(inp, i);
+        nums[l] = n;
+        l += 1;
+        i = j + 1;
+    }
+    (mix(&nums[0..l], 1, 1), mix(&nums[0..l], 10, 811589153))
+}
+
+fn main() {
+    let inp = std::fs::read(aoc::input_file()).expect("read error");
+    aoc::benchme(|bench: bool| {
+        let (p1, p2) = parts(&inp);
+        if !bench {
+            println!("Part 1: {}", p1);
+            println!("Part 2: {}", p2);
+        }
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parts_works() {
+        let inp = std::fs::read("../2022/20/test1.txt").expect("read error");
+        assert_eq!(parts(&inp), (3, 1623178306));
+        let inp = std::fs::read("../2022/20/input.txt").expect("read error");
+        assert_eq!(parts(&inp), (17490, 1632917375836));
+    }
+}
