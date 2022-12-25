c64d027e7a76c04377fd7bf4d289bbffe91170e7
Mark Hindess <soft-github@temporalanomaly.com>
2022-12-25 13:50:12 +0000

    chore: lib clean up

--- lib-go/chomp.go
+++ lib-go/chomp.go
@@ -1,11 +1,16 @@
 package aoc
 
+import "bytes"
+
 type AoCUnsigned interface { ~uint|~uint8|~uint16|~uint32|~uint64 }
 type AoCSigned interface { ~byte|int|~int8|~int16|~int32|~int64 }
 type AoCInt interface { AoCUnsigned|AoCSigned }
 
 func ChompUInt[T AoCInt](in []byte, i int) (j int, n T) {
 	j = i
+	if !('0' <= in[j] && in[j] <= '9') {
+		panic("not a number")
+	}
 	for ; '0' <= in[j] && in[j] <= '9'; j++ {
 		n = T(10)*n + T(in[j]&0xf)
 	}
@@ -19,6 +24,9 @@ func ChompInt[T AoCSigned](in []byte, i int) (int, T) {
 		negative = true
 		j++
 	}
+	if !('0' <= in[j] && in[j] <= '9') {
+		panic("not a number")
+	}
 	for ; '0' <= in[j] && in[j] <= '9'; j++ {
 		n = T(10)*n + T(in[j]&0xf)
 	}
@@ -29,17 +37,18 @@ func ChompInt[T AoCSigned](in []byte, i int) (int, T) {
 }
 
 func ChompToNextLine(in []byte, i int) int {
-	for in[i] != '\n' {
-		i++
+	j := bytes.IndexByte(in[i:], '\n')
+	if j == -1 {
+		return len(in)
 	}
-	i++
-	return i
+	return i+j+1
 }
 
-var isDigit = [256]bool{48: true, 49: true, 50: true, 51: true, 52: true, 53: true, 54: true, 55: true, 56: true, 57: true}
-
 func ChompOneOrTwoCharUInt[T AoCInt](in []byte, i int) (int, T) {
-	if isDigit[in[i+1]] {
+	if !('0' <= in[i] && in[i] <= '9') {
+		panic("not a number")
+	}
+	if '0' <= in[i+1] && in[i+1] <= '9' {
 		return i + 2, 10*T(in[i]&0xf) + T(in[i+1]&0xf)
 	}
 	return i + 1, T(in[i]&0xf)
---
+++ lib-go/chomp_test.go
@@ -0,0 +1,82 @@
+package aoc
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+)
+
+func Test_ChompUInt(t *testing.T) {
+	i, n := ChompUInt[int]([]byte("foo 123\n"), 4)
+	assert.Equal(t, i, 7)
+	assert.Equal(t, n, 123)
+	assert.Panics(t, func() {
+		ChompUInt[int]([]byte("foo"), 0)
+	})
+}
+
+func Test_ChompInt(t *testing.T) {
+	i, n := ChompInt[int]([]byte("foo 123\n"), 4)
+	assert.Equal(t, i, 7)
+	assert.Equal(t, n, 123)
+	i, n = ChompInt[int]([]byte("foo -123\n"), 4)
+	assert.Equal(t, i, 8)
+	assert.Equal(t, n, -123)
+	assert.Panics(t, func() {
+		ChompUInt[int]([]byte("foo"), 0)
+	})
+}
+
+func Test_ChompToNextLine(t *testing.T) {
+	in := []byte("1\nthis is a test of the chomp to next line\n012345\n")
+	i := ChompToNextLine(in, 3)
+	assert.Equal(t, i, 43)
+	assert.Equal(t, ChompToNextLine(in, 43), 50)
+}
+
+func Benchmark_ChompToNextLine(b *testing.B) {
+	in := []byte("1\nthis is a test of the chomp to next line\n012345\n")
+	for i := 0; i < b.N; i++ {
+		i := ChompToNextLine(in, 3)
+		j := ChompToNextLine(in, i)
+		if j != 50 {
+			panic("error")
+		}
+	}
+}
+
+func Test_ChompOneOrTwoCharUInt(t *testing.T) {
+	b := []byte("foo 1 2 3 4 12 13 14 15")
+	i, n := ChompUInt[int](b, 4)
+	assert.Equal(t, i, 5)
+	assert.Equal(t, n, 1)
+	i, n = ChompUInt[int](b, i+1)
+	assert.Equal(t, n, 2)
+	i, n = ChompUInt[int](b, i+1)
+	assert.Equal(t, n, 3)
+	i, n = ChompUInt[int](b, i+1)
+	assert.Equal(t, n, 4)
+	i, n = ChompUInt[int](b, i+1)
+	assert.Equal(t, n, 12)
+	i, n = ChompUInt[int](b, i+1)
+	assert.Equal(t, n, 13)
+	i, n = ChompUInt[int](b, i+1)
+	assert.Equal(t, i, 20)
+	assert.Equal(t, n, 14)
+	assert.Panics(t, func() {
+		ChompOneOrTwoCharUInt[int]([]byte("foo"), 0)
+	})
+}
+
+func Benchmark_ChompOneOrTwoCharUInt(b *testing.B) {
+	in := []byte("foo 1 2 3 4 12 13 14 15")
+	for i := 0; i < b.N; i++ {
+		j, _ := ChompUInt[int](in, 4)
+		j, _ = ChompUInt[int](in, j+1)
+		j, _ = ChompUInt[int](in, j+1)
+		j, _ = ChompUInt[int](in, j+1)
+		j, _ = ChompUInt[int](in, j+1)
+		j, _ = ChompUInt[int](in, j+1)
+		j, _ = ChompUInt[int](in, j+1)
+	}
+}
