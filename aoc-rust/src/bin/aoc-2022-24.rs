7c7dcafd1dd8889fe202454b9b3a24984991959c
Mark Hindess <soft-github@temporalanomaly.com>
2022-12-27 17:20:47 +0000

    2022 Day 24 Rust pre-calc snow

--- aoc-rust/src/bin/aoc-2022-24.rs
+++ aoc-rust/src/bin/aoc-2022-24.rs
@@ -13,10 +13,7 @@ struct Basin<'a> {
     h: usize,
     sx: usize,
     sy: usize,
-    snow_r_l: [u128; 27],
-    snow_r_r: [u128; 27],
-    snow_c_u: [u32; 122],
-    snow_c_d: [u32; 122],
+    snow: [[u32; 122]; 1000],
 }
 
 #[derive(Debug)]
@@ -69,24 +66,37 @@ impl<'a> Basin<'a> {
             }
         }
         let h = inp.len() / (w + 1);
-        let mut snow_r_l = [0; 27];
-        let mut snow_r_r = [0; 27];
-        let mut snow_c_u = [0; 122];
-        let mut snow_c_d = [0; 122];
-        let mut r_bit = 2;
-        for y in 1..h - 1 {
-            let mut c_bit = 2;
-            for x in 1..w - 1 {
-                match inp[x + y * (w + 1)] {
-                    b'<' => snow_r_l[y] |= c_bit,
-                    b'>' => snow_r_r[y] |= c_bit,
-                    b'^' => snow_c_u[x] |= r_bit,
-                    b'v' => snow_c_d[x] |= r_bit,
-                    _ => {}
+        let mut snow = [[0; 122]; 1000];
+        for sy in 1..h - 1 {
+            for sx in 1..w - 1 {
+                let (ix, iy): (isize, isize) = match inp[sx + sy * (w + 1)] {
+                    b'<' => (-1, 0),
+                    b'>' => (1, 0),
+                    b'^' => (0, -1),
+                    b'v' => (0, 1),
+                    _ => {
+                        continue;
+                    }
                 };
-                c_bit <<= 1;
+                let (mut x, mut y) = (sx, sy);
+                snow[0][x] |= 2 << y;
+                for t in 1..1000 {
+                    (x, y) = ((x as isize + ix) as usize, (y as isize + iy) as usize);
+                    if x == 0 {
+                        x = w - 2;
+                    }
+                    if y == 0 {
+                        y = h - 2;
+                    }
+                    if x == w - 1 {
+                        x = 1;
+                    }
+                    if y == h - 1 {
+                        y = 1;
+                    }
+                    snow[t][x] |= 2 << y;
+                }
             }
-            r_bit <<= 1;
         }
         Basin {
             m: inp,
@@ -94,10 +104,7 @@ impl<'a> Basin<'a> {
             h,
             sx,
             sy,
-            snow_r_l,
-            snow_r_r,
-            snow_c_u,
-            snow_c_d,
+            snow,
         }
     }
     fn parts(&self) -> (usize, usize) {
@@ -134,7 +141,7 @@ impl<'a> Basin<'a> {
                 if self.wall(nx, ny) {
                     continue;
                 }
-                if let Some(_) = self.snow(nx, ny, cur.t) {
+                if self.snow(nx, ny, cur.t) {
                     continue;
                 }
                 todo.push_back(Search::new(nx, ny, cur.t + 1, cur.start, cur.end));
@@ -145,34 +152,19 @@ impl<'a> Basin<'a> {
     fn wall(&self, x: usize, y: usize) -> bool {
         self.m[x + y * (self.w + 1)] == b'#'
     }
-    fn snow(&self, x: usize, y: usize, t: usize) -> Option<u8> {
+    fn snow(&self, x: usize, y: usize, t: usize) -> bool {
         if !(x > 0 && y > 0 && x < self.w - 1 && y < self.h - 1) {
-            return None;
+            return false;
         }
-        let c_r_bit = 1 << (1 + ((x - 1 + t * (self.w - 3)) % (self.w - 2)));
-        let c_l_bit = 1 << (1 + ((x - 1 + t) % (self.w - 2)));
-        let r_u_bit = 1 << (1 + ((y - 1 + t) % (self.h - 2)));
-        let r_d_bit = 1 << (1 + ((y - 1 + t * (self.h - 3)) % (self.h - 2)));
-        match (
-            c_r_bit & self.snow_r_r[y] != 0,
-            c_l_bit & self.snow_r_l[y] != 0,
-            r_u_bit & self.snow_c_u[x] != 0,
-            r_d_bit & self.snow_c_d[x] != 0,
-        ) {
-            (true, false, false, false) => Some(b'>'),
-            (false, true, false, false) => Some(b'<'),
-            (false, false, true, false) => Some(b'^'),
-            (false, false, false, true) => Some(b'v'),
-            (false, false, false, false) => None,
-            (_, _, _, _) => Some(b'*'),
-        }
+        let r_bit = 2 << y;
+        r_bit & self.snow[t][x] != 0
     }
     #[allow(dead_code)]
     fn pretty(&self, t: usize) {
         for y in 0..self.h {
             for x in 0..self.w {
-                if let Some(snow) = self.snow(x, y, t) {
-                    eprint!("{}", snow as char);
+                if self.snow(x, y, t) {
+                    eprint!("{}", b'*' as char);
                     continue;
                 }
                 eprint!("{}", if self.wall(x, y) { "#" } else { "." });
