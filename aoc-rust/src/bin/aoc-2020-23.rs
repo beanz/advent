dfeceacb266017f82f6defeec0d16e15cf57215d
Mark Hindess <soft-github@temporalanomaly.com>
2023-01-02 20:38:58 +0000

    2020 Day 23 Rust speed up

--- aoc-rust/src/bin/aoc-2020-23.rs
+++ aoc-rust/src/bin/aoc-2020-23.rs
@@ -1,71 +1,71 @@
 fn parts(inp: &[u8]) -> (usize, usize) {
-    let mut cups: [usize; 9] = [0; 9];
+    let mut cups: [u8; 9] = [0; 9];
     for i in 0..8 {
         let c = (inp[i] & 0xf) as usize;
-        let n = (inp[i + 1] & 0xf) as usize;
+        let n = inp[i + 1] & 0xf;
         cups[c - 1] = n - 1;
     }
-    let mut cur = (inp[0] & 0xf) as usize - 1;
+    let mut cur = (inp[0] & 0xf) - 1;
     let last = (inp[8] & 0xf) as usize - 1;
     cups[last] = cur;
 
     for _ in 0..100 {
-        let r1 = cups[cur];
-        let r2 = cups[r1];
-        let r3 = cups[r2];
-        let next = cups[r3];
-        cups[cur] = next;
+        let r1 = cups[cur as usize];
+        let r2 = cups[r1 as usize];
+        let r3 = cups[r2 as usize];
+        let next = cups[r3 as usize];
+        cups[cur as usize] = next;
         let mut new = if cur == 0 { 8 } else { cur - 1 };
         while new == r1 || new == r2 || new == r3 {
             new = if new == 0 { 8 } else { new - 1 };
         }
-        let new_next = cups[new];
-        cups[r3] = new_next;
-        cups[new] = r1;
+        let new_next = cups[new as usize];
+        cups[r3 as usize] = new_next;
+        cups[new as usize] = r1;
         cur = next;
     }
     let p1 = part1(&cups);
-    let mut cups: [usize; 1_000_000] = [0; 1_000_000];
+    let mut cups: [u32; 1_000_000] = [0; 1_000_000];
     for i in 0..8 {
         let c = (inp[i] & 0xf) as usize;
-        let n = (inp[i + 1] & 0xf) as usize;
+        let n = (inp[i + 1] & 0xf) as u32;
         cups[c - 1] = n - 1;
     }
-    let mut cur = (inp[0] & 0xf) as usize - 1;
+    let mut cur = (inp[0] & 0xf) as u32 - 1;
     let last = (inp[8] & 0xf) as usize - 1;
     cups[last] = 9;
     for i in 9..1_000_000 - 1 {
-        cups[i] = i + 1;
+        cups[i] = (i + 1) as u32;
     }
     cups[1_000_000 - 1] = cur;
 
     //eprintln!("{}", part1(&cups));
     for _ in 0..10_000_000 {
-        let r1 = cups[cur];
-        let r2 = cups[r1];
-        let r3 = cups[r2];
-        let next = cups[r3];
-        cups[cur] = next;
+        let r1 = cups[cur as usize];
+        let r2 = cups[r1 as usize];
+        let r3 = cups[r2 as usize];
+        let next = cups[r3 as usize];
+        cups[cur as usize] = next;
         let mut new = if cur == 0 { 1_000_000 - 1 } else { cur - 1 };
         while new == r1 || new == r2 || new == r3 {
             new = if new == 0 { 1_000_000 - 1 } else { new - 1 };
         }
         //eprintln!("removed [{} {} {}]", r1 + 1, r2 + 1, r3 + 1);
         //eprintln!("{} -> {} / {}", cur + 1, next + 1, new + 1);
-        let new_next = cups[new];
-        cups[r3] = new_next;
-        cups[new] = r1;
+        let new_next = cups[new as usize];
+        cups[r3 as usize] = new_next;
+        cups[new as usize] = r1;
         //eprintln!("{}", part1(&cups));
         cur = next;
     }
-    let p2a = cups[0];
-    let p2b = cups[p2a];
+    let p2a = cups[0] as usize;
+    let p2b = cups[p2a] as usize;
     //eprintln!("{} x {} = {}", (p2a + 1), (p2b + 1), (p2a + 1) * (p2b + 1));
     let p2 = (p2a + 1) * (p2b + 1);
     (p1, p2)
 }
 
-fn part1(cups: &[usize]) -> usize {
+fn part1(cups: &[u8]) -> usize {
     let mut cup = cups[0] as usize;
     let mut p1 = cup + 1;
     loop {
