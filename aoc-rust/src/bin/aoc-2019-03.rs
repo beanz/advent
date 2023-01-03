5d9f309531b3068afe183f65836797ac5bddf284
Mark Hindess <soft-github@temporalanomaly.com>
2023-01-03 20:02:03 +0000

    2019 Day 3 Rust speed up

--- aoc-rust/src/bin/aoc-2019-03.rs
+++ aoc-rust/src/bin/aoc-2019-03.rs
@@ -1,51 +1,94 @@
-use std::collections::HashMap;
+#[derive(Debug, Clone, Copy, Default)]
+struct Line {
+    x: i16,
+    y: i16,
+    d: i16,
+    step: u32,
+}
 
 fn parts(inp: &[u8]) -> (u32, u32) {
-    let mut points: HashMap<(i16, i16), (u32, u32)> = HashMap::new();
-    let mut p1 = u32::MAX;
-    let mut p2 = u32::MAX;
-    let mut i = 0;
-    let mut l = 0;
+    let mut hor: Vec<Line> = Vec::new();
+    let mut vrt: Vec<Line> = Vec::new();
     let mut x = 0i16;
     let mut y = 0i16;
     let mut step = 0u32;
-    while i < inp.len() {
-        loop {
-            let d = match inp[i] {
-                b'U' => (0, -1),
-                b'D' => (0, 1),
-                b'L' => (-1, 0),
-                b'R' => (1, 0),
-                _ => unreachable!("invalid direction"),
-            };
-            let (j, n) = aoc::read::uint::<usize>(inp, i + 1);
-            i = j;
-            for _ in 0..n {
-                x += d.0;
-                y += d.1;
-                step += 1;
-                if l == 0 {
-                    points.insert((x, y), ((x.abs() + y.abs()) as u32, step));
-                    continue;
-                }
-                let prev = match points.get(&(x, y)) {
-                    None => continue,
-                    Some(x) => x,
-                };
-                if p1 > prev.0 {
-                    p1 = prev.0;
-                }
-                if p2 > step + prev.1 {
-                    p2 = step + prev.1;
-                }
-            }
-            if inp[i] == b'\n' {
-                break;
-            }
-            i += 1;
+    let mut i = 0;
+    loop {
+        let (mut d, vert) = match inp[i] {
+            b'U' => (-1, true),
+            b'D' => (1, true),
+            b'L' => (-1, false),
+            b'R' => (1, false),
+            _ => unreachable!("invalid direction"),
+        };
+        let (j, n) = aoc::read::uint::<i16>(inp, i + 1);
+        i = j;
+        d *= n;
+        if vert {
+            vrt.push(Line { x, y, d, step });
+            y += d;
+        } else {
+            hor.push(Line { x, y, d, step });
+            x += d;
         }
-        l += 1;
-        (x, y, step) = (0, 0, 0);
+        step += n as u32;
+        if inp[i] == b'\n' {
+            break;
+        }
+        i += 1;
+    }
+    i += 1;
+    let mut p1 = u32::MAX;
+    let mut p2 = u32::MAX;
+    (x, y, step) = (0, 0, 0);
+    loop {
+        let (mut d, vert) = match inp[i] {
+            b'U' => (-1, true),
+            b'D' => (1, true),
+            b'L' => (-1, false),
+            b'R' => (1, false),
+            _ => unreachable!("invalid direction"),
+        };
+        let (j, n) = aoc::read::uint::<i16>(inp, i + 1);
+        i = j;
+        d *= n;
+        if vert {
+            for h in &hor {
+                let (min_x, max_x) = (h.x.min(h.x + h.d), h.x.max(h.x + h.d));
+                let (min_y, max_y) = (y.min(y + d), y.max(y + d));
+                if min_x < x && x < max_x && min_y < h.y && h.y < max_y {
+                    let md = x.abs() as u32 + h.y.abs() as u32;
+                    if md < p1 {
+                        p1 = md;
+                    }
+                    let st = step + y.abs_diff(h.y) as u32 + h.step + x.abs_diff(h.x) as u32;
+                    if st < p2 {
+                        p2 = st;
+                    }
+                }
+            }
+            y += d;
+        } else {
+            for v in &vrt {
+                let (min_y, max_y) = (v.y.min(v.y + v.d), v.y.max(v.y + v.d));
+                let (min_x, max_x) = (x.min(x + d), x.max(x + d));
+                if min_y < y && y < max_y && min_x < v.x && v.x < max_x {
+                    let md = v.x.abs() as u32 + y.abs() as u32;
+                    if md < p1 {
+                        p1 = md;
+                    }
+                    let st = step + x.abs_diff(v.x) as u32 + v.step + y.abs_diff(v.y) as u32;
+                    if st < p2 {
+                        p2 = st;
+                    }
+                }
+            }
+            x += d;
+        }
+        step += n as u32;
+        if inp[i] == b'\n' {
+            break;
+        }
         i += 1;
     }
     (p1, p2)
