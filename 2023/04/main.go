3463812dc0be437e6a6f37090a3fd351c64aacc4
beanz <79058+beanz@users.noreply.github.com>
2023-12-04 06:16:55 +0000

    2023 Day 4 Go

---
+++ 2023/04/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+
+	. "github.com/beanz/advent/lib-go"
+)
+
+//go:embed input.txt
+var input []byte
+
+func Parts(in []byte) (int, int) {
+	p1, p2 := 0, 0
+	i := 0
+	copies := make([][2]int, 0, 8)
+	for i < len(in) {
+		c1 := [256]bool{}
+		p := 0
+		for in[i] != ':' {
+			i++
+		}
+		i += 2
+		for in[i] != '|' {
+			for in[i] == ' ' {
+				i++
+			}
+			var n int
+			i, n = ChompUInt[int](in, i)
+			c1[n] = true
+			i++
+		}
+		i += 2
+		for {
+			for in[i] == ' ' {
+				i++
+			}
+			var n int
+			i, n = ChompUInt[int](in, i)
+			if c1[n] {
+				p++
+			}
+			if in[i] == '\n' {
+				break
+			}
+			i++
+		}
+		if p > 0 {
+			p1 += 1 << (p - 1)
+		}
+		n := 1
+		k := 0
+		for _, e := range copies {
+			e[0]--
+			n += e[1]
+			if e[0] > 0 {
+				copies[k] = e
+				k++
+			}
+		}
+		copies = copies[:k]
+		p2 += n
+		if p > 0 {
+			copies = append(copies, [2]int{p, n})
+		}
+		i++
+	}
+	return p1, p2
+}
+
+func main() {
+	p1, p2 := Parts(InputBytes(input))
+	if !benchmark {
+		fmt.Printf("Part 1: %d\n", p1)
+		fmt.Printf("Part 2: %d\n", p2)
+	}
+}
+
+var benchmark = false
---
+++ 2023/04/main_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+)
+
+//go:embed test1.txt
+var test1 []byte
+
+//go:embed input.txt
+var safeinput []byte
+
+func ExampleMain() {
+	main()
+	//Output:
+	// Part 1: 25183
+	// Part 2: 5667240
+}
+
+func TestParts(t *testing.T) {
+	tests := []struct {
+		file   string
+		data   []byte
+		p1, p2 int
+	}{
+		{"test1.txt", test1, 13, 30},
+		{"input.txt", input, 25183, 5667240},
+	}
+	for _, tc := range tests {
+		p1, p2 := Parts(tc.data)
+		assert.Equal(t, tc.p1, p1, tc.file)
+		assert.Equal(t, tc.p2, p2, tc.file)
+	}
+}
+
+func BenchmarkMain(b *testing.B) {
+	benchmark = true
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		copy(input, safeinput)
+		b.StartTimer()
+		main()
+	}
+}
