0a0a180b3af5c9f646c0e66c74c80ebc37e4a827
beanz <79058+beanz@users.noreply.github.com>
2023-12-12 21:10:58 +0000

    2023 Day 5 Go partial rewrite

--- 2023/05/main.go
+++ 2023/05/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	_ "embed"
 	"fmt"
+	"os"
+	"sort"
 
 	. "github.com/beanz/advent/lib-go"
 )
@@ -11,162 +13,48 @@ import (
 var input []byte
 
 func Parts(in []byte) (int, int) {
-	seeds := []int{}
+	seeds := make([]int, 0, 20)
 	i := 0
 	VisitUints[int](in, '\n', &i, func(n int) {
 		seeds = append(seeds, n)
 	})
+	seed_ranges := make([][2]int, 0, 10)
+	for j := 0; j < len(seeds); j += 2 {
+		seed_ranges = append(seed_ranges, [2]int{seeds[j], seeds[j+1]})
+	}
 	i += 2
-	for in[i] != '\n' {
-		i++
-	}
-	seedToSoil := [][3]int{}
-	for i++; i <= len(in); i++ {
-		s := [3]int{}
-		j := 0
-		VisitUints[int](in, '\n', &i, func(n int) {
-			s[j] = n
-			j++
-		})
-		seedToSoil = append(seedToSoil, s)
-		if i+1 >= len(in) || in[i+1] == '\n' {
-			break
+	for i < len(in) {
+		for in[i] != '\n' {
+			i++
 		}
-	}
 
-	for i += 2; in[i] != '\n'; {
-		i++
-	}
-	soilToFert := [][3]int{}
-	for i++; i <= len(in); i++ {
-		s := [3]int{}
-		j := 0
-		VisitUints[int](in, '\n', &i, func(n int) {
-			s[j] = n
-			j++
-		})
-		soilToFert = append(soilToFert, s)
-		if i+1 >= len(in) || in[i+1] == '\n' {
-			break
+		tf := make([][3]int, 0, 64)
+		for i++; i <= len(in); i++ {
+			fmt.Fprintf(os.Stderr, "I: %s\n", string(in[i:i+3]))
+			s := [3]int{}
+			j := 0
+			VisitUints[int](in, '\n', &i, func(n int) {
+				s[j] = n
+				j++
+			})
+			tf = append(tf, s)
+			if i+1 >= len(in) || in[i+1] == '\n' {
+				break
+			}
 		}
-	}
-
-	for i += 2; in[i] != '\n'; {
 		i++
-	}
-	fertToWater := [][3]int{}
-	for i++; i <= len(in); i++ {
-		s := [3]int{}
-		j := 0
-		VisitUints[int](in, '\n', &i, func(n int) {
-			s[j] = n
-			j++
-		})
-		fertToWater = append(fertToWater, s)
-		if i+1 >= len(in) || in[i+1] == '\n' {
-			break
-		}
-	}
-
-	for i += 2; in[i] != '\n'; {
-		i++
-	}
-	waterToLight := [][3]int{}
-	for i++; i <= len(in); i++ {
-		s := [3]int{}
-		j := 0
-		VisitUints[int](in, '\n', &i, func(n int) {
-			s[j] = n
-			j++
-		})
-		waterToLight = append(waterToLight, s)
-		if i+1 >= len(in) || in[i+1] == '\n' {
-			break
-		}
-	}
-
-	for i += 2; in[i] != '\n'; {
-		i++
-	}
-	lightToTemp := [][3]int{}
-	for i++; i <= len(in); i++ {
-		s := [3]int{}
-		j := 0
-		VisitUints[int](in, '\n', &i, func(n int) {
-			s[j] = n
-			j++
-		})
-		lightToTemp = append(lightToTemp, s)
-		if i+1 >= len(in) || in[i+1] == '\n' {
-			break
-		}
-	}
-
-	for i += 2; in[i] != '\n'; {
-		i++
-	}
-	tempToHumidity := [][3]int{}
-	for i++; i <= len(in); i++ {
-		s := [3]int{}
-		j := 0
-		VisitUints[int](in, '\n', &i, func(n int) {
-			s[j] = n
-			j++
-		})
-		tempToHumidity = append(tempToHumidity, s)
-		if i+1 >= len(in) || in[i+1] == '\n' {
-			break
-		}
-	}
-
-	for i += 2; in[i] != '\n'; {
-		i++
-	}
-	humidityToLocation := [][3]int{}
-	for i++; i <= len(in); i++ {
-		s := [3]int{}
-		j := 0
-		VisitUints[int](in, '\n', &i, func(n int) {
-			s[j] = n
-			j++
-		})
-		humidityToLocation = append(humidityToLocation, s)
-		if i+1 >= len(in) || in[i+1] == '\n' {
-			break
-		}
-	}
-
-	p1 := 99999999999
-	for _, s := range seeds {
-		for _, m := range [][][3]int{seedToSoil, soilToFert, fertToWater, waterToLight, lightToTemp, tempToHumidity, humidityToLocation} {
-			for _, r := range m {
-				if r[1] <= s && s < r[1]+r[2] {
-					s = r[0] + (s - r[1])
+		for j := range seeds {
+			for _, r := range tf {
+				if r[1] <= seeds[j] && seeds[j] < r[1]+r[2] {
+					seeds[j] = r[0] + (seeds[j] - r[1])
 					break
 				}
 			}
 		}
-		if s < p1 {
-			p1 = s
-		}
+		fmt.Fprintf(os.Stderr, "%v\n", seeds)
 	}
-	p2 := 0
-	for ; ; p2++ {
-		s := p2
-		for _, m := range [][][3]int{humidityToLocation, tempToHumidity, lightToTemp, waterToLight, fertToWater, soilToFert, seedToSoil} {
-			for _, r := range m {
-				if r[0] <= s && s < r[0]+r[2] {
-					s = r[1] + (s - r[0])
-					break
-				}
-			}
-		}
-		for k := 0; k < len(seeds); k += 2 {
-			if seeds[k] <= s && s < seeds[k]+seeds[k+1] {
-				return p1, p2
-			}
-		}
-	}
+	sort.Ints(seeds)
+	return seeds[0], 2
 }
 
 func main() {
