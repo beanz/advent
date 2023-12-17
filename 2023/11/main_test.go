019236b1f02d0ceb2dab653a226e42649c2a06f8
beanz <79058+beanz@users.noreply.github.com>
2023-12-17 14:15:00 +0000

    chore: drop some redundant tests

--- 2023/11/main_test.go
+++ 2023/11/main_test.go
@@ -10,13 +10,6 @@ import (
 //go:embed input.txt
 var safeinput []byte
 
-func ExampleMain() {
-	main()
-	//Output:
-	// Part 1: 9918828
-	// Part 2: 692506533832
-}
-
 func TestParts(t *testing.T) {
 	tester.RunWithArgs(t, Parts)
 }
--- 2023/12/main_test.go
+++ 2023/12/main_test.go
@@ -10,13 +10,6 @@ import (
 //go:embed input.txt
 var safeinput []byte
 
-func ExampleMain() {
-	main()
-	//Output:
-	// Part 1: 6488
-	// Part 2: 815364548481
-}
-
 func TestParts(t *testing.T) {
 	tester.RunWithArgs(t, Parts)
 }
