4cd8f7816fc6d79a68b14683df00be09c033978c
beanz <79058+beanz@users.noreply.github.com>
2023-12-17 11:50:25 +0000

    chore: missing only if attempted that year

--- bin/missing.sh
+++ bin/missing.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-F="aoc.pl aoc.raku cpp/aoc.cpp aoc.zig aoc.cr aoc.nim main.go"
+dir=$(readlink -m ${0%/*})/..
+cd $dir
+F="aoc.pl aoc.raku cpp/aoc.cpp aoc.zig aoc.cr aoc.nim main.go TC.txt"
 for d in ????/?? ; do
   if [ ! -e $d/input.txt ]; then
     continue
   fi
-  for f in $F; do
-    test -f $d/$f || echo $d/$f missing
-  done
   Y=${d%/*}
   D=${d#*/}
-  R=aoc-rust/src/bin/aoc-$Y-$D.rs
-  test -f $R || echo $R missing
+  for f in $F; do
+    a="$(echo "${Y}"/*/$f)"
+    if [ -f "${a%% *}" ]; then
+      test -f $d/$f || echo $d/$f missing
+    fi
+  done
+  a="$(echo aoc-rust/src/bin/aoc-"${Y}"-??.rs)"
+  if [ -f "${a%% *}" ]; then
+    R=aoc-rust/src/bin/aoc-$Y-$D.rs
+    test -f $R || echo $R missing
+  fi
 done
