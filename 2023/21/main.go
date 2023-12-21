954669ec1f646c8d1c1b38287bdf6b68ab9629d9
beanz <79058+beanz@users.noreply.github.com>
2023-12-21 16:21:08 +0000

    2023 Day 21 Go slow

---
+++ 2023/21/main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bytes"
+	_ "embed"
+	"fmt"
+
+	. "github.com/beanz/advent/lib-go"
+)
+
+//go:embed input.txt
+var input []byte
+
+func Parts(in []byte, args ...int) (int, int) {
+	w := int16(bytes.Index(in, []byte{'\n'}))
+	s := int16(bytes.Index(in, []byte{'S'}))
+	w1 := int16(w + 1)
+	h := int16(len(in)) / w1
+	rp := make(map[[2]int16]struct{}, 102400)
+	rp[[2]int16{s % w1, s / w1}] = struct{}{}
+	np := make(map[[2]int16]struct{}, 102400)
+	p1 := 0
+	chAt1 := func(x, y int16) byte {
+		if 0 <= x && x < w && 0 <= y && y < h {
+			return in[x+y*w1]
+		}
+		return '#'
+	}
+
+	iter := func(chAtFn func(x, y int16) byte) int {
+		for p := range rp {
+			x, y := p[0], p[1]
+			if chAtFn(x-1, y) != '#' {
+				np[[2]int16{x - 1, y}] = struct{}{}
+			}
+			if chAtFn(x+1, y) != '#' {
+				np[[2]int16{x + 1, y}] = struct{}{}
+			}
+			if chAtFn(x, y-1) != '#' {
+				np[[2]int16{x, y - 1}] = struct{}{}
+			}
+			if chAtFn(x, y+1) != '#' {
+				np[[2]int16{x, y + 1}] = struct{}{}
+			}
+		}
+		rp, np = np, rp
+		clear(np)
+		return len(rp)
+	}
+
+	for step := 1; step <= 64; step++ {
+		p1 = iter(chAt1)
+	}
+	clear(rp)
+	rp[[2]int16{s % w1, s / w1}] = struct{}{}
+	target := 26501365
+	mod := target % int(w)
+	seen := make([]int, 0, 3)
+	chAt := func(x, y int16) byte {
+		return in[Mod(x, w)+w1*Mod(y, h)]
+	}
+
+	for step := 1; step <= 1000; step++ {
+		c := iter(chAt)
+		if (step % int(w)) == mod {
+			seen = append(seen, c)
+			if len(seen) == 3 {
+				break
+			}
+		}
+	}
+
+	x := 1 + (target / int(w))
+	a := ((seen[2] - seen[1]) - (seen[1] - seen[0])) / 2
+	b := (seen[1] - seen[0]) - 3*a
+	c := seen[0] - b - a
+	p2 := a*x*x + b*x + c
+	return p1, p2
+}
+
+func main() {
+	p1, p2 := Parts(InputBytes(input))
+	if !benchmark {
+		fmt.Printf("Part 1: %d\n", p1)
+		fmt.Printf("Part 2: %d\n", p2)
+	}
+}
+
+var benchmark = false
