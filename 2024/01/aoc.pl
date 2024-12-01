f1f9337d54678a8bc1517ecd4ca2f14665d951d7
beanz <79058+beanz@users.noreply.github.com>
2024-12-01 05:45:25 +0000

    2024 Day 1 Perl and Go

---
+++ 2024/01/aoc.pl
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+use warnings FATAL => 'all';
+use strict;
+use v5.10;
+use lib "../../lib-perl";
+no warnings 'portable';
+use AoC::Helpers qw/:all/;
+
+#use Carp::Always qw/carp verbose/;
+#use Algorithm::Combinatorics qw/permutations combinations/;
+$; = $" = ',';
+
+my $file = shift // "input.txt";
+
+my $reader = \&read_stuff;
+my $i = $reader->($file);
+my $i2 = $reader->($file);
+
+sub read_stuff {
+  my $file = shift;
+  my $in = read_lines($file);
+  my %m = (a => [], b => [], ac => {}, bc => {});
+  for (@$in) {
+    my ($a, $b) = split;
+    push @{$m{a}}, $a;
+    push @{$m{b}}, $b;
+    $m{ac}->{$a}++;
+    $m{bc}->{$b}++;
+  }
+  return \%m;
+}
+
+sub calc {
+  my ($in) = @_;
+  my $p1 = 0;
+  my @a = sort { $a <=> $b } @{$in->{a}};
+  my @b = sort { $a <=> $b } @{$in->{b}};
+  for my $i (0..$#a) {
+    $p1 += abs($a[$i]-$b[$i]);
+  }
+  my $p2 = 0;
+  for my $a (keys %{$in->{ac}}) {
+    $p2 += $a * $in->{ac}->{$a} * ($in->{bc}->{$a}//0);
+  }
+  return [$p1, $p2];
+}
+
+RunTests(sub {my $f = shift; calc($reader->($f), @_)}) if (TEST);
+
+my $res = calc($i);
+printf "Part 1: %s\nPart 2: %s\n", @$res;
---
+++ 2024/01/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"sort"
+
+	. "github.com/beanz/advent/lib-go"
+)
+
+//go:embed input.txt
+var input []byte
+
+type Counts struct{ a, b int }
+
+func Parts(in []byte, args ...int) (int, int) {
+	a := make([]int, 0, 1024)
+	b := make([]int, 0, 1024)
+	for i := 0; i < len(in); {
+		var n int
+		i, n = ChompInt[int](in, i)
+		a = append(a, n)
+		for ; in[i] == ' '; i++ {
+		}
+		i, n = ChompInt[int](in, i)
+		b = append(b, n)
+		i++
+	}
+	sort.Ints(a)
+	sort.Ints(b)
+	p1 := 0
+	for i := 0; i < len(a); i++ {
+		p1 += Abs(a[i] - b[i])
+	}
+	p2 := 0
+	for ai, bi := 0, 0; ai < len(a) && bi < len(b); {
+		if a[ai] < b[bi] {
+			ai++
+			continue
+		}
+		if a[ai] > b[bi] {
+			bi++
+			continue
+		}
+		ac := 0
+		v := a[ai]
+		for ; ai < len(a) && a[ai] == v; ai++ {
+			ac++
+		}
+		bc := 0
+		for ; bi < len(b) && b[bi] == v; bi++ {
+			bc++
+		}
+		p2 += v * ac * bc
+	}
+	return p1, p2
+}
+
+func main() {
+	p1, p2 := Parts(InputBytes(input))
+	if !benchmark {
+		fmt.Printf("Part 1: %d\n", p1)
+		fmt.Printf("Part 2: %d\n", p2)
+	}
+}
+
+var benchmark = false
