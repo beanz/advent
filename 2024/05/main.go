31b1b7c7a8cab6990d08ec35ab2cc22314c9f774
beanz <79058+beanz@users.noreply.github.com>
2024-12-05 06:32:29 +0000

    2024 Day 5 Go faster
    This relies a little on the sort implementation but the nums2
    check could be put back without losing too much performance if
    it becomes a problem.

--- 2024/05/main.go
+++ 2024/05/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"os"
-	"sort"
+	"slices"
 
 	. "github.com/beanz/advent/lib-go"
 )
@@ -13,13 +12,12 @@ import (
 var input []byte
 
 func Parts(in []byte, args ...int) (int, int) {
-	rules := make(map[int]bool, 2048)
+	rules := make(map[int]struct{}, 2048)
 	i := 0
 	for i < len(in) {
 		j, a := ChompUInt[int](in, i)
 		j, b := ChompUInt[int](in, j+1)
-		rules[(a<<16)+b] = true
-		rules[(b<<16)+a] = false
+		rules[(a<<16)+b] = struct{}{}
 		i = j + 1
 		if in[i] == '\n' {
 			i++
@@ -28,42 +26,31 @@ func Parts(in []byte, args ...int) (int, int) {
 	}
 	p1, p2 := 0, 0
 	nums := make([]int, 0, 128)
-	nums2 := make([]int, 0, 128)
 	for i < len(in) {
 		for i < len(in) {
 			j, a := ChompUInt[int](in, i)
 			i = j + 1
 			nums = append(nums, a)
-			nums2 = append(nums2, a)
 			if in[j] == '\n' {
 				break
 			}
 		}
 		mid1 := nums[len(nums)/2]
-		sort.Slice(nums, func(i, j int) bool {
-			v, ok := rules[(nums[i]<<16)+nums[j]]
-			if !ok {
-				fmt.Fprintf(os.Stderr, "not found %d|%d %d\n", nums[j], nums[i],
-					(nums[i]<<16)+nums[j])
-				return true
+		var changed bool
+		slices.SortFunc(nums, func(a, b int) int {
+			if _, ok := rules[(a<<16)+b]; ok {
+				changed = true
+				return -1
+			} else {
+				return 1
 			}
-			return v
 		})
-		ok := true
-		for i, a := range nums {
-			if a != nums2[i] {
-				ok = false
-				break
-			}
-		}
-
-		if ok {
+		if !changed {
 			p1 += mid1
 		} else {
 			p2 += nums[len(nums)/2]
 		}
 		nums = nums[:0]
-		nums2 = nums2[:0]
 	}
 
 	return p1, p2
