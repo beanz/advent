8a66e89616ade9d298a3107c58958fe04ae0242d
beanz <79058+beanz@users.noreply.github.com>
2024-12-10 19:40:00 +0000

    2024 Day 10 Rust faster

--- aoc-rust/src/bin/aoc-2024-10.rs
+++ aoc-rust/src/bin/aoc-2024-10.rs
@@ -1,20 +1,68 @@
-fn parts(inp: &[u8]) -> (usize, usize) {
-    let w = inp.iter().position(|&ch| ch == b'\n').expect("no EOL");
-    let h = inp.len() / (w + 1);
-    fn get(inp: &[u8], w: usize, h: usize, x: i32, y: i32) -> u8 {
-        if 0 <= x && x < (w as i32) && 0 <= y && y < (h as i32) {
-            inp[(x as usize) + (y as usize) * (w + 1)]
-        } else {
-            b'.'
+pub struct Map<'a, T>
+where
+    T: std::ops::Add<T> + num::Integer,
+{
+    w: T,
+    w1: usize,
+    h: T,
+    inp: &'a [u8],
+}
+
+impl<'a, T> Map<'a, T>
+where
+    T: std::ops::Add<T>
+        + std::cmp::Ord
+        + TryInto<usize>
+        + std::convert::TryFrom<usize>
+        + std::fmt::Debug
+        + Copy
+        + num::Integer,
+    <T as TryInto<usize>>::Error: std::fmt::Debug,
+    <T as std::convert::TryFrom<usize>>::Error: std::fmt::Debug,
+    usize: TryFrom<T>,
+{
+    pub fn new(inp: &'a [u8]) -> Map<'a, T> {
+        let w = inp.iter().position(|&ch| ch == b'\n').expect("no EOL");
+        let h = inp.len() / (w + 1);
+
+        Map {
+            w: w.try_into().unwrap(),
+            w1: (w + 1).into(),
+            h: h.try_into().unwrap(),
+            inp,
         }
     }
+    pub fn height(&self) -> T {
+        self.h
+    }
+    pub fn width(&self) -> T {
+        self.w
+    }
+    pub fn in_bounds(&self, x: &T, y: &T) -> bool {
+        T::zero() <= *x && *x < self.w && T::zero() <= *y && *y < self.h
+    }
+    pub fn get(&self, x: T, y: T) -> Option<u8> {
+        if self.in_bounds(&x, &y) {
+            Some(
+                self.inp[TryInto::<usize>::try_into(x).unwrap()
+                    + TryInto::<usize>::try_into(y).unwrap() * self.w1],
+            )
+        } else {
+            None
+        }
+    }
+}
+
+fn parts<'a>(inp: &'a [u8]) -> (usize, usize) {
+    let m = Map::<'a, i32>::new(inp);
+    let w = m.width();
     let score = |x, y, z| -> usize {
         let mut sc = 0;
         let mut todo = aoc::deque::Deque::<(i32, i32, u8), 512>::default();
         todo.push((x, y, z));
         let mut seen = [false; 2048];
         while let Some((x, y, z)) = todo.pop() {
-            let k = (x as usize) + (y as usize) * (w + 1);
+            let k = (x + y * w) as usize;
             if seen[k] {
                 continue;
             }
@@ -23,46 +71,52 @@ fn parts(inp: &[u8]) -> (usize, usize) {
                 sc += 1;
                 continue;
             }
-            if get(inp, w, h, x, y - 1) == z + 1 {
-                todo.push((x, y - 1, z + 1));
+            let z = z + 1;
+            let want = Some(z);
+            if m.get(x, y - 1) == want {
+                todo.push((x, y - 1, z));
             }
-            if get(inp, w, h, x + 1, y) == z + 1 {
-                todo.push((x + 1, y, z + 1));
+            if m.get(x + 1, y) == want {
+                todo.push((x + 1, y, z));
             }
-            if get(inp, w, h, x, y + 1) == z + 1 {
-                todo.push((x, y + 1, z + 1));
+            if m.get(x, y + 1) == want {
+                todo.push((x, y + 1, z));
             }
-            if get(inp, w, h, x - 1, y) == z + 1 {
-                todo.push((x - 1, y, z + 1));
+            if m.get(x - 1, y) == want {
+                todo.push((x - 1, y, z));
             }
         }
         sc
     };
-    fn rank(inp: &[u8], w: usize, h: usize, x: i32, y: i32, z: u8) -> usize {
+    fn rank(m: &Map<'_, i32>, x: i32, y: i32, z: u8) -> usize {
         let mut r = 0;
         if z == b'9' {
             return 1;
         }
-        if get(inp, w, h, x, y - 1) == z + 1 {
-            r += rank(inp, w, h, x, y - 1, z + 1);
+        let z = z + 1;
+        let want = Some(z);
+        if m.get(x, y - 1) == want {
+            r += rank(m, x, y - 1, z);
         }
-        if get(inp, w, h, x + 1, y) == z + 1 {
-            r += rank(inp, w, h, x + 1, y, z + 1);
+        if m.get(x + 1, y) == want {
+            r += rank(m, x + 1, y, z);
         }
-        if get(inp, w, h, x, y + 1) == z + 1 {
-            r += rank(inp, w, h, x, y + 1, z + 1);
+        if m.get(x, y + 1) == want {
+            r += rank(m, x, y + 1, z);
         }
-        if get(inp, w, h, x - 1, y) == z + 1 {
-            r += rank(inp, w, h, x - 1, y, z + 1);
+        if m.get(x - 1, y) == want {
+            r += rank(m, x - 1, y, z);
         }
         r
     }
     let (mut p1, mut p2) = (0, 0);
+    let w = inp.iter().position(|&ch| ch == b'\n').expect("no EOL");
+    let h = inp.len() / (w + 1);
     for y in 0..h {
         for x in 0..w {
             if inp[x + y * (w + 1)] == b'0' {
                 p1 += score(x as i32, y as i32, b'0');
-                p2 += rank(inp, w, h, x as i32, y as i32, b'0');
+                p2 += rank(&m, x as i32, y as i32, b'0');
             }
         }
     }
