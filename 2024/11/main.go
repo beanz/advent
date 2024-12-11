c451f13c7b67e17ed152dc350537814b59cd3385
beanz <79058+beanz@users.noreply.github.com>
2024-12-11 20:00:54 +0000

    2024 Day 11 Go faster

--- 2024/11/main.go
+++ 2024/11/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	_ "embed"
 	"fmt"
-	"strconv"
 
 	. "github.com/beanz/advent/lib-go"
 )
@@ -11,45 +10,55 @@ import (
 //go:embed input.txt
 var input []byte
 
-type rec struct {
-	s, n int
+func uintLen[T AoCInt](n T) int {
+	for l, m := 1, T(10); true; l, m = l+1, m*10 {
+		if n < m {
+			return l
+		}
+	}
+	panic("unreachable")
 }
 
-func blink(mem map[rec]int, s, n int) int {
+func blink(mem []int, s, n int) int {
 	if n == 0 {
 		return 1
 	}
-	if v, ok := mem[rec{s, n}]; ok {
-		return v
+	k := (s << 7) + n
+	if k < len(mem) {
+		if v := mem[k]; v != 0 {
+			return v - 1
+		}
 	}
+
 	var res int
-	ss := strconv.Itoa(s)
-	l := len(ss)
-	switch {
-	case s == 0:
+	if s == 0 {
 		res = blink(mem, 1, n-1)
-	case l%2 == 0:
-		as, bs := ss[0:len(ss)/2], ss[len(ss)/2:]
-		a, _ := strconv.Atoi(as)
-		b, _ := strconv.Atoi(bs)
+	} else if l := uintLen(s); l%2 == 0 {
+		var a, b, m, i int
+		for a, b, m, i = 0, s, 1, 0; i < l/2; a, b, m, i = a+m*(b%10), b/10, m*10, i+1 {
+		}
 		resA := blink(mem, a, n-1)
 		resB := blink(mem, b, n-1)
 		res = resA + resB
-	default:
+	} else {
 		res = blink(mem, s*2024, n-1)
 	}
-	mem[rec{s, n}] = res
+	if k < len(mem) {
+		mem[k] = res + 1
+	}
 	return res
 }
 
+const CACHE_SIZE = 131072
+
 func Parts(in []byte, args ...int) (int, int) {
 	p1, p2 := 0, 0
-	mem := make(map[rec]int, 131072)
+	mem := [CACHE_SIZE]int{}
 	for i := 0; i < len(in); {
 		j, s := ChompUInt[int](in, i)
 		i = j + 1
-		p1 += blink(mem, s, 25)
-		p2 += blink(mem, s, 75)
+		p1 += blink(mem[:], s, 25)
+		p2 += blink(mem[:], s, 75)
 	}
 	return p1, p2
 }
--- 2024/11/main_test.go
+++ 2024/11/main_test.go
@@ -5,10 +5,20 @@ import (
 	"testing"
 
 	"github.com/beanz/advent/lib-go/tester"
+	"github.com/stretchr/testify/assert"
 )
 
 //go:embed input.txt
 var safeinput []byte
+
+func Test_uintLen(t *testing.T) {
+	assert.Equal(t, 1, uintLen(0))
+	assert.Equal(t, 1, uintLen(9))
+	assert.Equal(t, 2, uintLen(10))
+	assert.Equal(t, 2, uintLen(99))
+	assert.Equal(t, 3, uintLen(100))
+	assert.Equal(t, 3, uintLen(999))
+}
 
 func TestParts(t *testing.T) {
 	tester.RunWithArgs(t, Parts)
