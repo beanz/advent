e4d32a2d9517671679780c5b32aea5b661335d93
beanz <79058+beanz@users.noreply.github.com>
2024-12-25 05:56:53 +0000

    2024 Day 25 Zig (w/o SIMD is faster)
    Probably need to do batches.

--- 2024/25/aoc.zig
+++ 2024/25/aoc.zig
@@ -1,18 +1,16 @@
 const std = @import("std");
 const aoc = @import("aoc-lib.zig");
 
-const Vec5 = @Vector(5, u8);
-
 test "testcases" {
     try aoc.TestCases(usize, parts);
 }
 
 fn parts(inp: []const u8) anyerror![2]usize {
-    var keys = try std.BoundedArray(Vec5, 512).init(0);
-    var locks = try std.BoundedArray(Vec5, 512).init(0);
+    var keys = try std.BoundedArray([5]u8, 512).init(0);
+    var locks = try std.BoundedArray([5]u8, 512).init(0);
     var i: usize = 0;
     while (i < inp.len) : (i += 43) {
-        var c: Vec5 = @splat(0);
+        var c: [5]u8 = .{0} ** 5;
         for (0..5) |j| {
             for (1..6) |k| {
                 c[j] += if (inp[i + j + k * 6] == '#') 1 else 0;
@@ -27,7 +25,7 @@ fn parts(inp: []const u8) anyerror![2]usize {
     var p1: usize = 0;
     for (locks.slice()) |lock| {
         for (keys.slice()) |key| {
-            p1 += if (@reduce(.Max, lock + key) < 6) 1 else 0;
+            p1 += if (lock[0] + key[0] < 6 and lock[1] + key[1] < 6 and lock[2] + key[2] < 6 and lock[3] + key[3] < 6 and lock[4] + key[4] < 6) 1 else 0;
         }
     }
 
