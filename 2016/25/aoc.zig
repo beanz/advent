bc34f448f3f8e2005641f212a03c983018d3841b
beanz <79058+beanz@users.noreply.github.com>
2025-01-07 20:51:52 +0000

    2016 Day 25 Zig

---
+++ 2016/25/aoc.zig
@@ -0,0 +1,75 @@
+const std = @import("std");
+const aoc = @import("aoc-lib.zig");
+
+test "testcases" {
+    try aoc.TestCases(usize, parts);
+}
+
+const Op = enum { CpyReg, Load, Inc, Dec, Jnz, Jmp, Nop, Out };
+
+const Inst = struct {
+    op: Op,
+    lit: isize,
+    reg: u8,
+    out: u8,
+};
+
+fn parts(inp: []const u8) anyerror![2]usize {
+    var b = try std.BoundedArray(Inst, 32).init(0);
+    var i: usize = 0;
+    while (i < inp.len) {
+        switch (inp[i]) {
+            'c' => {
+                if (inp[i + 4] < 64) {
+                    i += 4;
+                    const l = try aoc.chompInt(isize, inp, &i);
+                    try b.append(Inst{ .op = Op.Load, .lit = l, .reg = inp[i + 1] - 'a', .out = 0 });
+                    i += 3;
+                } else {
+                    try b.append(Inst{ .op = Op.CpyReg, .lit = 0, .reg = inp[i + 4] - 'a', .out = inp[i + 6] - 'a' });
+                    i += 8;
+                }
+            },
+            'i' => {
+                try b.append(Inst{ .op = Op.Inc, .lit = 0, .reg = inp[i + 4] - 'a', .out = 0 });
+                i += 6;
+            },
+            'd' => {
+                try b.append(Inst{ .op = Op.Dec, .lit = 0, .reg = inp[i + 4] - 'a', .out = 0 });
+                i += 6;
+            },
+            'o' => {
+                try b.append(Inst{ .op = Op.Out, .lit = 0, .reg = inp[i + 4] - 'a', .out = 0 });
+                i += 6;
+            },
+            'j' => {
+                const v = inp[i + 4];
+                i += 6;
+                const o = try aoc.chompInt(isize, inp, &i);
+                if (v == '0') {
+                    try b.append(Inst{ .op = Op.Nop, .lit = o, .reg = 0, .out = 0 });
+                } else if (v == '1') {
+                    try b.append(Inst{ .op = Op.Jmp, .lit = o, .reg = 0, .out = 0 });
+                } else {
+                    try b.append(Inst{ .op = Op.Jnz, .lit = o, .reg = v - 'a', .out = 0 });
+                }
+                i += 1;
+            },
+            else => unreachable,
+        }
+    }
+    const prog = b.slice();
+    const n: usize = @as(usize, @intCast(prog[1].lit * prog[2].lit));
+    return [2]usize{ 0b101010101010 - n, 0 };
+}
+
+fn day(inp: []const u8, bench: bool) anyerror!void {
+    const p = try parts(inp);
+    if (!bench) {
+        aoc.print("Part1: {}\nPart2: {}\n", .{ p[0], p[1] });
+    }
+}
+
+pub fn main() anyerror!void {
+    try aoc.benchme(aoc.input(), day);
+}
