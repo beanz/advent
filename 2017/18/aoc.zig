8f6fc856b5fd187ea0f01fb652b7fec4b55e2d4c
beanz <79058+beanz@users.noreply.github.com>
2025-01-11 13:21:00 +0000

    2017 Day 18 Zig

--- 2017/18/aoc.zig
+++ 2017/18/aoc.zig
@@ -35,143 +35,38 @@ const Inst = union(enum) {
 };
 
 fn parts(inp: []const u8) anyerror![2]usize {
-    var s = try std.BoundedArray(Inst, 64).init(0);
     var i: usize = 0;
-    while (i < inp.len) : (i += 1) {
-        const r = inp[i + 4];
-        switch (inp[i]) {
-            'a' => {
-                i += 6;
-                if (inp[i] < 'a') {
-                    const v = try aoc.chompInt(Value, inp, &i);
-                    try s.append(Inst{ .addLit = RegValue{ .reg = r & 0x1f, .value = v } });
-                } else {
-                    const r2 = inp[i] & 0x1f;
-                    i += 1;
-                    try s.append(Inst{ .addReg = RegReg{ .reg = r & 0x1f, .reg2 = r2 } });
-                }
-            },
-            'j' => {
-                i += 6;
-                if (inp[i] < 'a') {
-                    const v = try aoc.chompInt(Value, inp, &i);
-                    if (r < 'a') {
-                        try s.append(Inst{ .jmp = v });
-                    } else {
-                        try s.append(Inst{ .jgzLit = RegValue{ .reg = r & 0x1f, .value = v } });
-                    }
-                } else {
-                    const r2 = inp[i] & 0x1f;
-                    i += 1;
-                    try s.append(Inst{ .jgzReg = RegReg{ .reg = r & 0x1f, .reg2 = r2 } });
-                }
-            },
-            'm' => {
-                if (inp[i + 1] == 'o') {
-                    i += 6;
-                    if (inp[i] < 'a') {
-                        const v = try aoc.chompInt(Value, inp, &i);
-                        try s.append(Inst{ .modLit = RegValue{ .reg = r & 0x1f, .value = v } });
-                    } else {
-                        const r2 = inp[i] & 0x1f;
-                        i += 1;
-                        try s.append(Inst{ .modReg = RegReg{ .reg = r & 0x1f, .reg2 = r2 } });
-                    }
-                } else {
-                    i += 6;
-                    if (inp[i] < 'a') {
-                        const v = try aoc.chompInt(Value, inp, &i);
-                        try s.append(Inst{ .mulLit = RegValue{ .reg = r & 0x1f, .value = v } });
-                    } else {
-                        const r2 = inp[i] & 0x1f;
-                        i += 1;
-                        try s.append(Inst{ .mulReg = RegReg{ .reg = r & 0x1f, .reg2 = r2 } });
-                    }
-                }
-            },
-            'r' => {
-                try s.append(Inst{ .rcv = r & 0x1f });
-                i += 5;
-            },
-            's' => {
-                if (inp[i + 1] == 'e') {
-                    i += 6;
-                    if (inp[i] < 'a') {
-                        const v = try aoc.chompInt(Value, inp, &i);
-                        try s.append(Inst{ .setLit = RegValue{ .reg = r & 0x1f, .value = v } });
-                    } else {
-                        const r2 = inp[i] & 0x1f;
-                        i += 1;
-                        try s.append(Inst{ .setReg = RegReg{ .reg = r & 0x1f, .reg2 = r2 } });
-                    }
-                } else {
-                    try s.append(Inst{ .snd = r & 0x1f });
-                    i += 5;
-                }
-            },
-            else => unreachable,
+    for (0..8) |_| {
+        while (inp[i] != '\n') : (i += 1) {}
+        i += 1;
+    }
+    i += 6;
+    const n = try aoc.chompUint(usize, inp, &i);
+    i += 7;
+    var p = try aoc.chompUint(usize, inp, &i);
+    var v: [127]usize = .{0} ** 127;
+    for (0..n) |ii| {
+        p *= 8505;
+        p %= 0x7fffffff;
+        p *= 129749;
+        p += 12345;
+        p %= 0x7fffffff;
+        v[ii] = p % 10000;
+    }
+    const p1 = v[n - 1];
+    var c: usize = 1;
+    var swap = true;
+    while (swap) {
+        c += 1;
+        swap = false;
+        for (1..n) |ii| {
+            if (v[ii - 1] < v[ii]) {
+                swap = true;
+                std.mem.swap(usize, &v[ii], &v[ii - 1]);
+            }
         }
     }
-    const prog = s.slice();
-    var ip: usize = 0;
-    var reg: [27]Value = .{0} ** 27;
-    var snd: usize = 0;
-    var p1: usize = 0;
-    while (ip < prog.len) {
-        const inst = prog[ip];
-        switch (inst) {
-            .addLit => |a| {
-                reg[a.reg] += a.value;
-            },
-            .addReg => |a| {
-                reg[a.reg] += reg[a.reg2];
-            },
-            .modLit => |a| {
-                reg[a.reg] = @mod(reg[a.reg], a.value);
-            },
-            .modReg => |a| {
-                reg[a.reg] = @mod(reg[a.reg], reg[a.reg2]);
-            },
-            .mulLit => |a| {
-                reg[a.reg] *= a.value;
-            },
-            .mulReg => |a| {
-                reg[a.reg] *= reg[a.reg2];
-            },
-            .setLit => |a| {
-                reg[a.reg] = a.value;
-            },
-            .setReg => |a| {
-                reg[a.reg] = reg[a.reg2];
-            },
-            .jgzLit => |a| {
-                if (reg[a.reg] > 0) {
-                    ip = @as(usize, @intCast(@as(Value, @intCast(ip)) + a.value));
-                    continue;
-                }
-            },
-            .jgzReg => |a| {
-                if (reg[a.reg] > 0) {
-                    ip = @as(usize, @intCast(@as(Value, @intCast(ip)) + a.reg2));
-                    continue;
-                }
-            },
-            .jmp => |v| {
-                ip = @as(usize, @intCast(@as(Value, @intCast(ip)) + v));
-            },
-            .snd => |r| {
-                snd = @as(usize, @intCast(reg[r]));
-            },
-            .rcv => |r| {
-                if (reg[r] != 0) {
-                    p1 = snd;
-                    break;
-                }
-            },
-        }
-        ip += 1;
-    }
-    return [2]usize{ snd, 999 };
+    return [2]usize{ p1, c * n / 2 };
 }
 
 fn day(inp: []const u8, bench: bool) anyerror!void {
