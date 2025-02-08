e641231a5e29f602456ce7b8004125813c33c96b
beanz <79058+beanz@users.noreply.github.com>
2025-02-08 10:49:55 +0000

    2021 Day 11 Zig faster

--- 2021/11/aoc.zig
+++ 2021/11/aoc.zig
@@ -1,22 +1,25 @@
 const std = @import("std");
 const aoc = @import("aoc-lib.zig");
 
-fn parts(alloc: std.mem.Allocator, in: []const u8, p1Days: usize) ![2]usize {
-    var oct: []u8 = try alloc.dupe(u8, in);
-    defer alloc.free(oct);
-    var w: usize = 0;
-    while (in[w] != '\n') : (w += 1) {}
-    const h = in.len / (w + 1);
+test "testcases" {
+    try aoc.TestCases(usize, parts);
+}
+
+fn parts(inp: []const u8) anyerror![2]usize {
+    var m: [110]u8 = undefined;
+    std.mem.copyForwards(u8, m[0..inp.len], inp);
+    var oct = m[0..inp.len];
+    const w = 1 + std.mem.indexOfScalar(u8, oct, '\n').?;
+    const h = inp.len / w;
     var r = [2]usize{ 0, 0 };
     var c: usize = 0;
-    var day: usize = 1;
-    while (true) : (day += 1) {
+    var d: usize = 1;
+    while (true) : (d += 1) {
         for (oct, 0..) |ch, i| {
             if (ch != '\n') {
                 oct[i] += 1;
             }
         }
-        //aoc.print("{s}\n", .{oct}) catch unreachable;
         for (oct, 0..) |ch, i| {
             if (ch > '9' and ch != '~') {
                 flash(oct, i, w, h);
@@ -30,13 +33,12 @@ fn parts(alloc: std.mem.Allocator, in: []const u8, p1Days: usize) ![2]usize {
             oct[i] = '0';
             p2 += 1;
         }
-        //aoc.print("{}: {}\n{s}\n", .{ day, p2, oct }) catch unreachable;
         c += p2;
-        if (day == p1Days) {
+        if (d == 100) {
             r[0] = c;
         }
-        if (p2 == w * h) {
-            r[1] = day;
+        if (p2 == (w - 1) * h) {
+            r[1] = d;
             return r;
         }
     }
@@ -44,19 +46,18 @@ fn parts(alloc: std.mem.Allocator, in: []const u8, p1Days: usize) ![2]usize {
 }
 
 fn flash(oct: []u8, i: usize, w: usize, h: usize) void {
-    const x = @as(isize, @intCast(i % (w + 1)));
-    const y = @as(isize, @intCast(i / (w + 1)));
-    //aoc.print("flash {},{}\n{s}\n", .{ x, y, oct }) catch unreachable;
+    const x = @as(isize, @intCast(i % w));
+    const y = @as(isize, @intCast(i / w));
     oct[i] = '~';
     for ([3]isize{ x - 1, x, x + 1 }) |nx| {
         for ([3]isize{ y - 1, y, y + 1 }) |ny| {
             if (x == nx and y == ny) {
                 continue;
             }
-            if (nx < 0 or ny < 0 or nx >= w or ny >= h) {
+            if (nx < 0 or ny < 0 or nx >= (w - 1) or ny >= h) {
                 continue;
             }
-            const ni = @as(usize, @intCast(nx)) + @as(usize, @intCast(ny)) * (w + 1);
+            const ni = @as(usize, @intCast(nx)) + @as(usize, @intCast(ny)) * w;
             if (oct[ni] == '~') {
                 continue;
             }
@@ -68,25 +69,13 @@ fn flash(oct: []u8, i: usize, w: usize, h: usize) void {
     }
 }
 
-test "examples" {
-    const test0 = try parts(aoc.talloc, aoc.test0file, 1);
-    const test1 = try parts(aoc.talloc, aoc.test1file, 100);
-    const real = try parts(aoc.talloc, aoc.inputfile, 100);
-    try aoc.assertEq(@as(usize, 9), test0[0]);
-    try aoc.assertEq(@as(usize, 1656), test1[0]);
-    try aoc.assertEq(@as(usize, 1652), real[0]);
-    try aoc.assertEq(@as(usize, 6), test0[1]);
-    try aoc.assertEq(@as(usize, 195), test1[1]);
-    try aoc.assertEq(@as(usize, 220), real[1]);
-}
-
-fn day11(inp: []const u8, bench: bool) anyerror!void {
-    const p = try parts(aoc.halloc, inp, 100);
+fn day(inp: []const u8, bench: bool) anyerror!void {
+    const p = try parts(inp);
     if (!bench) {
-        aoc.print("Part 1: {}\nPart 2: {}\n", .{ p[0], p[1] });
+        aoc.print("Part1: {}\nPart2: {}\n", .{ p[0], p[1] });
     }
 }
 
 pub fn main() anyerror!void {
-    try aoc.benchme(aoc.input(), day11);
+    try aoc.benchme(aoc.input(), day);
 }
