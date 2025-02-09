76e2fb68bd8891b09a151c9621504abaa45412c7
beanz <79058+beanz@users.noreply.github.com>
2025-02-09 08:53:59 +0000

    2021 Day 12 Zig faster with debug

--- 2021/12/aoc.zig
+++ 2021/12/aoc.zig
@@ -1,162 +1,144 @@
 const std = @import("std");
 const aoc = @import("aoc-lib.zig");
 
-const Cave = struct {
-    next: [16]u16,
-    lower: u16,
-    max: u16,
-    alloc: std.mem.Allocator,
+test "testcases" {
+    try aoc.TestCases(u32, parts);
+}
 
-    const START = 1;
-    const END = 2;
+fn chompCave(inp: []const u8, i: *usize) u16 {
+    const ch0: u16 = @intCast(inp[i.*]);
+    i.* += 1;
+    const ch1: u16 = @intCast(inp[i.*]);
+    i.* += 1;
+    switch (ch0) {
+        's' => if (ch1 == 't') {
+            i.* += 3;
+        },
+        'e' => if (ch1 == 'n') {
+            i.* += 1;
+        },
+        else => {},
+    }
+    return (((ch0 & 0x1f) * 26 + (ch1 & 0x1f)) << 1) + @intFromBool(ch0 & 32 != 0);
+}
 
-    pub fn init(alloc: std.mem.Allocator, inp: []const u8) !*Cave {
-        var cave = try alloc.create(Cave);
-        cave.alloc = alloc;
-        @memset(cave.next[0..], 0);
-        cave.lower = 0;
-        cave.max = END;
-        var ids = std.StringHashMap(u16).init(alloc);
-        defer ids.deinit();
-        try ids.put("start", START);
-        try ids.put("end", END);
-        var start: usize = 0;
-        var dash: usize = 0;
-        for (inp, 0..) |ch, i| {
-            switch (ch) {
-                '-' => {
-                    dash = i;
-                },
-                '\n' => {
-                    try cave.addpath(
-                        &ids,
-                        inp[start..dash],
-                        inp[dash + 1 .. i],
-                    );
-                    start = i + 1;
-                },
-                else => {},
-            }
-        }
-        //aoc.print("{any}\n", .{cave}) catch unreachable;
-        return cave;
-    }
-    pub fn deinit(self: *Cave) void {
-        self.alloc.destroy(self);
-    }
-    pub fn solve(self: *Cave, start: u16, cseen: u16, cache: *std.AutoHashMap(usize, usize), p2: bool, twice: bool) anyerror!usize {
-        //aoc.print("solving {b} {b}\n", .{ start, cseen });
-        var seen = cseen;
-        if (start == END) {
-            //aoc.print("  found end\n", .{});
-            return 1;
-        }
-        if (self.lower & start != 0) {
-            seen |= start;
-        }
-        var k = ((@as(usize, @ctz(start)) << 16) + seen) << 1;
-        if (twice) {
-            k += 1;
-        }
-        if (cache.get(k)) |v| {
-            //aoc.print("  found in cache {}\n", .{v});
-            return v;
-        }
-        var paths: usize = 0;
-        const neighbors = self.next[@ctz(start)];
-        //aoc.print("  neighbors: {b}\n", .{neighbors});
-        var nb: u16 = 1;
-        while (nb <= self.max) : (nb <<= 1) {
-            if (nb & neighbors == 0) {
-                continue;
-            }
-            var ntwice = twice;
-            if (seen & nb != 0) {
-                if (!p2 or twice) {
-                    continue;
-                }
-                ntwice = true;
-            }
-            paths += try self.solve(nb, seen, cache, p2, ntwice);
-        }
-        try cache.put(k, paths);
-        return paths;
-    }
-    pub fn part1(self: *Cave) !usize {
-        var cache = std.AutoHashMap(usize, usize).init(self.alloc);
-        defer cache.deinit();
-        return try self.solve(START, 0, &cache, false, false);
-    }
-    pub fn part2(self: *Cave) !usize {
-        var cache = std.AutoHashMap(usize, usize).init(self.alloc);
-        defer cache.deinit();
-        return try self.solve(START, 0, &cache, true, false);
-    }
-    fn id(self: *Cave, ids: *std.StringHashMap(u16), cave: []const u8) !u16 {
-        if (ids.get(cave)) |v| {
-            return v;
-        }
-        self.max <<= 1;
-        try ids.put(cave, self.max);
-        if ('a' <= cave[0] and cave[0] <= 'z') {
-            self.lower |= self.max;
-        }
-        return self.max;
-    }
-    fn addpath(self: *Cave, ids: *std.StringHashMap(u16), s: []const u8, e: []const u8) !void {
-        const start = try self.id(ids, s);
-        const end = try self.id(ids, e);
-        //aoc.print("would add path {s} {b} {} -> {s} {b} {}\n", .{
-        //    s, start, @ctz(start), e, end, @ctz(end),
-        //}) catch unreachable;
-        if (end != START and start != END) { // don't add path to start
-            self.next[@ctz(start)] |= end;
-        }
-        if (start != START and end != END) { // don't add path to start
-            self.next[@ctz(end)] |= start;
-        }
-    }
+const START_ID = start: {
+    var i: usize = 0;
+    break :start chompCave("start", &i);
 };
 
-test "part1" {
-    var t1 = try Cave.init(aoc.talloc, aoc.test1file);
-    defer t1.deinit();
-    try aoc.assertEq(@as(usize, 10), try t1.part1());
-    var t2 = try Cave.init(aoc.talloc, aoc.test2file);
-    defer t2.deinit();
-    try aoc.assertEq(@as(usize, 19), try t2.part1());
-    var t3 = try Cave.init(aoc.talloc, aoc.test3file);
-    defer t3.deinit();
-    try aoc.assertEq(@as(usize, 226), try t3.part1());
-    var r = try Cave.init(aoc.talloc, aoc.inputfile);
-    defer r.deinit();
-    try aoc.assertEq(@as(usize, 4691), try r.part1());
+const END_ID = end: {
+    var i: usize = 0;
+    break :end chompCave("end", &i);
+};
+
+const START = 0;
+const END = 1;
+const END_BIT = 1 << END;
+
+fn parts(inp: []const u8) anyerror![2]u32 {
+    var adj: [16]u16 = .{0} ** 16;
+    var rev: [16][2]u8 = undefined;
+    rev[0] = [2]u8{ 's', 't' };
+    rev[1] = [2]u8{ 'e', 'n' };
+    var small: u16 = 0b11;
+    {
+        var ids: [1404]?u4 = .{null} ** 1404;
+        ids[START_ID] = START;
+        ids[END_ID] = END;
+        var l: u4 = 2;
+        var i: usize = 0;
+        while (i < inp.len) : (i += 1) {
+            const from = from: {
+                const n = chompCave(inp, &i);
+                if (ids[n] == null) {
+                    ids[n] = l;
+                    l += 1;
+                    rev[ids[n].?] = [2]u8{ inp[i - 2], inp[i - 1] };
+                }
+                const id = ids[n].?;
+                small |= @as(u16, n & 1) << id;
+                break :from id;
+            };
+            i += 1;
+            const to = to: {
+                const n = chompCave(inp, &i);
+                if (ids[n] == null) {
+                    ids[n] = l;
+                    l += 1;
+                    rev[ids[n].?] = [2]u8{ inp[i - 2], inp[i - 1] };
+                }
+                const id = ids[n].?;
+                small |= @as(u16, n & 1) << id;
+                break :to id;
+            };
+            if (to != 0) {
+                adj[from] |= @as(u16, 1) << to;
+            }
+            if (to != 1 and from != 0) {
+                adj[to] |= @as(u16, 1) << from;
+            }
+        }
+    }
+    var mem: [MEM_SIZE]u32 = .{0} ** MEM_SIZE;
+    return [2]u32{ solve(rev, mem[0..], adj, small, false), solve(rev, mem[0..], adj, small, true) };
 }
 
-test "part2" {
-    var t1 = try Cave.init(aoc.talloc, aoc.test1file);
-    defer t1.deinit();
-    try aoc.assertEq(@as(usize, 36), try t1.part2());
-    var t2 = try Cave.init(aoc.talloc, aoc.test2file);
-    defer t2.deinit();
-    try aoc.assertEq(@as(usize, 103), try t2.part2());
-    var t3 = try Cave.init(aoc.talloc, aoc.test3file);
-    defer t3.deinit();
-    try aoc.assertEq(@as(usize, 3509), try t3.part2());
-    var r = try Cave.init(aoc.talloc, aoc.inputfile);
-    defer r.deinit();
-    try aoc.assertEq(@as(usize, 140718), try r.part2());
+const MEM_SIZE = 2097152;
+
+fn solve(rev: [16][2]u8, mem: []u32, adj: [16]u16, small: u16, part2: bool) u32 {
+    @memset(mem, 0);
+    var s: [100]u8 = .{32} ** 100;
+    s[0] = 's';
+    s[1] = 't';
+    s[2] = ',';
+    return search(rev, mem, adj, small, part2, START, 0, 3, s[0..]);
 }
 
-fn day12(inp: []const u8, bench: bool) anyerror!void {
-    var cave = try Cave.init(aoc.halloc, inp);
-    const p1 = try cave.part1();
-    const p2 = try cave.part2();
+inline fn key(twice: bool, cur: u4, visited: u16) usize {
+    return @as(usize, @intFromBool(twice)) + (@as(usize, @intCast(cur)) << 17) + (@as(usize, @intCast(visited)) << 1);
+}
+
+fn search(rev: [16][2]u8, mem: []u32, adj: [16]u16, small: u16, twice: bool, cur: u4, visited: u16, sl: usize, s: []u8) u32 {
+    const k: usize = key(twice, cur, visited);
+    var st = mem[k];
+    if (st > 0) {
+        return st;
+    }
+    var next = adj[cur];
+    if (next & END_BIT != 0) {
+        next ^= END_BIT;
+        st += 1;
+        aoc.print("P: {s}end\n", .{s[0..sl]});
+    }
+    var bit = aoc.biterator(u16, next);
+    while (bit.next()) |n| {
+        const to = @as(u4, @intCast(n));
+        const nBit = @as(u16, 1) << to;
+        const big = small & nBit == 0;
+        if (big or visited & nBit == 0) {
+            s[sl] = rev[to][0];
+            s[sl + 1] = rev[to][1];
+            s[sl + 2] = ',';
+            st += search(rev, mem, adj, small, twice, to, visited | nBit, sl + 3, s);
+        } else if (twice) {
+            s[sl] = rev[to][0];
+            s[sl + 1] = rev[to][1];
+            s[sl + 2] = ',';
+            st += search(rev, mem, adj, small, false, to, visited | nBit, sl + 3, s);
+        }
+    }
+    mem[k] = st;
+    return st;
+}
+
+fn day(inp: []const u8, bench: bool) anyerror!void {
+    const p = try parts(inp);
     if (!bench) {
-        aoc.print("Part 1: {}\nPart 2: {}\n", .{ p1, p2 });
+        aoc.print("Part1: {}\nPart2: {}\n", .{ p[0], p[1] });
     }
 }
 
 pub fn main() anyerror!void {
-    try aoc.benchme(aoc.input(), day12);
+    try aoc.benchme(aoc.input(), day);
 }
