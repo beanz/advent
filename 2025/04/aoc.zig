fa6f6454d8023659c6184d56ac5ae0b623a870c7
beanz <79058+beanz@users.noreply.github.com>
2025-12-05 20:25:24 +0000

    2025 Day 4 Zig speed up

--- 2025/04/aoc.zig
+++ 2025/04/aoc.zig
@@ -13,57 +13,69 @@ fn parts(inp: []const u8) anyerror![2]usize {
     var p2: usize = 0;
     const w: usize = std.mem.indexOfScalar(u8, inp, '\n') orelse unreachable;
     const h = inp.len / (w + 1);
-    for (0..h) |y| {
-        for (0..w) |x| {
-            const ch = inp[x + y * (w + 1)];
-            if (ch == '@') {
-                var c: u4 = 0;
-                for (OFFSETS) |o| {
-                    const nx: isize = o[0] + @as(isize, @intCast(x));
-                    const ny: isize = o[1] + @as(isize, @intCast(y));
-                    if (nx < 0 or ny < 0) {
-                        continue;
+    var rolls = try std.BoundedArray(usize, 16384).init(0);
+    {
+        for (0..h) |y| {
+            for (0..w) |x| {
+                const ch = inp[x + y * (w + 1)];
+                if (ch == '@') {
+                    var c: u4 = 0;
+                    for (OFFSETS) |o| {
+                        const nx: isize = o[0] + @as(isize, @intCast(x));
+                        const ny: isize = o[1] + @as(isize, @intCast(y));
+                        if (nx < 0 or ny < 0) {
+                            continue;
+                        }
+                        const unx: usize = @intCast(nx);
+                        const uny: usize = @intCast(ny);
+                        if (unx >= w or uny >= h) {
+                            continue;
+                        }
+                        if (inp[unx + uny * (w + 1)] == '@') {
+                            c += 1;
+                        }
                     }
-                    const unx: usize = @intCast(nx);
-                    const uny: usize = @intCast(ny);
-                    if (unx >= w or uny >= h) {
-                        continue;
+                    if (c < 4) {
+                        p1 += 1;
+                        if (c == 0) {
+                            p2 += 1;
+                        }
                     }
-                    if (inp[unx + uny * (w + 1)] == '@') {
-                        c += 1;
+                    if (c != 0) {
+                        const gi = 1 + x + (1 + y) * (w + 2);
+                        grid[gi] = c;
+                        try rolls.append(gi);
                     }
                 }
-                if (c < 4) {
-                    p1 += 1;
-                    if (c == 0) {
-                        p2 += 1;
-                    }
-                }
-                grid[1 + x + (1 + y) * (w + 2)] = c;
             }
         }
     }
-    var done: bool = false;
-    while (!done) {
-        done = true;
-        for (1..h + 1) |y| {
-            const iy = y * (w + 2);
-            for (1..w + 1) |x| {
-                const i = x + iy;
+    {
+        var r = rolls.slice();
+        var l = r.len;
+        const gw = w + 2;
+        const gwi: isize = @intCast(gw);
+        const offsets = [8]isize{ -1 - gwi, -gwi, 1 - gwi, -1, 1, -1 + gwi, gwi, 1 + gwi };
+        var done: bool = false;
+        while (!done) {
+            done = true;
+            var j: usize = 0;
+            while (j < l) {
+                const i = r[j];
                 const c = grid[i];
-                if (c == 0) {
+                if (c >= 4) {
+                    j += 1;
                     continue;
                 }
-                if (c < 4) {
-                    done = false;
-                    p2 += 1;
-                    grid[i] = 0;
-                    for (OFFSETS) |o| {
-                        const nx: usize = @intCast(o[0] + @as(isize, @intCast(x)));
-                        const ny: usize = @intCast(o[1] + @as(isize, @intCast(y)));
-                        if (grid[nx + ny * (w + 2)] > 1) {
-                            grid[nx + ny * (w + 2)] -= 1;
-                        }
+                done = false;
+                l -= 1;
+                r[j] = r[l];
+                p2 += 1;
+                grid[i] = 0;
+                for (offsets) |o| {
+                    const ni: usize = @intCast(o + @as(isize, @intCast(i)));
+                    if (grid[ni] > 1) {
+                        grid[ni] -= 1;
                     }
                 }
             }
