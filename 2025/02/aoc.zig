24b2f1cf02e55be8cd570fcd75592416197c95ea
beanz <79058+beanz@users.noreply.github.com>
2025-12-05 19:23:35 +0000

    2025 Day 2 Zig speed up (number string)

--- 2025/02/aoc.zig
+++ 2025/02/aoc.zig
@@ -9,46 +9,102 @@ fn parts(inp: []const u8) anyerror![2]usize {
     var p1: usize = 0;
     var p2: usize = 0;
     var i: usize = 0;
+    var buf: [16]u8 = .{0} ** 16;
     while (i < inp.len) : (i += 1) {
+        const si: usize = i;
         const s = try aoc.chompUint(usize, inp, &i);
+        const sl: usize = i - si;
         i += 1;
         const e = try aoc.chompUint(usize, inp, &i);
-        for (s..e + 1) |n| {
-            const l = std.math.log10_int(n) + 1;
-            if (l & 1 == 0) {
-                const max = try std.math.powi(usize, 10, l / 2);
-                const second = @divFloor(n, max);
-                if (n == second + second * max) {
-                    p1 += n;
-                    p2 += n;
-                    continue;
+        std.mem.copyForwards(u8, buf[16 - sl .. 16], inp[si .. si + sl]);
+        var ns = Num{
+            .n = s,
+            .b = &buf,
+            .i = 16 - sl,
+            .l = sl,
+        };
+        while (true) {
+            const repeats = ns.invalid();
+            if (repeats >= 2) {
+                p2 += ns.n;
+                if (repeats == 2) {
+                    p1 += ns.n;
                 }
             }
-            var j: usize = 3;
-            while (j <= l) {
-                if (l % j != 0) {
-                    j += 2;
-                    continue;
-                }
-                const k = @divExact(l, j);
-                const m = try std.math.powi(usize, 10, k);
-                const div = try std.math.powi(usize, 10, l - k);
-                const first = @divFloor(n, div);
-                var c: usize = 0;
-                for (0..j) |_| {
-                    c *= m;
-                    c += first;
-                }
-                if (c == n) {
-                    p2 += n;
-                    break;
-                }
-                j += 2;
+            if (ns.n == e) {
+                break;
             }
+            ns.inc();
         }
     }
     return [2]usize{ p1, p2 };
 }
+
+const Num = struct {
+    n: usize,
+    b: []u8,
+    i: usize,
+    l: usize,
+    pub fn inc(n: *Num) void {
+        n.n += 1;
+        var j: usize = n.l - 1;
+        while (true) : (j -= 1) {
+            n.b[n.i + j] += 1;
+            if (n.b[n.i + j] != ':') {
+                return;
+            }
+            n.b[n.i + j] = '0';
+            if (j == 0) {
+                break;
+            }
+        }
+        n.i -= 1;
+        n.b[n.i] = '1';
+        n.l += 1;
+    }
+    pub fn bytes(n: @This()) []u8 {
+        return n.b[n.i .. n.i + n.l];
+    }
+    pub fn matchingHalves(n: @This()) bool {
+        if (n.l & 1 == 1) {
+            return false;
+        }
+        const m = n.l / 2;
+        for (0..m) |j| {
+            if (n.b[n.i + j] != n.b[n.i + m + j]) {
+                return false;
+            }
+        }
+        return true;
+    }
+    pub fn invalid(n: @This()) usize {
+        if (n.matchingHalves()) {
+            return 2;
+        }
+        var i: usize = 3;
+        OUTER: while (i < n.l) : (i += 2) {
+            const m = @divFloor(n.l, i);
+            if (m * i != n.l) {
+                continue;
+            }
+            for (0..m) |j| {
+                var k = m + j;
+                while (k < n.l) : (k += m) {
+                    if (n.b[n.i + j] != n.b[n.i + k]) {
+                        continue :OUTER;
+                    }
+                }
+            }
+            return i;
+        }
+        for (1..n.l) |j| {
+            if (n.b[n.i] != n.b[n.i + j]) {
+                return 0;
+            }
+        }
+        return n.l;
+    }
+};
 
 fn day(inp: []const u8, bench: bool) anyerror!void {
     const p = try parts(inp);
